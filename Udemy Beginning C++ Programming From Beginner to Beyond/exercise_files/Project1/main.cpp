fe7ebb0542c99f7fe292e630ff6529819f32bdce
Erhan Ertem <eertem@gmail.com>
2022-11-23 13:16:42 +0300

    Udemy Beginning C++ Programming From Beginner to Beyond - Setting up vscode for c++ coding/compiling/debugging

---
+++ Udemy Beginning C++ Programming From Beginner to Beyond/exercise_files/Project1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello from Proj1" << endl;
+  return 0;
+} 
---
+++ Udemy Beginning C++ Programming From Beginner to Beyond/exercise_files/Project2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num{100};
+  cout << "Hello from Proj2" << endl;
+  cout << "Enter your favorite number : ";
+  cin >> num;
+  cout << "Your favorite number is " << num << endl;
+  return 0;
+} 
