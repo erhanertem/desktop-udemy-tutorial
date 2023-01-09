f0bf64482f2c67ea6ac6da942d5122de4175bc37
Erhan Ertem <eertem@gmail.com>
2023-01-09 17:17:23 +0300

    Finalize section 6

--- Udemy The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert/exercise_files/v2/exercises.sql
+++ Udemy The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert/exercise_files/v2/exercises.sql
@@ -2,7 +2,7 @@
 
 -- SHOW DATABASES;
 -- CREATE DATABASE pet_shop;
--- USE DATABASE pet_shop;
+-- USE pet_shop;
 -- SELECT DATABASE();
 -- CREATE TABLE cats (name VARCHAR(50), age INT);
 -- CREATE TABLE dogs (name VARCHAR(50), breed VARCHAR(50), age INT);
@@ -59,5 +59,104 @@
 -- current_status VARCHAR(100) NOT NULL DEFAULT 'employed');
 -- INSERT INTO employees (first_name, last_name, age) VALUES('thomas', 'chickenman', 87);
 -- SELECT * FROM employees;
+-- DROP TABLE cats, cats2, cats3, cats4, people, shops, unique_cats, employees;
 
 -- SECTION 5: CRUD OPERATIONS
+-- CREATE TABLE cats(cat_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), breed VARCHAR(100), age INT);
+-- DESC cats;
+-- INSERT INTO cats(name, breed, age) 
+-- VALUES ('Ringo', 'Tabby', 4),
+--       ('Cindy', 'Maine Coon', 10),
+--       ('Dumbledore', 'Maine Coon', 11),
+--       ('Egg', 'Persian', 4),
+--       ('Misty', 'Tabby', 13),
+--       ('George Michael', 'Ragdoll', 9),
+--       ('Jackson', 'Sphynx', 7);
+-- SELECT * FROM cats;
+-- CRUD - READ DATA
+-- SELECT age FROM  cats;  -- FILTER BY COLUMN/FIELD
+-- SELECT age, breed FROM  cats;  -- FILTER BY COLUMN/FIELD
+-- SELECT * FROM cats WHERE breed="Maine Coon" AND age>=11;
+-- SELECT cat_id FROM cats;
+-- SELECT name, breed FROM cats;
+-- SELECT name, age FROM cats WHERE breed='Tabby';
+-- SELECT cat_id, age FROM cats WHERE cat_id=age;
+-- SELECT cat_id AS id, age AS kitty_age FROM cats WHERE cat_id=age;
+-- CRUD - UPDATE DATA
+-- UPDATE cats SET breed='Shorthair' WHERE breed='Tabby';
+-- UPDATE cats SET breed='Shorthair',age =5 WHERE breed='Tabby';
+-- UPDATE cats SET breed='Shorthair',age =5 WHERE name='Georgy';
+-- UPDATE cats SET name='Jack' WHERE name='Jackson';
+-- UPDATE cats SET breed='British Shorthair' WHERE name='Ringo';
+-- UPDATE cats SET age='12' WHERE breed='Maine Coon';
+-- SELECT * FROM cats;
+-- CRUD - DELETE DATA
+-- DELETE FROM cats WHERE name='Egg';
+-- DELETE FROM cats WHERE age=4;
+-- DELETE FROM cats WHERE cat_id=age;
+-- DELETE FROM cats;
+
+-- SECTION 6 CRUD CHALLANGE
+-- CREATE DATABASE shirts_db;
+-- SHOW DATABASES;
+-- USE shirts_db;
+-- SELECT DATABASE();
+-- CREATE TABLE shirts (shirt_id INT AUTO_INCREMENT PRIMARY KEY, article VARCHAR(50) NOT NULL, color VARCHAR(50) NOT NULL, shirt_size VARCHAR(4) NOT NULL, last_worn INT NOT NULL DEFAULT 0);
+-- INSERT INTO shirts (article, color, shirt_size, last_worn) VALUES ('t-shirt', 'white', 'S', 10),
+-- ('t-shirt', 'green', 'S', 200),
+-- ('polo shirt', 'black', 'M', 10),
+-- ('tank top', 'blue', 'S', 50),
+-- ('t-shirt', 'pink', 'S', 0),
+-- ('polo shirt', 'red', 'M', 5),
+-- ('tank top', 'white', 'S', 200),
+-- ('tank top', 'blue', 'M', 15);
+-- INSERT INTO shirts (article, color, shirt_size, last_worn) VALUES ('polo shirt', 'purple', 'M', 50);
+-- SELECT article, color FROM shirts;
+-- SELECT article, color, shirt_size, last_worn FROM shirts WHERE shirt_size='M';
+-- UPDATE shirts  SET shirt_size = 'L' WHERE article='polo shirt';
+-- UPDATE shirts SET last_worn=0 WHERE last_worn=15;
+-- UPDATE shirts SET shirt_size='XS', color='off white' WHERE color='white';
+-- DELETE FROM shirts WHERE last_worn=200;
+-- DELETE FROM shirts WHERE article='tank top';
+-- DELETE FROM shirts;
+-- SELECT * FROM shirts;
+-- DROP TABLE shirts;
+-- DROP DATABASE shirts_db;
+
+-- SECTION 7 STRING FUNCTIONS
+-- CREATE DATABASE book_shop;
+-- USE book_shop;
+-- CREATE TABLE books 
+-- 	(
+-- 		book_id INT NOT NULL AUTO_INCREMENT,
+-- 		title VARCHAR(100),
+-- 		author_fname VARCHAR(100),
+-- 		author_lname VARCHAR(100),
+-- 		released_year INT,
+-- 		stock_quantity INT,
+-- 		pages INT,
+-- 		PRIMARY KEY(book_id)
+-- 	);
+
+-- INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+-- VALUES
+-- ('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+-- ('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+-- ('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+-- ('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+-- ('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+-- ('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+-- ('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+-- ('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+-- ('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+-- ('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+-- ('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+-- ("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+-- ('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+-- ('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+-- ('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+-- ('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+-- SELECT * FROM `book_shop`.`books` LIMIT 1000;
+
+SELECT  title
+FROM books;
