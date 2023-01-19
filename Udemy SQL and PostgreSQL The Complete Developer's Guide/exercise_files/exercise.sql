ab77240dd4e77a4bc52c056db023bdcd89b9cd01
Erhan Ertem <eertem@gmail.com>
2023-01-19 13:21:59 +0300

    Finalize section 2 - SQL BOOTCAMP Stephen

--- Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
+++ Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
@@ -54,3 +54,146 @@ FROM
   cities;
 
 -- -- LESSON 2 FILTERING RECORDS
+-- WHERE STATEMENT
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area > 4000;
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area <> 4000;
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area != 4000;
+
+-- WHERE WITH LOGICAL OPERATORS
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area BETWEEN 2000
+  AND 4000;
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  name IN ('Delhi', 'Shangai');
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  name NOT IN ('Delhi', 'Shangai');
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area NOT IN (3043, 8223)
+  OR name = 'Sao Paulo';
+
+SELECT
+  name,
+  area
+FROM
+  cities
+WHERE
+  area NOT IN (3043, 8223)
+  AND name = 'Sao Paulo';
+
+SELECT
+  name,
+  price
+FROM
+  phones
+WHERE
+  units_sold > 5000;
+
+SELECT
+  name,
+  manufacturer
+FROM
+  phones
+WHERE
+  manufacturer IN ('Apple', 'Samsung');
+
+SELECT
+  name,
+  (price * units_sold) AS total_revenue
+FROM
+  phones
+WHERE
+  (price * units_sold) > 1000000;
+
+SELECT
+  name,
+  (population / area) AS pop_density
+FROM
+  cities
+WHERE
+  (population / area) > 6000;
+
+-- UPDATE ROWS
+UPDATE
+  cities
+SET
+  population = 39505000
+WHERE
+  name = 'Tokyo';
+
+SELECT
+  *
+FROM
+  cities;
+
+-- DELETE ROWS
+DELETE FROM
+  cities
+WHERE
+  name = 'Tokyo';
+
+DELETE FROM
+  cities
+WHERE
+  name <> 'Tokyo';
+
+UPDATE
+  phones
+SET
+  units_sold = 8543
+WHERE
+  name = 'N8';
+
+DELETE FROM
+  phones
+WHERE
+  manufacturer = 'Samsung';
+
+SELECT
+  *
+FROM
+  phones;
+
+-- -- LESSON 3 WORKING WITH TABLES
