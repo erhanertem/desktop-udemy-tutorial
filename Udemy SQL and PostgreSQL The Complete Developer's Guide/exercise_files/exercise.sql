90bf06114b207d687ed3ec70f31a5e954874c766
Erhan Ertem <eertem@gmail.com>
2023-01-25 18:14:50 +0300

    Finalize section 27 - SQL BOOTCAMP Stephen

--- Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
+++ Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
@@ -2833,3 +2833,47 @@ WHERE
   tags.created_at < '2010-01-07';
 
 -- -- LESSON 27 RECURSIVE COMMON TABLE EXPRESSIONS (RCTEs)
+WITH RECURSIVE countdown AS (
+  SELECT
+    3 AS val
+  UNION
+  SELECT
+    val - 1
+  FROM
+    countdown
+  WHERE
+    val > 0
+)
+SELECT
+  *
+FROM
+  countdown;
+
+WITH RECURSIVE suggestions(leader_id, follower_id, depth) AS (
+  SELECT
+    leader_id,
+    follower_id,
+    1 AS depth
+  FROM
+    followers
+  WHERE
+    follower_id = 1
+  UNION
+  SELECT
+    f.leader_id,
+    f.follower_id,
+    depth + 1
+  FROM
+    followers f
+    JOIN suggestions s ON s.leader_id = f.follower_id
+  WHERE
+    depth < 2
+)
+SELECT
+  DISTINCT users.id,
+  users.username
+FROM
+  suggestions
+  JOIN users ON users.id = suggestions.leader_id
+WHERE
+  depth > 1;
