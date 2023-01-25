a786d6abeb2fcfce3fb3587a462541f1f6130275
Erhan Ertem <eertem@gmail.com>
2023-01-25 13:26:07 +0300

    Finalize section 26 - SQL BOOTCAMP Stephen

--- Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
+++ Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
@@ -2784,3 +2784,52 @@ FROM
   JOIN comments ON comments.user_id = users.id
 WHERE
   username = 'Alyson14';
+
+-- -- LESSON 26 SIMPLE COMMON TABLE EXPRESSIONS (CTEs)
+SELECT
+  username,
+  tags.created_at
+FROM
+  users
+  JOIN(
+    SELECT
+      user_id,
+      created_at
+    FROM
+      caption_tags
+    UNION
+    ALL
+    SELECT
+      user_id,
+      created_at
+    FROM
+      photo_tags
+  ) AS tags ON tags.user_id = users.id
+WHERE
+  tags.created_at < '2010-01-07';
+
+--rewrite the statement with CTEs
+WITH tags AS (
+  SELECT
+    user_id,
+    created_at
+  FROM
+    caption_tags
+  UNION
+  ALL
+  SELECT
+    user_id,
+    created_at
+  FROM
+    photo_tags
+)
+SELECT
+  username,
+  tags.created_at
+FROM
+  users
+  JOIN tags ON tags.user_id = users.id
+WHERE
+  tags.created_at < '2010-01-07';
+
+-- -- LESSON 27 RECURSIVE COMMON TABLE EXPRESSIONS (RCTEs)
