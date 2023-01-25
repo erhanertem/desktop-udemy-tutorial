06b587d0c2e4b54990cd28ff18556e47090a962c
Erhan Ertem <eertem@gmail.com>
2023-01-26 00:25:33 +0300

    Finalize section 29 - SQL BOOTCAMP Stephen

--- Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
+++ Udemy SQL and PostgreSQL The Complete Developer's Guide/exercise_files/exercise.sql
@@ -2973,3 +2973,28 @@ OR REPLACE VIEW recent_posts AS (
 DROP VIEW recent_posts;
 
 -- -- LESSON 29 OPTIMIZING QUERIES WITH MATERIALIZED VIEWS
+-- FOR EACH WEEK, SHOW THE NUMBER OF LIKES THAT POSTS ANDS COMMENTS RECEIVED
+CREATE MATERIALIZED VIEW weekly_likes AS (
+  SELECT
+    date_trunc(
+      'week',
+      COALESCE(posts.created_at, comments.created_at)
+    ) AS week,
+    COUNT(posts.id) AS num_posts,
+    COUNT(comments.id) AS num_comments
+  FROM
+    likes
+    LEFT JOIN posts ON posts.id = likes.post_id
+    LEFT JOIN comments ON comments.id = likes.comment_id
+  GROUP BY
+    week
+  ORDER BY
+    week
+) WITH DATA;
+
+SELECT
+  *
+FROM
+  weekly_likes;
+
+REFRESH MATERIALIZED VIEW weekly_likes;
