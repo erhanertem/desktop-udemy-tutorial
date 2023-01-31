d1f8f5da7878e18c4ab68295a938ca235c966372
Erhan Ertem <eertem@gmail.com>
2023-01-31 17:02:03 +0300

    Finalize section 3 - JS RegExp Steven

--- Udemy Mastering Regular Expressions in JavaScript/exercise_files/app.js
+++ Udemy Mastering Regular Expressions in JavaScript/exercise_files/app.js
@@ -36,24 +36,105 @@
 // console.log(txt.match(regex1));
 
 // LESSON 2 SPECIFYING CHARACTERS IN REGULAR EXPRESSIONS
-let txt = 'how it that so hot? hoot a';
-console.log(txt.match(/h.t/g));
-console.log(txt.match(/h..t/g));
-// d with any character - TAB counts for a single character
-txt = 'This could@ be d\\ the final word.';
-console.log(txt.match(/d./g));
-// escape the character for its literal value
-console.log(txt.match(/d\./g));
-console.log(txt.match(/d\@/g));
-console.log(txt.match(/d\\/g)[0]);
-console.log('This could\\ be the final word.'.match(/d\\/g));
+// let txt = 'how it that so hot? hoot a';
+// console.log(txt.match(/h.t/g));
+// console.log(txt.match(/h..t/g));
+// // d with any character - TAB counts for a single character
+// txt = 'This could@ be d\\ the final word.';
+// console.log(txt.match(/d./g));
+// // escape the character for its literal value
+// console.log(txt.match(/d\./g));
+// console.log(txt.match(/d\@/g));
+// console.log(txt.match(/d\\/g)[0]);
+// console.log('This could\\ be the final word.'.match(/d\\/g));
 
-txt = 'This could be the final word. H\not';
-console.log(txt.match(/H\not/g)[0]);
+// txt = 'This could be the final word. H\not';
+// console.log(txt.match(/H\not/g)[0]);
+
+// // CODING CHALLENGE
+// /*
+// Using the provided array, create a second array that only includes the numbers with the 801 area code. (The area code is the first 3 numbers.)
+// */
+
+// let phoneNums = [
+//   '801-766-9754',
+//   '801-545-5454',
+//   '435-666-1212',
+//   '801-796-8010',
+//   '435-555-9801',
+//   '801-009-0909',
+//   '435-222-8013',
+//   '801-777-6655',
+// ];
+
+// console.log(phoneNums.filter(el => el.match(/^801/) !== null));
+// console.log(phoneNums.filter(el => el.match(/801-/) !== null));
+
+// let regEx = /801-/;
+// console.log(phoneNums.filter(el => regEx.test(el)));
+
+// LESSON 3 WORKING WITH CHARACTERS
+let txt = `Make the outline for t-he square gray and the fill for the circle grey.`;
+let regex = /gr[ae]y/g;
+regex = /[abcd]/g;
+regex = /[abcd][i]/g;
+regex = /[abcd][ i]/g;
+regex = /[r ][abcd][ i]/g;
+regex = /gr[ae]y[.]/g;
+
+// - range metacharacter
+txt = `There have been 42  4 - 5 times I have tried, but I will try it BEZ fagain.`;
+regex = /[\-.]/g;
+regex = /[1-5]/g;
+regex = /[1-5][1-6]/g;
+regex = /[1-6a-zA-Z]/g;
+regex = /[12\-5]/g;
+regex = /[A-Z][a-i]/g;
+regex = /[A-e]/g; // means a range of A-Z + a-e
+regex = /[125-]/g; //- here is not confused as a range so there is no need for an escape character
+regex = /[-,.]/g; //- here is not confused as a range so there is no need for an escape character
+
+txt = `Exception 0xF89F`;
+regex = /0x/g;
+regex = /[0x]/g;
+regex = /[0x]/g;
+regex = /0x[0-9A-F]..[0-9A-F]/g;
+
+txt = `How do we capture the numbers 13 - 20?`;
+regex = /[0-9][0-9]/g;
+regex = /[10-20]/g;
+
+// ^ negate character
+txt = `Exception 0xF89F`;
+regex = /0x[^0-9A-F][^0-9A-F]/g;
+
+txt = `abcdefghijklmnop`;
+regex = /[a-z]/g;
+regex = /[^a-z]/g;
+txt = `abcdefghijklmnop]^`;
+regex = /[^a-z^]/g;
+regex = /[^a-z]/g;
+
+// Escaping metacharacters
+regex = /[A-b\]]/g;
+regex = /[Ab\]]/g;
+
+// Regex shorthands
+txt = `
+A string that contains numbers (12345)
+A second line _ (12345)			3 Tabs.
+`;
+regex = /\d/g;
+regex = /\w/g;
+regex = /\s/g;
+regex = /\D/g;
+regex = /\W/g;
+regex = /\S/g;
+console.log(txt.match(regex));
 
 // CODING CHALLENGE
 /*
-Using the provided array, create a second array that only includes the numbers with the 801 area code. (The area code is the first 3 numbers.)
+Using the provided array, create a second array that only includes the numbers with the 801 area code. (The area code is the first 3 numbers.) Make sure that the phone numbers are valid (nnn-nnn-nnnn).
 */
 
 let phoneNums = [
@@ -64,11 +145,13 @@ let phoneNums = [
   '435-555-9801',
   '801-009-0909',
   '435-222-8013',
-  '801-777-6655',
+  '801-777-66553',
+  '801-777-665-',
+  '801-77A-6655',
+  '801-778-665',
 ];
 
 console.log(phoneNums.filter(el => el.match(/^801/) !== null));
-console.log(phoneNums.filter(el => el.match(/801-/) !== null));
+console.log(phoneNums.filter(el => el.match(/^801-\d\d\d-\d\d\d\d/) !== null));
 
-let regEx = /801-/;
-console.log(phoneNums.filter(el => regEx.test(el)));
+// LESSON 4 USING REPITITION IN REGULAR EXPRESSIONS
