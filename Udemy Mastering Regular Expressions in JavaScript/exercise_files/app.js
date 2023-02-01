b3e0f5cb5d55bceaa72218dba3f7c912526a9a5f
Erhan Ertem <eertem@gmail.com>
2023-02-02 01:45:36 +0300

    Finalize section 6 - JS RegExp Steven

--- Udemy Mastering Regular Expressions in JavaScript/exercise_files/app.js
+++ Udemy Mastering Regular Expressions in JavaScript/exercise_files/app.js
@@ -210,41 +210,122 @@
 // console.log(txt.match(regex));
 
 // LESSON 5 USING ANCHORED EXPRESSIONS
-// ^ Start Anchor metacharacter
-let txt = `801-766-9754, 801-545-5454, 435-666-1212, 801-796-8010, 435-555-9801, 801-009-0909, 435-222-8013, 801-777-801`;
-regex = /^801/g;
-// $ End Anchor metacharacter
-regex = /801$/g;
-// Mixed ^ $
-txt = '801';
-regex = /^801$/g;
-txt = '801a';
-regex = /^801.$/g;
+// // ^ Start Anchor metacharacter
+// let txt = `801-766-9754, 801-545-5454, 435-666-1212, 801-796-8010, 435-555-9801, 801-009-0909, 435-222-8013, 801-777-801`;
+// regex = /^801/g;
+// // $ End Anchor metacharacter
+// regex = /801$/g;
+// // Mixed ^ $
+// txt = '801';
+// regex = /^801$/g;
+// txt = '801a';
+// regex = /^801.$/g;
 
-txt = `The dot is a very powerful regex metacharacter. It allows you to be lazy. Put in a dot, and 
-everything matches just fine when you test the regex on valid data.
-The problem is that the regex also matches in cases where it should not match. If you are new to regular expressions, 
-some of these cases may not be so obvious at first.`;
-regex = /^The/gim;
-regex = /\.$/gm;
+// txt = `The dot is a very powerful regex metacharacter. It allows you to be lazy. Put in a dot, and
+// everything matches just fine when you test the regex on valid data.
+// The problem is that the regex also matches in cases where it should not match. If you are new to regular expressions,
+// some of these cases may not be so obvious at first.`;
+// regex = /^The/gim;
+// regex = /\.$/gm;
 
-// boundary metacharacters
-txt = 'HELLO, LOOK AT YOU!';
-regex = /LO\b/;
-regex = /\bLO/;
-txt = 'Inplant this idea: plan to plant multiple trees on this planet.';
-regex = /\bplant\b/g;
-regex = /\bthis\b/g;
-regex = /\Bplan\B/g;
-console.log(txt.match(regex));
+// // boundary metacharacters
+// txt = 'HELLO, LOOK AT YOU!';
+// regex = /LO\b/;
+// regex = /\bLO/;
+// txt = 'Inplant this idea: plan to plant multiple trees on this planet.';
+// regex = /\bplant\b/g;
+// regex = /\bthis\b/g;
+// regex = /\Bplan\B/g;
+// console.log(txt.match(regex));
+
+// // CODING CHALLENGE
+// /*
+// The content.js file contains a string of text stored in a variable text1. This string of text is a statement that contains a day of the week as a part of the statement. Write a regular expression that will match any day of the week and then replace that day of the week in the string with Monday. Display the results to the console.
+// */
+// let text1 =
+//   'Each and every Tuesday, at the beginning of the day, we hold a staff meeting. At the Tuesday staff meeting, you will need to make a report on the past weeks progress, and you will receive assignments for the following Tuesday. Just be aware that somedays this Tuesday meeting might not occur. When that happens, we will make an announcement.';
+// regex = /Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday/gi;
+// regex = /(Tues|Wednes|Thurs|Fri|Satur|Sun)day/gim;
+// regex = /\b[mtwfs][a-z]{1,4}[nris]day\b/gi;
+// console.log(text1.replace(regex, 'Monday'));
+
+// LESSON 6 WORKING WITH ALTERNATES AND GROUPS
+// () grouping metacharacter
+let txt =
+  'a5c3a2b1d1 a1e3a2b1a4 d5c3a2b1d1 aac3a2b1d1 a5c3a2b1d6 a5c312b1d1 d4b2a5b2d3';
+regex = /([a-z][0-9]){5}/g;
+
+txt = '2018/3/9';
+regex = /^(\d{4})[-./](\d{1,2})[-./](\d{1,2})$/;
+// console.log(txt.match(regex));
+// console.log(regex.exec(txt));
+
+txt = 'yoyomo yo yoyomoyoyo';
+regex = /(yo)\1/g;
+regex = /yoyo/g;
+
+// capturing groups
+txt = '2018/9/9';
+regex = /^(\d{4})[-./](\d{1,2})[-./]\2$/g;
+//non-capturing groups with (?:)
+regex = /^(?:\d{4})[-./](\d{1,2})[-./]\1$/g;
+
+txt = 'a1a1c1d5c1c1d5b2b3d4';
+regex = /([a-d][1-5])/g;
+regex = /([a-d][1-5])\1/g;
+regex = /(?:[a-d][1-5])\1/g;
+
+txt = '<strong>This is a strong tag</strong><i>this is italic</i>';
+regex = /<(strong>).*\/\1/g;
+regex = /<(\w+>).*\/\1/g;
+regex = /<(\w+>)[\w\s]+<\/\1/g;
+
+// named capture group
+txt = '<strong>This is a strong tag</strong><i>this is italic</i>';
+regex = /<(?<first_group>\w+>)[\w\s]+<\/\k<first_group>/g;
+
+//lookahead group (?=)
+txt = 'allthingsjavascript.com google.com youtube.com';
+regex = /\w+(?=\.com)/g;
+
+txt = '16262ertrteAtetyte';
+regex = /^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$/g;
+// console.log(txt.match(regex));
 
 // CODING CHALLENGE
 /*
-The content.js file contains a string of text stored in a variable text1. This string of text is a statement that contains a day of the week as a part of the statement. Write a regular expression that will match any day of the week and then replace that day of the week in the string with Monday. Display the results to the console. 
+Iterate through the data provided. Use a regular expression to store the names in a new array but change the order of the name so first name is listed first and last name is last. 
 */
-let text1 =
-  'Each and every Tuesday, at the beginning of the day, we hold a staff meeting. At the Tuesday staff meeting, you will need to make a report on the past weeks progress, and you will receive assignments for the following Tuesday. Just be aware that somedays this Tuesday meeting might not occur. When that happens, we will make an announcement.';
-regex = /(Tuesday|Wendesday|Thursday|Friday|Saturday|Sunday)/gi;
-regex = /(Tues|Wendes|Thurs|Fri|Satur|Sun)day/gim;
-regex = /\b[mtwfs][a-z]{1,4}[nris]day\b/gi;
-console.log(text1.replace(regex, 'Monday'));
+txt = 'Jensen, Dale';
+let data = [
+  'Jensen, Dale',
+  'Smith, Andrea',
+  'Jorgensen, Michael',
+  'Vasefi, Annika',
+  'Lopez, Monica',
+  'Crockett, Steven',
+  36456464,
+  true,
+  'Prueba 1',
+];
+let data_n = [];
+//#1 solution
+// regex = /^([a-z]+)[,\s]\s*([a-z]+)$/i;
+//#2 solution - named group solution
+regex = /^(?<last>[a-z]+)[,\s]\s*(?<first>[a-z]+)$/i;
+data.forEach((el, i, _) => {
+  console.log(el);
+  if (typeof el === 'string' && regex.test(el)) {
+    // console.log(el.match(regex));
+    // #1 solution
+    // data_n[i] = el.match(regex)[2] + ', ' + el.match(regex)[1];
+    // #2 solution - named group solution
+    data_n[i] =
+      el.match(regex).groups.first + ', ' + el.match(regex).groups.last;
+  }
+});
+
+console.log(data);
+console.log(data_n);
+
+// LESSON 7 WORKIGN WITH UNICODE
