89406da6ade95ec78bda4acfacc8f89dfbb42bb2
Erhan Ertem <eertem@gmail.com>
2023-02-14 17:25:46 +0300

    Finalize section 4 - sql-master bootcamp - Donatus

---
+++ Udemy SQL–MySQL Complete Master Bootcamp Beginner to Expert 2023/exercise_files/exercises.sql
@@ -0,0 +1,54 @@
+-- LESSON 4 SELECT STATEMENT
+SHOW TABLES;
+
+USE northwind;
+
+SELECT
+  *
+FROM
+  employees;
+
+SELECT
+  1+1;
+
+SELECT
+  10/(21 -1);
+
+SELECT
+  'Hello';
+
+SELECT
+  firstname,
+  'great employee',
+  8*10
+FROM
+  employees;
+
+SELECT
+  *
+FROM
+  orders;
+
+SELECT
+  employeeid,
+  orderdate
+FROM
+  orders;
+
+SELECT
+  donuts,
+  orderdate
+FROM
+  orders;
+
+SELECT
+  'donuts',
+  orderdate
+FROM
+  orders;
+
+SELECT
+  67,
+  orderdate
+FROM
+  orders;
