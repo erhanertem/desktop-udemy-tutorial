e291a8f9f058dc738b1a79a104cbd4d1c470d730
Erhan Ertem <eertem@gmail.com>
2023-03-05 22:58:56 +0300

    Get single document from MongoDB

--- Udemy MongoDB - The Complete Developer's Guide 2023/exercise_files/Project/backend/routes/products.js
+++ Udemy MongoDB - The Complete Developer's Guide 2023/exercise_files/Project/backend/routes/products.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const db = require('../db');
 
 const { Decimal128 } = require('mongodb');
+const { ObjectId } = require('mongodb');
 
 const router = Router();
 
@@ -38,8 +39,18 @@ router.get('/', (req, res, next) => {
 
 // Get single product
 router.get('/:id', (req, res, next) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
+  db.getDb()
+    .db()
+    .collection('products')
+    .findOne({ _id: new ObjectId(req.params.id) })
+    .then(productDoc => {
+      productDoc.price = productDoc.price.toString();
+      res.status(200).json(productDoc);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'An error occured' });
+    });
 });
 
 // Add new product
