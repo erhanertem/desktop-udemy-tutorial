5ea6212858c365dab79410dae1055adfcf21a187
Erhan Ertem <eertem@gmail.com>
2023-03-14 17:02:58 +0300

    Fetch user input

--- Udemy Understanding TypeScript/exercise_files/drag&drop_project/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/drag&drop_project/src/app.ts
@@ -1,1 +1,94 @@
 // Code goes here!
+//AutoBind decorator
+function AutoBind(
+  _1: any,
+  _2: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    get() {
+      return originalMethod.bind(this);
+    },
+  };
+  return adjustedDescriptor;
+}
+
+//Project Input Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    // In general, a template is meant to be reused in multiple places. For this reason we copy the content, using importNode (or cloneNode, like here on MDN : https://developer.mozilla.org/en-US/docs/Web/API/Document/importNode)
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    } else return [enteredTitle, enteredDescription, +enteredPeople];
+  }
+
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+    }
+  }
+
+  // #1. Solving this issue with bind(this) @ submit handler
+  // private configure() {
+  //   this.element.addEventListener('submit', this.submitHandler.bind(this));
+  // }
+  // #2. Solving thru class method decorator function to submitHandler
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
