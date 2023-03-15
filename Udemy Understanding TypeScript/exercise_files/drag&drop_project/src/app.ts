426a35cdec4c7ae65401cf1a36966450396f78c6
Erhan Ertem <eertem@gmail.com>
2023-03-15 14:02:57 +0300

    Update ReadMe.md

--- Udemy Understanding TypeScript/exercise_files/drag&drop_project/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/drag&drop_project/src/app.ts
@@ -57,6 +57,40 @@ function AutoBind(
   return adjustedDescriptor;
 }
 
+//ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement; //https://developer.mozilla.org/en-US/docs/Web/API
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent =
+      this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 //Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -133,6 +167,12 @@ class ProjectInput {
     } else return [enteredTitle, enteredDescription, +enteredPeople];
   }
 
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault();
@@ -141,6 +181,7 @@ class ProjectInput {
     if (Array.isArray(userInput)) {
       const [title, desc, people] = userInput;
       console.log(title, desc, people);
+      this.clearInputs();
     }
   }
 
@@ -159,3 +200,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
