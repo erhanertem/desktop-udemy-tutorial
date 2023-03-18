f13c8af61af22024dbcac2866c129164d05c9200
Erhan Ertem <eertem@gmail.com>
2023-03-18 21:11:53 +0300

    Finalize section 3 - typescript - Colt Steele

---
+++ Udemy Mastering TypeScript 2023 Edition/exercise_files/exercises.js
@@ -0,0 +1,17 @@
+//LESSON - TYPE ANNOTATION BASICS
+// STRING
+var movieTitle = 'Amadeus!!';
+movieTitle = 'Arrival';
+movieTitle = 9;
+// movieTitle.upper();
+movieTitle.toUpperCase();
+// NUMBER
+var myNumber = 42;
+// myNumber = 'I am a string!!';
+myNumber = 60;
+var numCatLives = 9;
+numCatLives += 1;
+//BOOLEAN
+var myBoolean = true;
+// myBoolean = 87;
+myBoolean = false;
