c0ae9dff38b38312ed96f8d1078e96ac05458204
Erhan Ertem <eertem@gmail.com>
2023-03-23 11:11:38 +0300

    Finalize section 2 - typescript - Stephen Grider

---
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/dist/exercises.js
@@ -0,0 +1,9 @@
+"use strict";
+const today = new Date();
+today.getMonth();
+const person = {
+    age: 20,
+};
+class Color {
+}
+const red_ = new Color();
--- Udemy Typescript The Complete Developer's Guide/exercise_files/src/exercises.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/src/exercises.ts
@@ -1,24 +1,40 @@
-//LESSON 1 - GETTING STARTED WITH TYPESCRIPT
-import axios from 'axios';
+// //LESSON 1 - GETTING STARTED WITH TYPESCRIPT
+// import axios from 'axios';
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+// const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-interface ToDo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+// interface ToDo {
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// }
 
-axios.get(url).then(res => {
-  // const { id, title, completed } = res.data as ToDo;
-  const { id, title, completed }: ToDo = res.data;
-  logToDo(id, title, completed);
-});
+// axios.get(url).then(res => {
+//   // const { id, title, completed } = res.data as ToDo;
+//   const { id, title, completed }: ToDo = res.data;
+//   logToDo(id, title, completed);
+// });
 
-const logToDo = (id: number, title: string, completed: boolean) => {
-  console.log(`
-  The Todo with ID: ${id}
-  Has a title of  : ${title}
-  Is it finished  ? ${completed}
-  `);
+// const logToDo = (id: number, title: string, completed: boolean) => {
+//   console.log(`
+//   The Todo with ID: ${id}
+//   Has a title of  : ${title}
+//   Is it finished  ? ${completed}
+//   `);
+// };
+
+//LESSON 2 - WHAT'S A TYPE SYSTEM
+const today = new Date();
+today.getMonth();
+// today.sakskdskds();
+
+const person = {
+  age: 20,
 };
+// person.akskjdkds;
+
+class Color {}
+const red_ = new Color();
+// red_.sksksk;
+
+//LESSON 3 - TYPE ANNOTATIONS
