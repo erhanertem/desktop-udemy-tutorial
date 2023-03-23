f17a0ac7657d4f71f2d838a63ffbe6bc081bc99a
Erhan Ertem <eertem@gmail.com>
2023-03-23 12:39:34 +0300

    Finalize section 3 - typescript - Stephen Grider

--- Udemy Typescript The Complete Developer's Guide/exercise_files/dist/exercises.js
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/dist/exercises.js
@@ -1,9 +1,41 @@
 "use strict";
-const today = new Date();
-today.getMonth();
-const person = {
-    age: 20,
-};
-class Color {
+let apples = 5;
+let pineapples = 5;
+let apples_;
+let speed = 'fast';
+let nothingMuch = null;
+let notKnown = undefined;
+let now = new Date();
+let colors = ['red', 'green', 'orange'];
+let myNumbers = [1, 2, 3];
+let truths = [true, false, true];
+class Car {
 }
-const red_ = new Color();
+let care = new Car();
+let point = {
+    x: 1,
+    y: 10,
+};
+const logNumber = i => {
+    console.log(i);
+};
+logNumber(5);
+let samples;
+samples = 5;
+const json = '{"x":10, "y":20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
+let words = ['red', 'green', 'blue'];
+let foundWord;
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+let numbers = [-10, -1, 12];
+let numberAboveZero = false;
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
--- Udemy Typescript The Complete Developer's Guide/exercise_files/src/exercises.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/src/exercises.ts
@@ -23,18 +23,87 @@
 //   `);
 // };
 
-//LESSON 2 - WHAT'S A TYPE SYSTEM
-const today = new Date();
-today.getMonth();
-// today.sakskdskds();
+// //LESSON 2 - WHAT'S A TYPE SYSTEM
+// const today = new Date();
+// today.getMonth();
+// // today.sakskdskds();
 
-const person = {
-  age: 20,
-};
-// person.akskjdkds;
+// const person = {
+//   age: 20,
+// };
+// // person.akskjdkds;
 
-class Color {}
-const red_ = new Color();
-// red_.sksksk;
+// class Color {}
+// const red_ = new Color();
+// // red_.sksksk;
 
 //LESSON 3 - TYPE ANNOTATIONS
+
+// Basic Data Type Annotation
+let apples: number = 5; //typed
+let pineapples = 5; //inferred
+// const apples_: number = true;
+let apples_: number;
+// apples_ = 'Erhan';
+let speed: string = 'fast';
+// let speed_: string = 10.02;
+let nothingMuch: null = null;
+// let nothingMuch_: null = 0;
+let notKnown: undefined = undefined;
+
+// Built-in JS object Annonation
+let now: Date = new Date();
+
+// Array Annotation
+let colors: string[] = ['red', 'green', 'orange'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classe Annotation
+class Car {}
+let care: Car = new Car();
+
+//Object Literals Annonation
+let point: { x: number; y: number } = {
+  // x: 'Er',
+  x: 1,
+  // a: 10,
+  y: 10,
+};
+
+//Arrow Function Annonation
+const logNumber: (i: number) => void = i => {
+  console.log(i);
+};
+logNumber(5);
+// logNumber('Ernie');
+
+//Inference
+let samples;
+samples = 5; //any - no type inference
+
+//When to use annotations
+//1.Function that returns the 'any' type
+const json = '{"x":10, "y":20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
+//2.When we declare a variable on one line and intialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+//3.Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
+
+//LESSON 4 - ANNOTATIONS WITH FUNCTIONS AND OBJECTS
