e6d14c4f0ad4691e794338609f1affd39501571c
Erhan Ertem <eertem@gmail.com>
2023-03-28 13:24:18 +0300

    Incorporate images for each card component

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/App.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/App.js
@@ -1,15 +1,15 @@
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
-import cortonaImage from './images/cortana.png';
+import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
     </div>
   );
 }
--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/ProfileCard.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/ProfileCard.js
@@ -1,10 +1,11 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
   // const title = props.title;
   // const handle = props.handle;
   // const { title, handle } = props;
 
   return (
     <div>
+      <img src={image} alt="product logo" />
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
