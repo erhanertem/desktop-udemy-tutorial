3bbfc65477e910a9a61414cc92579c00dbe30f8b
Erhan Ertem <eertem@gmail.com>
2023-03-28 12:50:00 +0300

    Add props destructuring

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/App.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/App.js
@@ -3,7 +3,10 @@ import ProfileCard from './ProfileCard';
 function App() {
   return (
     <div>
+      <div>Personal Digital Assistants</div>
       <ProfileCard title="Alexa" handle="@alexa99" />
+      <ProfileCard title="Cortana" handle="@cortana32" />
+      <ProfileCard title="Siri" handle="@siri01" />
     </div>
   );
 }
--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/ProfileCard.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/ProfileCard.js
@@ -1,8 +1,12 @@
-function ProfileCard(props) {
+function ProfileCard({ title, handle }) {
+  // const title = props.title;
+  // const handle = props.handle;
+  // const { title, handle } = props;
+
   return (
     <div>
-      <div>Title is {props.title}</div>
-      <div>Handle is {props.handle}</div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
     </div>
   );
 }
