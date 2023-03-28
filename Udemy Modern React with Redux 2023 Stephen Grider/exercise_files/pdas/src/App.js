e6acdfadf847b2ae464b711696966dbd6a954186
Erhan Ertem <eertem@gmail.com>
2023-03-28 14:37:36 +0300

    Finalize section 3 - react&redux - Stephen Grider

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/App.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/App.js
@@ -7,10 +7,42 @@ import SiriImage from './images/siri.png';
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was created by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was created by Apple and is being phased out."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/ProfileCard.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pdas/src/ProfileCard.js
@@ -1,13 +1,22 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   // const title = props.title;
   // const handle = props.handle;
   // const { title, handle } = props;
 
   return (
-    <div>
-      <img src={image} alt="product logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="product logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
