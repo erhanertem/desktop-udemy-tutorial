34c7062389ec6784621b83c43fb464d54caadd30
Erhan Ertem <eertem@gmail.com>
2023-03-29 16:03:27 +0300

    Continue  section 3 - react&redux - Stephen Grider - Establish state and event handlers

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/animals/src/AnimalShow.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/animals/src/AnimalShow.js
@@ -1,5 +1,33 @@
-function AnimalShow() {
-  return <div>Cow!!!</div>;
+import { useState } from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+  bird, // bird: bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <img src={svgMap[type]} alt="animal" />
+      <img src={heart} alt="heart" style={{ width: 10 + 10 * clicks + 'px' }} />
+    </div>
+  );
 }
 
 export default AnimalShow;
--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/animals/src/App.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/animals/src/App.js
@@ -1,18 +1,26 @@
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)]; //Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
     <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Add Animal
-      </button>
-      <div>Number of Animals: {count}</div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
