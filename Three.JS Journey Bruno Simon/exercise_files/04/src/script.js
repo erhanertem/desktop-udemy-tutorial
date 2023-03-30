496f8d51abe0b782ace44f28f5534ae8e1e20d79
Erhan Ertem <eertem@gmail.com>
2023-03-30 16:37:40 +0300

    Finalize Section 1-5 - three.js - Bruno Simon

--- Three.JS Journey Bruno Simon/exercise_files/04/src/script.js
+++ Three.JS Journey Bruno Simon/exercise_files/04/src/script.js
@@ -6,33 +6,60 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 //-->2) Create a mesh/object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-//-> 2.1)Position Object
-// Separate Axis Positioning
-// mesh.position.x = 0.7; //All objects that inherit from Object3D possess these transform properties
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-// Vector set Positioning
-mesh.position.set(0.7, -0.6, 1);
 
-scene.add(mesh); //Add mesh to the scene
+//Create a group of  objects
+const group = new THREE.Group(); //Create a group container
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group); //Add group container to the scene
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -2;
+group.add(cube1); //Add object to the group container
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffbb00 })
+);
+group.add(cube2); //Add object to the group container
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xadab12 })
+);
+cube3.position.x = 2;
+group.add(cube3); //Add object to the group container
+
+// //Create a single object
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+//-> 2.1)Position Object
+// // Separate Axis Positioning
+// // mesh.position.x = 0.7; //All objects that inherit from Object3D possess these transform properties
+// // mesh.position.y = -0.6;
+// // mesh.position.z = 1;
+// // Vector set Positioning
+// mesh.position.set(0.7, -0.6, 1);
+
+// scene.add(mesh); //Add mesh to the scene
 
 //->2.2)Scale Object
-//Separate Axis Scaling
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 1;
-//Vector set Scaling
-mesh.scale.set(2, 0.5, 2);
+// //Separate Axis Scaling
+// // mesh.scale.x = 2;
+// // mesh.scale.y = 0.5;
+// // mesh.scale.z = 1;
+// //Vector set Scaling
+// mesh.scale.set(2, 0.5, 2);
 
 //->2.3)Rotate Object
-//Separate Axis Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x = Math.PI; //half rotation
-mesh.rotation.y = 2 * Math.PI; //full rotation
-// mesh.rotation.z = 0.25 * Math.PI;
+// //Separate Axis Rotation
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.x = Math.PI; //half rotation
+// mesh.rotation.y = 2 * Math.PI; //full rotation
+// // mesh.rotation.z = 0.25 * Math.PI;
 
 //->2.4)Quaternion
 
@@ -53,15 +80,15 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 //->4.1)Position Camera
-camera.position.x = 2;
-camera.position.y = 1;
-camera.position.z = 6;
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 5;
 scene.add(camera); //Add cam to the scene
 // console.log(mesh.position.distanceTo(camera.position));
 
 //->4.2)direct Camera to Look an Object
-camera.lookAt(new THREE.Vector3(1, 1, 1)); //camera view direction - custom vector
-camera.lookAt(mesh.position); //camera view direction - adjusted to an object center
+// camera.lookAt(new THREE.Vector3(1, 1, 1)); //camera view direction - custom vector
+// camera.lookAt(mesh.position); //camera view direction - adjusted to an object center
 
 //-->5) Render the scene to HTML canvas element
 const canvas = document.querySelector('canvas.webgl');
