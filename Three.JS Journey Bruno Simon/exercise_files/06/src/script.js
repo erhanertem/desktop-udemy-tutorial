8fd0ff174e7f2dddddd81867da8d4391abdcd4af
Erhan Ertem <eertem@gmail.com>
2023-03-30 17:02:38 +0300

    Manual way of adjusting computer clock for animation

---
+++ Three.JS Journey Bruno Simon/exercise_files/06/src/script.js
@@ -0,0 +1,52 @@
+import './style.css';
+import * as THREE from 'three';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+//Time
+let time = Date.now();
+
+//Animations
+const tick = () => {
+  //Time - Estimated FPS rate of your computer
+  const currentTime = Date.now();
+  const deltaTime = currentTime - time;
+  time = currentTime;
+  console.log(deltaTime);
+  //Update objects
+  //   mesh.position.x += 0.01;
+  //   mesh.rotation.y += 0.001;
+  mesh.rotation.y += 0.001 * deltaTime; //Adjusted FPS rate per computer performanse to provide equal experience across many machines
+  //Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+tick();
