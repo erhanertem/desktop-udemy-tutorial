fe6ae8d3c6630b4038ac392bb2e11a626eca804c
Erhan Ertem <eertem@gmail.com>
2023-04-02 01:01:17 +0300

    Section 5 - react&redux - Stephen Grider - Establish downward-upward props flow

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pics/src/components/SearchBar.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/pics/src/components/SearchBar.js
@@ -1,5 +1,14 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ onSubmit }) {
+  const handleClick = () => {
+    onSubmit('cars');
+  };
+
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
 }
 
 export default SearchBar;
