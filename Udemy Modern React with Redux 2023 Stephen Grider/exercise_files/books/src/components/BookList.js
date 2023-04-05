bd4b6710f192734930ad04052a7dded79dcaed4a
Erhan Ertem <eertem@gmail.com>
2023-04-05 12:50:12 +0300

    Try context system on the project

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/books/src/components/BookList.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/books/src/components/BookList.js
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map(el => {
     return (
       <BookShow key={el.id} book={el} onDelete={onDelete} onEdit={onEdit} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {value}
+      {renderedBooks}
+    </div>
+  );
 }
 export default BookList;
--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/books/src/index.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/books/src/index.js
@@ -2,8 +2,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BooksContext from './context/books';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+); //NOTE: Sandwich App component within context component so all the app children gets access to this context file
