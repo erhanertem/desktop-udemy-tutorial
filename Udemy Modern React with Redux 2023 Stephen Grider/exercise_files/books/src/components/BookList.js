f3a5d475c41fd4007a907e889688322949f12cb4
Erhan Ertem <eertem@gmail.com>
2023-04-07 10:57:35 +0300

    Refactor booklist to use context

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/books/src/components/BookList.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/books/src/components/BookList.js
@@ -2,20 +2,13 @@ import { useContext } from 'react';
 import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
-function BookList({ books, onDelete, onEdit }) {
-  // const value = useContext(BooksContext);
+function BookList() {
+  const { books } = useContext(BooksContext);
 
   const renderedBooks = books.map(el => {
-    return (
-      <BookShow key={el.id} book={el} onDelete={onDelete} onEdit={onEdit} />
-    );
+    return <BookShow key={el.id} book={el} />;
   });
 
-  return (
-    <div className="book-list">
-      {/* {value} */}
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 export default BookList;
