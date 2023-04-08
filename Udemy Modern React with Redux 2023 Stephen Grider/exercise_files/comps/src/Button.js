86bdbf3e71a8d71164e3b0d7e4bf0e0ae67ad86e
Erhan Ertem <eertem@gmail.com>
2023-04-08 13:58:39 +0300

    Add tailwind CSS

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/comps/src/Button.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/comps/src/Button.js
@@ -10,7 +10,11 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>; //underlying element - actual jsx element
+  return (
+    <button className="px-3 py-1.5 border-2 border-blue-500 bg-blue-500 text-white">
+      {children}
+    </button>
+  ); //underlying element - actual jsx element
 }
 
 Button.propTypes = {
