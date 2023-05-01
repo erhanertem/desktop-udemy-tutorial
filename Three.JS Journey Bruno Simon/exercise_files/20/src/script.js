b5a16b1de25d771e099419665fb45985fea74415
Erhan Ertem <eertem@gmail.com>
2023-05-01 18:17:39 +0300

    Finalize Section 1-20 - three.js - Bruno Simon

--- Three.JS Journey Bruno Simon/exercise_files/20/src/script.js
+++ Three.JS Journey Bruno Simon/exercise_files/20/src/script.js
@@ -1,90 +1,237 @@
-import * as THREE from 'three'
-import * as dat from 'lil-gui'
+import * as THREE from 'three';
+import * as dat from 'lil-gui';
+import gsap from 'gsap';
 
 /**
  * Debug
  */
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 const parameters = {
-    materialColor: '#ffeded'
-}
+  materialColor: '#ffeded',
+};
 
-gui
-    .addColor(parameters, 'materialColor')
+gui.addColor(parameters, 'materialColor').onChange(() => {
+  {
+    particlesMaterial.color.set(parameters.materialColor);
+    material.color.set(parameters.materialColor);
+  }
+});
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+// /**
+//  * Test cube
+//  */
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// );
+// scene.add(cube);
 
 /**
- * Test cube
+ * Objects
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-scene.add(cube)
+//Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
+gradientTexture.magFilter = THREE.NearestFilter;
 
+// Material
+const material = new THREE.MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+});
+// Meshes
+const objectsDistance = 4;
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
+const mesh3 = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+  material
+);
+
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
+
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
+
+scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
+
+/**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
+
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10; //X
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 -
+    Math.random() * objectsDistance * sectionMeshes.length; //Y
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10; //Z
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+);
+
+//Material
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
+
+//Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
+// Group
+//NOTE - In order to make parallax effect and site scrolling simultaneous animation without each getting into their way encapsulate camera in a group and provide seperation of concerns.
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 6
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 6;
+cameraGroup.add(camera);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+  alpha: true,
+});
+// renderer.setClearAlpha(1); //same as assigning alpha property false in WEbGLRenderer object
+// renderer.setClearAlpha(0); //same as assigning alpha property true in WEbGLRenderer object
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY; //Get the intiial Y scroll position value
+let currentSection = 0;
+
+window.addEventListener('scroll', () => {
+  scrollY = window.scrollY; //Get the new value of Y scroll position
+  //   console.log(scrollY);
+
+  const newSection = Math.round(scrollY / sizes.height);
+  //   console.log(newSection);
+
+  if (newSection != currentSection) {
+    currentSection = newSection;
+    // console.log('changed', currentSection);
+    // console.log(sectionMeshes);
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: 'power2.inOut',
+      x: '+=6',
+      y: '+=3',
+      z: '+=1.5',
+    });
+  }
+});
+
+/**
+ * Cursor - Parallax Effect
+ */
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener('mousemove', event => {
+  // console.log(event.clientX, event.clientY);
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+  //   console.log(cursor);
+});
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
+let previousTime = 0;
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  //Deltatime to standardtize the experience amongst many nonmatching frame rate experiences
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-    // Render
-    renderer.render(scene, camera)
+  //Animate Camera
+  cameraGroup.position.y = (-scrollY / sizes.height) * objectsDistance;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  const parallaxX = cursor.x;
+  const parallaxY = -cursor.y;
+  camera.position.x += (parallaxX - camera.position.x) * 5 * deltaTime;
+  camera.position.y += (parallaxY - camera.position.y) * 5 * deltaTime;
 
-tick()
+  //Animate Meshes
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
+  }
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
