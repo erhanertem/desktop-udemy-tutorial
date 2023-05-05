4a35d6011231231cc551acd05a64497fefae39d6
Erhan Ertem <eertem@gmail.com>
2023-05-05 16:24:01 +0300

    Fix modal tab issue

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/comps/src/components/Modal.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/comps/src/components/Modal.js
@@ -24,10 +24,10 @@ function Modal({ onClose, children, actionBar }) {
         className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
       <div className="fixed inset-40 p-10 bg-white">
-        <div className="flex flex-col justify-between h-full">
+        <dialog className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
-        </div>
+        </dialog>
       </div>
     </div>,
     document.querySelector('.modal-container')
