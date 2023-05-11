8f1d4620b9f92dbd3fb623a39847ea0a5193a96c
Erhan Ertem <eertem@gmail.com>
2023-05-11 15:28:52 +0300

    Initialize Section 20 - react&redux - Stephen Grider - Using React+Redux project setup

---
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/cars/src/App.js
@@ -0,0 +1,17 @@
+import CarForm from './components/CarForm';
+import CarValue from './components/CarValue';
+import CarList from './components/CarList';
+import CarSearch from './components/CarSearch';
+
+function App() {
+  return (
+    <div>
+      <CarForm />
+      <CarSearch />
+      <CarList />
+      <CarValue />
+    </div>
+  );
+}
+
+export default App;
