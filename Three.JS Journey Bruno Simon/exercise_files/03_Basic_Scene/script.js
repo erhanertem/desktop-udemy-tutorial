b6db8190c201b390dcadc87d7b213203576fccfd
Erhan Ertem <eertem@gmail.com>
2023-05-12 10:34:07 +0300

    Rename three.js exercise folders

---
+++ Three.JS Journey Bruno Simon/exercise_files/03_Basic_Scene/script.js
@@ -0,0 +1,31 @@
+// console.log(THREE);
+
+//1) Create a scene
+const scene = new THREE.Scene();
+
+//2) Create a mesh/object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh); //Add mesh to the scene
+
+//3) Create a camera
+const sizes = { width: 800, height: 600 };
+const camera = new THREE.PerspectiveCamera(
+  75, //fov
+  sizes.width / sizes.height //aspect ratio
+  // 0.1, // near
+  // 1000 // far
+);
+camera.position.z = 5;
+camera.position.x = 2;
+camera.position.y = -1;
+scene.add(camera); //Add cam to the scene
+
+//4) Render the scene to HTML canvas element
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  canvas, // canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
