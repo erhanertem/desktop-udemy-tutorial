f159b8fd49b289667a63a5750c1d241a77045f03
Erhan Ertem <eertem@gmail.com>
2023-05-17 13:07:59 +0300

    Section 21 - react&redux - Stephen Grider - save before loading state fix#1 with multiple useStates

--- Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/media/src/components/UsersList.js
+++ Udemy Modern React with Redux 2023 Stephen Grider/exercise_files/media/src/components/UsersList.js
@@ -1,35 +1,61 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'; //import useDispatch to call thunk action
-import { fetchUsers } from '../store'; //import thunk to fetch data
-import { addUser } from '../store';
+import { fetchUsers, addUser } from '../store'; //import thunk to fetch data
 import Button from './Button';
 import Skeleton from './Skeleton';
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setCreatingUserError] = useState(null);
+
   const dispatch = useDispatch();
 
   //->Access BIG state objects and desructure its variables
-  const { isLoading, data, error } = useSelector(state => {
+  const { data } = useSelector(state => {
     return state.users; // {data:[], isLoading:false, error:null}
   });
 
   //->Dispatch current data inside the db using async thunk @ start only!
   useEffect(() => {
-    dispatch(fetchUsers()); //thunk requires useDispatch to run...
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap() //NOTE: returns the promise from the dispatch function properly to continue with then and catch functions.Its buiult into dispatch(fetchUsers()) - see console.log message
+      // .then(() => {
+      //   // console.log('SUCCESS');
+      //   // setIsLoadingUsers(false);
+      // })
+      .catch(err => {
+        // console.log('FAILED');
+        setLoadingUsersError(err);
+        // setIsLoadingUsers(false);
+      })
+      .finally(() => {
+        setIsLoadingUsers(false);
+      }); //thunk requires useDispatch to run...
+    // console.log(
+    //   '🚀 | file: UsersList.js:24 | useEffect | dispatch:',
+    //   dispatch(fetchUsers())
+    // );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   //Eventhandlers
   const handleUserAdd = () => {
-    dispatch(addUser()); //thunk requires useDispatch to run ...
+    setIsCreatingUser(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch(err => setCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false)); //thunk requires useDispatch to run ...
   };
 
   //If loading true display....
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
   //If error fetching display...
-  if (error) {
+  if (loadingUsersError) {
     return <div> Error Fetching Data...</div>;
   }
   //If success fetching display...
@@ -46,7 +72,12 @@ function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        <Button onClick={handleUserAdd}>+ Add User</Button>
+        {isCreatingUser ? (
+          'Creating User....'
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && 'Error creating user...'}
       </div>
       {renderedUsers}
     </div>
