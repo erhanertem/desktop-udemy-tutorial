987f7ff8d6b331a2ae8752eb821d3f50b5d48c55
Erhan Ertem <eertem@gmail.com>
2023-05-24 16:43:45 +0300

    Customize tw project setup and configuration files

--- Udemy Tailwind CSS From Scratch  Learn By Building Projects Brad Traversy/exercise_files/tailwind-starter-template/tailwind.config.js
+++ Udemy Tailwind CSS From Scratch  Learn By Building Projects Brad Traversy/exercise_files/tailwind-starter-template/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: '32rem',
+      },
+    },
   },
   plugins: [],
 };
