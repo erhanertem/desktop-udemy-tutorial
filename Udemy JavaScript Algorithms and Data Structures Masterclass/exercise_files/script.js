5ad9b18b5e97ebf8ed57c2adbb5c0a5583c6b531
Erhan Ertem <eertem@gmail.com>
2023-06-08 15:03:26 +0300

    Finalize section 5 - Algorithms and Data Structures Colt Steele

--- Udemy JavaScript Algorithms and Data Structures Masterclass/exercise_files/script.js
+++ Udemy JavaScript Algorithms and Data Structures Masterclass/exercise_files/script.js
@@ -83,91 +83,313 @@
 //  * forEach/map/filter/reduce/... - O(N)
 //  */
 
-// LESSON 4 - PROBLEM SOLVING APPROACH
+// // LESSON 4 - PROBLEM SOLVING APPROACH
 
-/**
- * ALGORITHM IS A PROCESS OR SET OF STEPS TO ACCOMPLISH A CERTAIN TASK
- * 1.DEVISE A PLAN FOR SOLVING PROBLEMS
- * 2.MASTER COMMON PROBLEM SOLVING PATTERNS
- *
- * PROBLEM SOLVING:
- * 1.UNDERSTAND THE PROBLEM
- *
- * What are the expected inputs?
- * What are the expected outputs?
- * Can I derive an output just from the provided inputs, do I have enough information to solve the problem?
- * How should I label the importnat pieces of data that are part of the problem?
- *
- * 2.EXPLORE CONRETE EXAMPLES
- *
- * Try to make examples of different inputs, edge cases to make the solution fool-proof.
- *
- * 3.BREAKE IT DOWN
- *
- * List actual steps to approach the problem.
- *
- * 4.SOLVE/SIMPLIFY
- *
- * Break down the solutions into several consequtive solutions
- * Find the core difficulty in what you're trying to do
- * Temporarily ignore that difficulty
- * Write a simplified solution
- * Then incorporate that difficulty back in
- *
- * 5.LOOK BACK AND REFACTOR
- *
- * Can you check the result?
- * Can you derive the result differently?
- * Can you understand the code @ first glance?
- * Can you imprive the performance of the code?
- * Can you apply the method to another problem?
- * Can you think other ways of refactor?
- * How others solved the same or similar problem even in a different language setting?
- *
- */
+// /**
+//  * ALGORITHM IS A PROCESS OR SET OF STEPS TO ACCOMPLISH A CERTAIN TASK
+//  * 1.DEVISE A PLAN FOR SOLVING PROBLEMS
+//  * 2.MASTER COMMON PROBLEM SOLVING PATTERNS
+//  *
+//  * PROBLEM SOLVING:
+//  * 1.UNDERSTAND THE PROBLEM
+//  *
+//  * What are the expected inputs?
+//  * What are the expected outputs?
+//  * Can I derive an output just from the provided inputs, do I have enough information to solve the problem?
+//  * How should I label the importnat pieces of data that are part of the problem?
+//  *
+//  * 2.EXPLORE CONRETE EXAMPLES
+//  *
+//  * Try to make examples of different inputs, edge cases to make the solution fool-proof.
+//  *
+//  * 3.BREAKE IT DOWN
+//  *
+//  * List actual steps to approach the problem.
+//  *
+//  * 4.SOLVE/SIMPLIFY
+//  *
+//  * Break down the solutions into several consequtive solutions
+//  * Find the core difficulty in what you're trying to do
+//  * Temporarily ignore that difficulty
+//  * Write a simplified solution
+//  * Then incorporate that difficulty back in
+//  *
+//  * 5.LOOK BACK AND REFACTOR
+//  *
+//  * Can you check the result?
+//  * Can you derive the result differently?
+//  * Can you understand the code @ first glance?
+//  * Can you imprive the performance of the code?
+//  * Can you apply the method to another problem?
+//  * Can you think other ways of refactor?
+//  * How others solved the same or similar problem even in a different language setting?
+//  *
+//  */
 
-function charCount(str) {
-  const obj = {};
-  for (let char of str) {
-    char = char.toLowerCase();
-    if (/[a-z0-9]/.test(char)) {
-      obj[char] = ++obj[char] || 1;
+// function charCount(str) {
+//   const obj = {};
+//   for (let char of str) {
+//     char = char.toLowerCase();
+//     if (/[a-z0-9]/.test(char)) {
+//       obj[char] = ++obj[char] || 1;
+//     }
+//   }
+//   return obj;
+// }
+
+// charCount('yelloqqqw');
+// console.log(
+//   "🚀 | file: script.js:141 | charCount('yelloqqqw')",
+//   charCount('yelloqqqw')
+// );
+
+// function charCount2(str) {
+//   const obj = {};
+//   for (let char of str) {
+//     char = char.toLowerCase();
+//     if (isAlphaNumeric(char)) {
+//       obj[char] = ++obj[char] || 1;
+//     }
+//   }
+//   return obj;
+// }
+
+// function isAlphaNumeric(char) {
+//   let code = char.charCodeAt(0);
+//   if (
+//     !(code > 47 && code < 58) &&
+//     !(code > 64 && code < 91) &&
+//     !(code > 96 && code < 123)
+//   ) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// charCount2('yelloqqqw');
+// console.log(
+//   "🚀 | file: script.js:141 | charCount('yelloqqqw')",
+//   charCount2('yelloqqqw')
+// );
+
+// LESSON 5 - PROBLEM SOLVING PATTERNS
+
+// -->FREQUENCY COUNTER PATTERN
+
+function same(arr, chkArr) {
+  const termArr = [...chkArr];
+  if (arr.length !== termArr.length) {
+    console.log('arr lenghts do not match!');
+    return;
+  }
+  for (const element of arr) {
+    const valueSeek = Math.pow(element, 2);
+    if (termArr.includes(valueSeek)) {
+      const index = termArr.indexOf(valueSeek);
+      termArr.splice(index, 1);
     }
   }
-  return obj;
-}
-
-charCount('yelloqqqw');
-console.log(
-  "🚀 | file: script.js:141 | charCount('yelloqqqw')",
-  charCount('yelloqqqw')
-);
-
-function charCount2(str) {
-  const obj = {};
-  for (let char of str) {
-    char = char.toLowerCase();
-    if (isAlphaNumeric(char)) {
-      obj[char] = ++obj[char] || 1;
-    }
+  if (termArr.length === 0) {
+    console.log('All checks!');
+  } else {
+    console.log('Not a perfect match');
   }
-  return obj;
 }
 
-function isAlphaNumeric(char) {
-  let code = char.charCodeAt(0);
-  if (
-    !(code > 47 && code < 58) &&
-    !(code > 64 && code < 91) &&
-    !(code > 96 && code < 123)
-  ) {
+// same([1, 2, 3], [1, 9]);
+// same([1, 2, 3], [4, 1, 9]);
+// same2([1, 2, 3, 2], [4, 1, 4, 9]);
+
+function same2(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = ++frequencyCounter1[val] || 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = ++frequencyCounter2[val] || 1;
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
   }
   return true;
 }
 
-charCount2('yelloqqqw');
+// Anagram Challange
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return console.log('Letter count mismatch!!!');
+  }
+  let countStr1 = {};
+  let countStr2 = {};
+  for (let letter of str1.toLowerCase()) {
+    countStr1[letter] = ++countStr1[letter] || 1;
+  }
+  for (let letter of str2.toLowerCase()) {
+    countStr2[letter] = ++countStr2[letter] || 1;
+  }
+  console.log(countStr1);
+  console.log(countStr2);
+  for (let key in countStr1) {
+    if (!(key in countStr2)) {
+      return false;
+    }
+    if (countStr1[key] !== countStr2[key]) {
+      return false;
+    }
+  }
+  return console.log('Anagram is 100% match');
+}
+// validAnagram('zaz', 'zza');
+
+function validAnagram2(str1, str2) {
+  if (str1.length !== str2.length) {
+    return console.log('Letter count mismatch!!!');
+  }
+  const lookup = {};
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return console.log('Anagram is 100% match');
+}
+// validAnagram2('za', 'zza');
+
+// -->MULTIPLE POINTERS PATTERN
+
+const result = [];
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        result.push([arr[i], arr[j]]);
+      }
+    }
+  }
+}
+// sumZero([-4, -3, -2, -1, 0, 1, 2, 5, 10]);
+// console.log(result);
+
+const result2 = [];
+function sumZero2(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      result2.push([arr[left], arr[right]]);
+      sum > 0 ? right-- : left++;
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+// sumZero2([-4, -3, -2, -1, 0, 1, 2, 5, 10]);
+// console.log(result2);
+
+// COUNT UNIQUE VALUES CHALLANGE
+function countUniqueValues(arr) {
+  let i = 0;
+  let j = 1;
+  while (j < arr.length) {
+    if (arr[j] === arr[i]) {
+      j++;
+    } else {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return console.log(arr.splice(0, i + 1).length);
+}
+
+// countUniqueValues([1, 1, 1, 2, 3, 3, 4, 4, 5, 6]);
+// countUniqueValues([]);
+
+// const newUnique = [...new Set([1, 1, 1, 2, 3, 3, 4, 4, 5, 6])].length;
+// console.log(newUnique);
+
+// -->SLIDING WINDOW PATTERN
+
+function maxSubarraySum(arr, groupof) {
+  if (groupof > arr.length) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - groupof + 1; i++) {
+    tempSum = 0;
+    for (let j = 0; j < groupof; j++) {
+      tempSum += arr[i + j];
+    }
+    if (tempSum > max) {
+      max = tempSum;
+    }
+  }
+  return console.log(max);
+}
+// maxSubarraySum([1, 2, 5, 6, 7, 8], 2);
+
+function maxSubarraySum2(arr, groupof) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < groupof) return null;
+  for (let i = 0; i < groupof; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = groupof; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - groupof] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+maxSubarraySum2([1, 2, 5, 6, 7, 8], 2);
 console.log(
-  "🚀 | file: script.js:141 | charCount('yelloqqqw')",
-  charCount2('yelloqqqw')
+  '🚀 | file: script.js:366 | maxSubarraySum2([1, 2, 5, 6, 7, 8], 2)',
+  maxSubarraySum2([1, 2, 5, 6, 7, 8], 2)
+);
+
+// -->DIVIDE & CONQUER PATTERN
+function search(arr, val) {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currEl = arr[middle];
+
+    if (currEl < val) {
+      min = middle + 1;
+    } else if (currEl > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
+
+search([4, 1, 1, 2, 23, 42, 1], 1);
+console.log(
+  '🚀 | file: script.js:392 | search([4, 1, 1, 2, 23, 42, 1], 1)',
+  search([4, 1, 1, 2, 23, 42, 1], 1)
 );
