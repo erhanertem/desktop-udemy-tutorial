177d6550549aad07938e5578259cd3df872a2993
Erhan Ertem <eertem@gmail.com>
2023-06-11 19:19:04 +0300

    Finalize section 6 - Algorithms and Data Structures Colt Steele

--- Udemy JavaScript Algorithms and Data Structures Masterclass/exercise_files/script.js
+++ Udemy JavaScript Algorithms and Data Structures Masterclass/exercise_files/script.js
@@ -1,14 +1,16 @@
+'use strict';
+
 // // LESSON 2 - BIG O NOTATION
 
 // // NOTE BIG-O NOTATION IS USED TO ANALYZE THE PERFORMANCE OF AN ALGORITHM. BIG-O NOTATION GIVES US A HIGH LEVEL UNDERSTANDING OF THE TIME OR SPACE COMPLEXITY OF AN ALGORITHM.
 
-// function addUpTo1(n) {
-//   let total = 0;
-//   for (let i = 1; i <= n; i++) {
-//     total += i;
-//   }
-//   return total;
-// }
+function addUpTo1(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
 
 // // console.log('🚀 | file: script.js:12 | addUpTo(6):', addUpTo1(6));
 // var t1 = performance.now();
@@ -16,9 +18,9 @@
 // var t2 = performance.now();
 // console.log(`Time Elapsed: ${t2 - t1} mlseconds.`);
 
-// function addUpTo2(n) {
-//   return (n * (n + 1)) / 2;
-// }
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
 
 // // console.log('🚀 | file: script.js:12 | addUpTo(6):', addUpTo2(6));
 // var t1 = performance.now();
@@ -31,14 +33,14 @@
 // /** NOTES
 //  * 1. ARITHMETIC OPERATIONS ARE CONSTANT TIME
 //  * 2. VARIABLE ASSIGNMENTS ARE CONSTANT TIME
-//  * 3. ACCESSIGN ELEMENTS IN AN ARRAY VIA INDEX OR OBJECTS VIA KEYS ARE CONSTANT TIME
+//  * 3. ACCESSING ELEMENTS IN AN ARRAY VIA INDEX OR OBJECTS VIA KEYS ARE CONSTANT TIME
 //  * 4. LOOPS ARE N COMPLEXITY TIME
 //  * 5. CASCADED LOOPS ARE N * WHATEVER INNER LOOPS COMPLEXITY TIME
 //  * /
 // //->SPACE COMPLEXITY
 // // Auxillary space complexity is the space required by the algorithm, exclusive of the space taken up by the inputs.
 // /** NOTES
-//  * 1. BOOLEANS, NMUMBERS,UNDEFINMED,NULL ARE CONSTANT SPACE
+//  * 1. BOOLEANS,NUMBERS,UNDEFINED,NULL ARE CONSTANT SPACE
 //  * 2. STRINGS REQUIRE O(N) SPACE
 //  * 3. REFERENCE TYPES LIKE ELEMENTS IN AN ARRAY VIA INDEX OR OBJECTS VIA KEYS REQUIRE O(N) SPACE
 //  */
@@ -52,11 +54,11 @@
 //  * ACCESS O(1)
 //  */
 
-// let instructor = {
-//   firstName: 'Temmy',
-//   isInstructor: true,
-//   favoriteNumbers: [1, 2, 3, 4, 5],
-// };
+let instructor = {
+  firstName: 'Temmy',
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3, 4, 5],
+};
 
 // console.log('🚀 | file: script.js:62 ', Object.entries(instructor)); //O(N)
 // console.log('🚀 | file: script.js:62 ', Object.keys(instructor)); //O(N)
@@ -126,16 +128,16 @@
 //  *
 //  */
 
-// function charCount(str) {
-//   const obj = {};
-//   for (let char of str) {
-//     char = char.toLowerCase();
-//     if (/[a-z0-9]/.test(char)) {
-//       obj[char] = ++obj[char] || 1;
-//     }
-//   }
-//   return obj;
-// }
+function charCount(str) {
+  const obj = {};
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
 
 // charCount('yelloqqqw');
 // console.log(
@@ -143,28 +145,28 @@
 //   charCount('yelloqqqw')
 // );
 
-// function charCount2(str) {
-//   const obj = {};
-//   for (let char of str) {
-//     char = char.toLowerCase();
-//     if (isAlphaNumeric(char)) {
-//       obj[char] = ++obj[char] || 1;
-//     }
-//   }
-//   return obj;
-// }
+function charCount2(str) {
+  const obj = {};
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (isAlphaNumeric(char)) {
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
 
-// function isAlphaNumeric(char) {
-//   let code = char.charCodeAt(0);
-//   if (
-//     !(code > 47 && code < 58) &&
-//     !(code > 64 && code < 91) &&
-//     !(code > 96 && code < 123)
-//   ) {
-//     return false;
-//   }
-//   return true;
-// }
+function isAlphaNumeric(char) {
+  let code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+}
 
 // charCount2('yelloqqqw');
 // console.log(
@@ -362,11 +364,11 @@ function maxSubarraySum2(arr, groupof) {
   }
   return maxSum;
 }
-maxSubarraySum2([1, 2, 5, 6, 7, 8], 2);
-console.log(
-  '🚀 | file: script.js:366 | maxSubarraySum2([1, 2, 5, 6, 7, 8], 2)',
-  maxSubarraySum2([1, 2, 5, 6, 7, 8], 2)
-);
+// maxSubarraySum2([1, 2, 5, 6, 7, 8], 2);
+// console.log(
+//   '🚀 | file: script.js:366 | maxSubarraySum2([1, 2, 5, 6, 7, 8], 2)',
+//   maxSubarraySum2([1, 2, 5, 6, 7, 8], 2)
+// );
 
 // -->DIVIDE & CONQUER PATTERN
 function search(arr, val) {
@@ -388,8 +390,228 @@ function search(arr, val) {
   return -1;
 }
 
-search([4, 1, 1, 2, 23, 42, 1], 1);
-console.log(
-  '🚀 | file: script.js:392 | search([4, 1, 1, 2, 23, 42, 1], 1)',
-  search([4, 1, 1, 2, 23, 42, 1], 1)
-);
+// search([4, 1, 1, 2, 23, 42, 1], 1);
+// console.log(
+//   '🚀 | file: script.js:392 | search([4, 1, 1, 2, 23, 42, 1], 1)',
+//   search([4, 1, 1, 2, 23, 42, 1], 1)
+// );
+
+// LESSON 6 - OPTIONAL CHALLANGES
+
+// ->Challange #3
+function sameFrequency(n1, n2) {
+  const str1 = n1.toString();
+  const str2 = n2.toString();
+
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const arr1 = Array.from(str1);
+  const arr2 = Array.from(str2);
+  for (const el of arr1) {
+    if (arr2.includes(el)) {
+      const index = arr2.indexOf(el);
+      arr2.splice(index, 1);
+    }
+  }
+  if (arr2.length === 0) {
+    return true;
+  } else return false;
+}
+
+// const res = sameFrequency(82, 281);
+// console.log('🚀 | file: script.js:426 | res', res);
+
+function sameFrequency2(n1, n2) {
+  let str1 = n1.toString();
+  let str2 = n2.toString();
+  if (str1.length !== str2.length) return false;
+
+  let count1 = {};
+  let count2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    count1[str1[i]] = (count1[str1[i]] || 0) + 1;
+  }
+
+  for (let j = 0; j < strNum1.length; j++) {
+    count2[str2[j]] = (count2[str2[j]] || 0) + 1;
+  }
+
+  for (let key in count1) {
+    if (count1[key] !== count2[key]) return false;
+  }
+
+  return true;
+}
+
+// ->Challange #4
+// A.Frequency Counter Solution
+function areThereDuplicates(...arg) {
+  const count = {};
+  const arr = [...arg];
+
+  for (const el of arr) {
+    count[el] = ++count[el] || 1;
+  }
+
+  for (const key in count) {
+    if (count[key] > 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// B.Multiple Pointer Solution
+function areThereDuplicates2(...args) {
+  // Two pointers
+  args.sort((a, b) => a - b);
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true;
+    }
+    start++;
+    next++;
+  }
+  return false;
+}
+
+// const res = areThereDuplicates2(1, 3, 2, 1);
+// console.log(res);
+
+// ->Challange #5
+function averagePair(arr, targetAvg) {
+  if (arr.length < 2) return false;
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let avg = (arr[left] + arr[right]) / 2;
+    console.log(arr[left], arr[right], avg);
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg < targetAvg) {
+      left++;
+    } else right--;
+  }
+  return false;
+}
+
+// const res = averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+// console.log(res);
+
+// ->Challange #6
+function isSubsequence(str1, str2) {
+  if (str1.length > str2.length || !str2) {
+    return false;
+  }
+  if (!str1) return true;
+
+  let start1 = 0;
+  let start2 = 0;
+
+  while (start1 < str1.length) {
+    if (start2 > str2.length) {
+      return false;
+    }
+    if (str1[start1] === str2[start2]) {
+      start1++;
+      start2++;
+    } else {
+      start2++;
+    }
+  }
+  return true;
+}
+
+function isSubsequence2(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+// const res = isSubsequence2('hee hee', 'hee  hee');
+// console.log(res);
+
+// ->Challange #7
+
+function maxSubarraySum3(arr, groupof) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < groupof) return null;
+  for (let i = 0; i < groupof; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = groupof; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - groupof] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+// const res = maxSubarraySum3([1, 4, 2, 10, 23, 3, 1, 0, 20], 4);
+// console.log(res);
+
+// ->Challange #8
+
+function minSubarrayLen(arr, targetSum) {
+  let total = 0;
+  let startIndex = 0;
+  let endIndex = 0;
+  let minLength = Infinity;
+
+  while (startIndex < arr.length) {
+    //if current window doesn't add up to the given sum then move the window to the right
+    if (total < targetSum && endIndex < arr.length) {
+      total += arr[endIndex];
+      endIndex++;
+    }
+    //if current window adds up to at least the sum given then we can shrink the window
+    else if (total >= targetSum) {
+      minLength = Math.min(minLength, endIndex - startIndex);
+      total -= arr[startIndex];
+      startIndex++;
+    }
+    //Current total less than required total but we reach the end, need this or else we will be in an infinite loop.
+    else {
+      break;
+    }
+  }
+  return minLength === Infinity ? 0 : minLength;
+}
+
+// const res = minSubarrayLen([2, 1, 6, 5, 4], 9);
+// console.log(res);
+
+// ->Challange #8
+
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+    console.log(seen);
+  }
+  return longest;
+}
+
+const res = findLongestSubstring('thisisawesome');
+console.log(res);
