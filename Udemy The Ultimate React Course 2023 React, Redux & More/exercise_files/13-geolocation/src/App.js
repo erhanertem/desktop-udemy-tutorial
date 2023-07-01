8bf268b9f2332bd95406fcdc195491d2ae54af62
Erhan Ertem <eertem@gmail.com>
2023-07-01 13:25:20 +0300

    Finalize section 13 - The Ultimate React Course 2023 Jonas Schmedtmann

---
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/13-geolocation/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { useGeolocation } from './useGeoLocation'
+
+export default function App() {
+	const [countClicks, setCountClicks] = useState(0)
+	const {
+		position: { lat, lng }, //destructuring at destructure
+		isLoading,
+		error,
+		getPosition,
+	} = useGeolocation()
+
+	function handleClick() {
+		setCountClicks(count => count + 1)
+		getPosition()
+	}
+
+	return (
+		<div>
+			<button onClick={handleClick} disabled={isLoading}>
+				Get my position
+			</button>
+
+			{isLoading && <p>Loading position...</p>}
+			{error && <p>{error}</p>}
+			{!isLoading && !error && lat && lng && (
+				<p>
+					Your GPS position:{' '}
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+					>
+						{lat}, {lng}
+					</a>
+				</p>
+			)}
+
+			<p>You requested position {countClicks} times</p>
+		</div>
+	)
+}
