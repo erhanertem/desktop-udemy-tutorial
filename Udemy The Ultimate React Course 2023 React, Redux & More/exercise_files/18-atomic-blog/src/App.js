9f64a96123d013ecd94b62fd587fa4dd065d287c
Erhan Ertem <eertem@gmail.com>
2023-07-13 18:41:31 +0300

    Section 19 - The Ultimate React Course 2023 Jonas Schmedtmann - Optimization with children

--- Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/18-atomic-blog/src/App.js
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/18-atomic-blog/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
 // import { PostProvider, PostContext } from './PostContext'
 import { PostProvider, usePosts } from './PostContext'
+import Test from './Test'
 
 function createRandomPost() {
 	return {
@@ -138,14 +139,17 @@ function List() {
 	const { posts } = usePosts()
 
 	return (
-		<ul>
-			{posts.map((post, i) => (
-				<li key={i}>
-					<h3>{post.title}</h3>
-					<p>{post.body}</p>
-				</li>
-			))}
-		</ul>
+		<>
+			<ul>
+				{posts.map((post, i) => (
+					<li key={i}>
+						<h3>{post.title}</h3>
+						<p>{post.body}</p>
+					</li>
+				))}
+			</ul>
+			<Test />
+		</>
 	)
 }
 
