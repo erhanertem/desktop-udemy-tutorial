38cd2b7862917649cb1d1214d4205cf2a825c9a0
Erhan Ertem <eertem@gmail.com>
2023-07-15 23:58:30 +0300

    Add action creators

--- Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/store.js
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/store.js
@@ -1,11 +1,13 @@
 import { createStore } from 'redux'
 
+//REDUCER INITIAL STATE
 const initialState = {
 	balance: 0,
 	loan: 0,
 	loanPurpose: '',
 }
 
+//CREATE A REDUCER
 function reducer(state = initialState, action) {
 	switch (action.type) {
 		case 'account/deposit':
@@ -35,18 +37,46 @@ function reducer(state = initialState, action) {
 	}
 }
 
+//CREATE REDUX STORE
 const store = createStore(reducer)
 
-store.dispatch({ type: 'account/deposit', payload: 500 })
-console.log(store.getState())
-store.dispatch({ type: 'account/withdraw', payload: 200 })
-console.log(store.getState())
-store.dispatch({
-	type: 'account/requestLoan',
-	payload: { amount: 1200, purpose: 'car financing' },
-})
-console.log(store.getState())
-store.dispatch({ type: 'account/payLoan' })
-console.log(store.getState())
+// -> #1. TYPE IN ACTION OBJECT INSIDE DISPATCH DIRECTLY...PRONE TO ERROR
+// store.dispatch({ type: 'account/deposit', payload: 500 })
+// console.log(store.getState())
+// store.dispatch({ type: 'account/withdraw', payload: 200 })
+// console.log(store.getState())
+// store.dispatch({
+// 	type: 'account/requestLoan',
+// 	payload: { amount: 1200, purpose: 'car financing' },
+// })
+// console.log(store.getState())
+// store.dispatch({ type: 'account/payLoan' })
+// console.log(store.getState())
 
-console.log('Hey Redux')
+// console.log('Hey Redux')
+
+// -> #2. SETUP ACTION CREATORS - DESIGNATED FUNCTIONS TO BE CALLED BY DISPATCH
+function deposit(amount) {
+	return { type: 'account/deposit', payload: amount }
+}
+function withdraw(amount) {
+	return { type: 'account/withdraw', payload: amount }
+}
+function requestLoan(amount, purpose) {
+	return {
+		type: 'account/requestLoan',
+		payload: { amount, purpose },
+	}
+}
+function payLoan() {
+	return { type: 'account/payLoan' }
+}
+
+store.dispatch(deposit(1000))
+console.log(store.getState())
+store.dispatch(withdraw(500))
+console.log(store.getState())
+store.dispatch(requestLoan(500, 'car loan'))
+console.log(store.getState())
+store.dispatch(payLoan())
+console.log(store.getState())
