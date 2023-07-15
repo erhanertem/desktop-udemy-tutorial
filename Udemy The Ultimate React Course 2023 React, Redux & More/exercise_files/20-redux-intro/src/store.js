42f50c0aeda4bf494ebce4b319eb68fd130a2e4c
Erhan Ertem <eertem@gmail.com>
2023-07-15 23:21:45 +0300

    Create a redux store

--- Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/store.js
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux'
+
 const initialState = {
 	balance: 0,
 	loan: 0,
@@ -15,7 +17,12 @@ function reducer(state = initialState, action) {
 			return { ...state, balance: state.balance - action.payload }
 		case 'account/requestLoan':
 			if (state.loan > 0) return state
-			return { ...state, loan: action.payload }
+			return {
+				...state,
+				loan: action.payload.amount,
+				loanPurpose: action.payload.purpose,
+				balance: state.balance + action.payload.amount,
+			}
 		case 'account/payLoan':
 			return {
 				...state,
@@ -27,3 +34,19 @@ function reducer(state = initialState, action) {
 			return state
 	}
 }
+
+const store = createStore(reducer)
+
+store.dispatch({ type: 'account/deposit', payload: 500 })
+console.log(store.getState())
+store.dispatch({ type: 'account/withdraw', payload: 200 })
+console.log(store.getState())
+store.dispatch({
+	type: 'account/requestLoan',
+	payload: { amount: 1200, purpose: 'car financing' },
+})
+console.log(store.getState())
+store.dispatch({ type: 'account/payLoan' })
+console.log(store.getState())
+
+console.log('Hey Redux')
