2a30a9c8fe0c43eeb6cd4075ccec81e11a4c9237
Erhan Ertem <eertem@gmail.com>
2023-07-16 16:38:18 +0300

    useDispatch to start an action creator to update a state

--- Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/index.js
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux' // Provider wraps App component to transfer the states to the component and its children
+import store from './store'
+
 import './index.css'
 import App from './App'
 
-import store from './store'
-
-store.dispatch({ type: 'account/deposit', payload: 250 })
-console.log(store.getState())
+// store.dispatch({ type: 'account/deposit', payload: 250 })
+// console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
