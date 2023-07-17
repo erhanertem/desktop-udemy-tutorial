d6faa417eec8e7ea30077985a0c9d8b4992ab595
Erhan Ertem <eertem@gmail.com>
2023-07-17 19:07:56 +0300

    Import redux-devtools

--- Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/store.js
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/store.js
@@ -2,6 +2,8 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 
 import thunk from 'redux-thunk'
 
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 //IMPORT REDUCERS
 import accountReducer from './features/accounts/accountSlice'
 import customerReducer from './features/customers/customerSlice'
