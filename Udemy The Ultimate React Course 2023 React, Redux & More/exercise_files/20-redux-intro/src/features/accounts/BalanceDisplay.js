af7dfe7b72ff45761d3fcf9f1172089c6d478f92
Erhan Ertem <eertem@gmail.com>
2023-07-17 17:43:48 +0300

    Implement the legacy way of connecting components to redux

--- Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/features/accounts/BalanceDisplay.js
+++ Udemy The Ultimate React Course 2023 React, Redux & More/exercise_files/20-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,22 @@
+import { connect } from 'react-redux'
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
-  }).format(value);
+	return new Intl.NumberFormat('en', {
+		style: 'currency',
+		currency: 'USD',
+	}).format(value)
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+	return <div className="balance">{formatCurrency(balance)}</div>
 }
 
-export default BalanceDisplay;
+//LEGACY WAY OF CONNECTING COMPONENTS TO REDUX....NOWADAYS NOT USED!!!!
+
+function mapStateToProps(state) {
+	return {
+		balance: state.account.balance,
+	}
+}
+
+export default connect(mapStateToProps)(BalanceDisplay)
