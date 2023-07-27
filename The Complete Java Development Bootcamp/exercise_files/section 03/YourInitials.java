b380b6ae6ebe02daa10f13e43a2bdfded6626d05
Erhan Ertem <eertem@gmail.com>
2023-07-27 12:26:25 +0300

    Complete section 1 thru 3

---
+++ The Complete Java Development Bootcamp/exercise_files/section 03/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println(" SSS     A  ");
+         System.out.println("S   S   A A ");
+         System.out.println("S      A   A");
+         System.out.println(" SSS   AAAAA");
+         System.out.println("    S  A   A");
+         System.out.println("S   S  A   A");
+         System.out.println(" SSS   A   A");
+    }
+}
