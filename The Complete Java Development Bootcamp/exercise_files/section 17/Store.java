74207e862ebd517444b6f2b7c877ca3a1c6c8604
Erhan Ertem <eertem@gmail.com>
2023-08-10 19:41:25 +0300

    Udemy The Complete Java BootCamp -  Finalize section 17

---
+++ The Complete Java Development Bootcamp/exercise_files/section 17/Store.java
@@ -0,0 +1,26 @@
+public class Store {
+  Movie[] movies;
+
+  public Store() {
+    this.movies = new Movie[10];
+  }
+
+  public Movie getMovie(int index) {
+    return new Movie(movies[index]);
+  }
+
+  public void setMovie(int index, Movie movie) {
+    this.movies[index] = new Movie(movie);
+  }
+
+  public String toString() {
+    String temp = "";
+    for (Movie movie : movies) {
+      if (movie != null) {
+        temp += movie.toString() + "\n";
+      }
+    }
+    return temp;
+  }
+
+}
