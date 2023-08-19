ac9253099930582c99b732174cec9f72d2ba81a1
Erhan Ertem <eertem@gmail.com>
2023-08-19 23:07:31 +0300

    Udemy One Week Python - Finalize Section 5

---
+++ Udemy One Week Python/exercise_files/exercises.py
@@ -0,0 +1,29 @@
+# LESSON 5
+# twith_handler = "Arabic"
+# twith_handler = '@OPUS'
+# print(twith_handler)
+# type(34)
+# type("Aremaic")
+# type("")
+# print('She said "hi"')
+# print('She said "hi"')
+
+# age = "eighty-five"
+# age = 85
+# print(type(age))
+
+# state = "hello" + " world"
+# print(state)
+
+# last_name = "Tammy"
+# first_name = "Horsemann"
+# full_name = first_name + " " + last_name
+# print(full_name[0])
+
+# print("4" + 5)
+# print("4" * 5)
+
+# "hello"[3]
+
+# Same as null
+user = None
