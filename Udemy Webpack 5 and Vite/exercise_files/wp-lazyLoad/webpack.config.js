93b68d3bb925867588f8f0daaf0d653a1c2ca24a
Erhan Ertem <eertem@gmail.com>
2023-08-25 19:34:21 +0300

    Udemy Webpack 5 and Vite - Finalize Section1.14

--- Udemy Webpack 5 and Vite/exercise_files/wp-lazyLoad/webpack.config.js
+++ Udemy Webpack 5 and Vite/exercise_files/wp-lazyLoad/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const html = require('html-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -7,12 +8,20 @@ module.exports = {
   optimization: {
     chunkIds: 'named',
     // chunkIds: 'deterministic',
-
     splitChunks: {
       minSize: 2000, // bytes
       chunks: 'all', //OPTIONS ARE async, all, initial - SPECIFY CHUNKS TO BE FURTHER SPLIT. FILES NEEDS TO HAVE DEPENDENCIES (imports) TO BE FURTHER SPLIT INTO CHUNKS.
     },
   },
+  plugins: [
+    new html({
+      filename: 'index.html',
+      minify: false, //true under prod. mode
+      inject: 'body', //default head
+      title: 'by htmlWebpackPlugin', //custom title name for the page
+      template: './tpl.html',
+    }),
+  ], //THIS FIELD ALLOWS MULTIPLE PLUGINS INSTALLATION
   output: {
     asyncChunks: true, // default to true - IF YOU WANT TO TURN LAZY LOADING OFF MAKE IT FALSE
     clean: true,
