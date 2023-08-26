6cf7474c4235edffa56e9001d9e21162a160f9eb
Erhan Ertem <eertem@gmail.com>
2023-08-26 14:51:19 +0300

    Udemy Webpack 5 and Vite - Finalize Section1.17

---
+++ Udemy Webpack 5 and Vite/exercise_files/wp-asset_modules/src/page.js
@@ -0,0 +1,10 @@
+import London from './assets/London.png'
+let img = document.getElementById('img')
+img.src = London
+
+import Txt from './assets/London.txt'
+let p = document.getElementById('p')
+p.innerText = Txt
+
+import bg from './assets/bg.svg'
+document.body.style.backgroundImage = `url(${bg})`
