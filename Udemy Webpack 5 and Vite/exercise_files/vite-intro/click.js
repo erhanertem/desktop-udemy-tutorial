4faa749068263bfccab926d29841023ab13802f6
Erhan Ertem <eertem@gmail.com>
2023-08-27 15:14:43 +0300

    Udemy Webpack 5 and Vite - Finalize Section2.28

---
+++ Udemy Webpack 5 and Vite/exercise_files/vite-intro/click.js
@@ -0,0 +1,8 @@
+import './lazzy.scss';
+
+export default function () {
+  let p = document.createElement('p');
+  p.setAttribute('id', 'lazy');
+  p.innerText = 'Lazy Loading';
+  document.body.append(p);
+}
