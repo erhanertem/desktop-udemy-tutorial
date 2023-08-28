acade7ae30d02070bc6faa761254d087d174af90
Erhan Ertem <eertem@gmail.com>
2023-08-28 17:07:09 +0300

    Udemy Webpack 5 and Vite - Finalize Section2.44

--- Udemy Webpack 5 and Vite/exercise_files/wp-module-federation/remote_data_provider/webpack.config.js
+++ Udemy Webpack 5 and Vite/exercise_files/wp-module-federation/remote_data_provider/webpack.config.js
@@ -1,30 +1,31 @@
 //REMOTE
-const path = require('path')
-// -->MODULE FEDERATION PLUGIN COMES FROM WEBPACK WITH .CONTAINER APPEND. CONTAINER REFERS TO A PROJECT WHOSE MAIN JOB IS TO PROVIDE DATA TO OTHERS. 
-const { ModuleFederationPlugin } = require('webpack').container
+const path = require('path');
+// -->MODULE FEDERATION PLUGIN COMES FROM WEBPACK WITH .CONTAINER APPEND. CONTAINER REFERS TO A PROJECT WHOSE MAIN JOB IS TO PROVIDE DATA TO OTHERS.
+const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: './entry.js',
-  output: {
-    clean: true,
-    path:path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    port:4444
-  },
-  //MAMP SERVER: localhost:80/remote
-  plugins: [
-    // -->INSTANTIATE THE MODULEFEDERATION PLUGIN
-    new ModuleFederationPlugin({
-      name: 'remote_container',  //NOTE: DECLARES A GLOBAL VARIABLE WHICH IS A IIFE FOR THE BUNDLED FILE.
-      filename: 'remoteEntry.js', //INTIAL BUNDLED CHUNK
-      exposes: { //REST OF THE CHUNKS 
-        './info': './info.js', 
-        './createElem': './createElem.js',
-        './entry': './entry.js'
-      }
-    })
-  ]
-
-}
+	context: path.resolve(__dirname, 'src'),
+	entry: './entry.js',
+	output: {
+		clean: true,
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		port: 4444,
+	},
+	//MAMP SERVER: localhost:80/remote
+	plugins: [
+		// -->INSTANTIATE THE MODULEFEDERATION PLUGIN
+		new ModuleFederationPlugin({
+			name: 'remote_container', //NOTE: DECLARES A GLOBAL VARIABLE WHICH IS A IIFE FOR THE BUNDLED FILE.
+			filename: 'remoteEntry.js', //INTIAL BUNDLED CHUNK
+			exposes: {
+				//REST OF THE CHUNKS
+				'./info': './info.js',
+				'./createElem': './createElem.js',
+				'./entry': './entry.js',
+			},
+			shared: ['sample_npm_pkg'], //NAME THE NPM PACKAGE INSTALLED ON BOTH LOCAL~REMOTE
+		}),
+	],
+};
