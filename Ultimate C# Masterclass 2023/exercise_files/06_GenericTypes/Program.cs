9082747e64b571616a01af9cae66c4eab057f19c
Erhan Ertem <eertem@gmail.com>
2023-09-20 21:19:10 +0300

    Ultimate C# Masterclass for 2023 - Finalize Section 6 Project

--- Ultimate C# Masterclass 2023/exercise_files/06_GenericTypes/Program.cs
+++ Ultimate C# Masterclass 2023/exercise_files/06_GenericTypes/Program.cs
@@ -875,29 +875,29 @@
 
 // //> CACHING
 
-var dataDownloader = new SlowDataDownloader();
+// var dataDownloader = new SlowDataDownloader();
 
-System.Console.WriteLine(dataDownloader.DownloadData("id1"));
-System.Console.WriteLine(dataDownloader.DownloadData("id2"));
-System.Console.WriteLine(dataDownloader.DownloadData("id3"));
-System.Console.WriteLine(dataDownloader.DownloadData("id1"));
-System.Console.WriteLine(dataDownloader.DownloadData("id3"));
-System.Console.WriteLine(dataDownloader.DownloadData("id1"));
-System.Console.WriteLine(dataDownloader.DownloadData("id2"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id1"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id2"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id3"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id1"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id3"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id1"));
+// System.Console.WriteLine(dataDownloader.DownloadData("id2"));
 
-Console.ReadKey();
+// Console.ReadKey();
 
-public interface IDataDownloader
-{
-    string DownloadData(string resourceId);
-}
-public class SlowDataDownloader : IDataDownloader
-{
-    public string DownloadData(string resourceId)
-    {
-        //let's imagine this method downloads real data, 
-        //and it does it slowly
-        Thread.Sleep(1000);
-        return $"Some data for {resourceId}";
-    }
-}
+// public interface IDataDownloader
+// {
+//     string DownloadData(string resourceId);
+// }
+// public class SlowDataDownloader : IDataDownloader
+// {
+//     public string DownloadData(string resourceId)
+//     {
+//         //let's imagine this method downloads real data, 
+//         //and it does it slowly
+//         Thread.Sleep(1000);
+//         return $"Some data for {resourceId}";
+//     }
+// }
--- Ultimate C# Masterclass 2023/exercise_files/CustomCash/Program.cs
+++ Ultimate C# Masterclass 2023/exercise_files/CustomCash/Program.cs
@@ -1,5 +1,7 @@
 ﻿
-var dataDownloader = new SlowDataDownloader();
+// IDataDownloader dataDownloader = new SlowDataDownloader();
+
+IDataDownloader dataDownloader = new CachingDataDownloader(new PrintingDataDownloader(new SlowDataDownloader()));
 
 System.Console.WriteLine(dataDownloader.DownloadData("id1"));
 System.Console.WriteLine(dataDownloader.DownloadData("id2"));
@@ -11,11 +13,64 @@ System.Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
+public class Cache<TKey, TData>
+{
+    private readonly Dictionary<TKey, TData> _cachedData = new();
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+    {
+        if (!_cachedData.ContainsKey(key))
+        {
+            _cachedData[key] = getForTheFirstTime(key);
+        }
+        return _cachedData[key];
+    }
+}
+
 public interface IDataDownloader
 {
     string DownloadData(string resourceId);
 }
-public class SlowDataDownloader : IDataDownloader
+
+public class PrintingDataDownloader : IDataDownloader //BOTH IMPLEMENTS THE SAME INTERFACE CLASS
+{
+    //INTERNALIZE THE CLASS OF INTEREST
+    private readonly IDataDownloader _dataDownloader;
+
+    //CLASS CONSTRUCTOR 
+    public PrintingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    //IMPLEMENT INTERFACE FUNCTION DOWNLOADDATA()
+    public string DownloadData(string resourceId)
+    {
+        var data = _dataDownloader.DownloadData(resourceId);
+        System.Console.WriteLine("Data is ready! ");
+        return data;
+    }
+}
+
+public class CachingDataDownloader : IDataDownloader //BOTH IMPLEMENTS THE SAME INTERFACE CLASS
+{
+    //INTERNALIZE THE CLASS OF INTEREST
+    private readonly IDataDownloader _dataDownloader;
+    private readonly Cache<string, string> _cache = new();
+
+    //CLASS CONSTRUCTOR 
+    public CachingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    //IMPLEMENT INTERFACE FUNCTION DOWNLOADDATA()
+    public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+
+public class SlowDataDownloader : IDataDownloader //BOTH IMPLEMENTS THE SAME INTERFACE CLASS
 {
     public string DownloadData(string resourceId)
     {
