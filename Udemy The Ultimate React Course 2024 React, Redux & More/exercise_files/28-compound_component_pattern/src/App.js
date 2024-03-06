7acf31d56ca0a021667b2b10897ac2e3a88f3b13
Erhan Ertem <eertem@gmail.com>
2024-03-07 00:33:16 +0300

    Establish a CC Pattern

--- Udemy The Ultimate React Course 2024 React, Redux & More/exercise_files/28-compound_component_pattern/src/App.js
+++ Udemy The Ultimate React Course 2024 React, Redux & More/exercise_files/28-compound_component_pattern/src/App.js
@@ -5,14 +5,33 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
+
+      <div>
+        <Counter>
+          <Counter.Label>My super flexible counter</Counter.Label>
+          <Counter.Increase icon="+" />
+          <Counter.Decrease icon="-" />
+          <Counter.Count />
+        </Counter>
+      </div>
+
+      <div>
+        <Counter>
+          <Counter.Label>My other super flexible counter</Counter.Label>
+          <div>
+            <Counter.Increase icon="+" />
+          </div>
+          <Counter.Count />
+        </Counter>
+      </div>
     </div>
   );
 }
--- Udemy The Ultimate React Course 2024 React, Redux & More/exercise_files/28-compound_component_pattern/src/Counter.js
+++ Udemy The Ultimate React Course 2024 React, Redux & More/exercise_files/28-compound_component_pattern/src/Counter.js
@@ -1,4 +1,40 @@
-function Counter() {
-  return <span>Counter</span>;
+import { createContext, useContext, useState } from "react";
+
+//> #1.Create a context
+const CounterContext = createContext();
+
+//> #2.Create a Parent Component
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  const increase = () => setCount((count) => ++count);
+  const decrease = () => setCount((count) => --count);
+
+  return (
+    <CounterContext.Provider value={{ count, increase, decrease }}>
+      <span>{children}</span>
+    </CounterContext.Provider>
+  );
 }
+
+//> #3.Create Child Components to help implement the common tasks
+function Count() {
+  const { count } = useContext(CounterContext);
+  return <span>{count}</span>;
+}
+function Label({ children }) {
+  return <span>{children}</span>;
+}
+function Increase({ icon }) {
+  const { increase } = useContext(CounterContext);
+  return <button onClick={increase}>{icon}</button>;
+}
+function Decrease({ icon }) {
+  const { decrease } = useContext(CounterContext);
+  return <button onClick={decrease}>{icon}</button>;
+}
+//> #4.Add Child Components as Custom properties to Parent component
+Counter.Count = Count;
+Counter.Label = Label;
+Counter.Increase = Increase;
+Counter.Decrease = Decrease;
 export default Counter;
