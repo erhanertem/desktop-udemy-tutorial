626029e15e359a15767358b9df0a570f11c09b29
Erhan Ertem <eertem@gmail.com>
2024-03-15 22:45:30 +0300

    Handle Post requests along with express json parser

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/08_express-project/server.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/08_express-project/server.js
@@ -18,14 +18,17 @@ const friends = [
 ];
 
 // > REGISTER A MIDDLEWARE
+// We want this middleware @ the top of all middlewares because we want the timer to capture as much time as possible of the process.
 app.use(function (req, res, next) {
   const start = Date.now();
-
   next();
   // actions go here.... MEANING RETURNING RES LAST VISITS HERE...
   const delta = Date.now() - start;
   console.log(`${req.method} ${req.url} , it took ${delta}ms`);
 });
+// > REGISTER A MIDDLEWARE THAT PARSES(DESTRINGIFY) THE JSON DATA PASSED INTO REQ FLOW TO A JS OBJECT
+// USEFULL FOR POST REQUEST
+app.use(express.json());
 
 // > GET REQUEST /friends endpoint
 app.get('/friends', (req, res) => {
@@ -49,6 +52,24 @@ app.get('/friends/:friendId', (req, res) => {
   } else {
     res.status(404).json({ error: 'Friend does not exist' });
   }
+});
+
+// > POST REQUEST /friends
+app.post('/friends', (req, res) => {
+  //GUARD CLAUSE
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Missing friend name' });
+  }
+
+  //json format object is passed from sender to the server thru req
+  const newFriend = {
+    id: friends.length,
+    name: req.body.name,
+  };
+  friends.push(newFriend);
+
+  //SENDS NEWFRIEND AS JSON STRING AS A RESPONSE
+  res.json(newFriend);
 });
 
 // > GET REQUEST /messages endpoint
