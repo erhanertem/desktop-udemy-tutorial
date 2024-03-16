b02fb7f8a5bc32ac6f55b4be62e5cb606be6dc58
Erhan Ertem <eertem@gmail.com>
2024-03-16 17:13:53 +0300

    Sending file via express

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/08_express-project/controllers/messages.controller.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/08_express-project/controllers/messages.controller.js
@@ -1,5 +1,22 @@
+const path = require('node:path');
+// /folder/files.jpg (@mac) \folder\files.jpg (@windows)
+
 function getMessages(_req, res) {
-  res.send('<ul><li>Hello Einstein</li></ul>');
+  const filePath = path.join(
+    __dirname,
+    '..',
+    'public',
+    'images',
+    'skimountain.jpg'
+  );
+  // __dirname is the NodeJS global variable that gets the name of the folder where this messages.controller.js module lives
+  // .. is required as we want to go up from here
+  // to dial into 'public' folder
+  // which has the file named ....
+  // FINAL PATH would look like this --> C:\CODING\REPO ARCHIEVE\udemy\Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie\exercise_files\08_express-project\public\images\skimountain.jp
+
+  res.sendFile(filePath); //BUT WHERE IS THIS FILE LOCATED....WE NEED TO PROVIDE THE ABSOLUTE PATH OF THIS FILE BEFORE SENDING IT....IN ORDER TO FIND THIS FILE WE CAN MAKE USE OF BUILT-IN NODEJS MODULE CALLED PATH. @ https://nodejs.org/en/learn/manipulating-files/nodejs-file-paths
+  // res.send('<ul><li>Hello Einstein</li></ul>');
 }
 
 function postMessage(_req, _res) {
