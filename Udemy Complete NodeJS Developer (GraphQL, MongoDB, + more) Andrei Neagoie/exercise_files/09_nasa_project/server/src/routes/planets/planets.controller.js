a0a21afaf003d61a4d2d8fdd10c750bf32e03f71
Erhan Ertem <eertem@gmail.com>
2024-03-21 13:29:45 +0300

    Construe separation of concern for planets data

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/planets/planets.controller.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
 const { planets } = require('../../models/planets.model');
 
-function getAllPlanets(req, res) {
+function httpGetAllPlanets(req, res) {
   return res.status(200).json(planets); //IMPORTANT!! RETURN THINGS TO BE EXPLICIT OF WHAT THIS FUNCTION DOES
 }
 
-module.exports = { getAllPlanets };
+module.exports = { httpGetAllPlanets };
