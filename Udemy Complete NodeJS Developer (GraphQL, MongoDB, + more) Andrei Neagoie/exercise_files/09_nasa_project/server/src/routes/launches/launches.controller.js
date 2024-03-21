9eb7ed7180d98e7ea493e0b1897891efb4ea61ee
Erhan Ertem <eertem@gmail.com>
2024-03-22 00:41:09 +0300

    Establish validation checks on POST /launches material

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/launches/launches.controller.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/launches/launches.controller.js
@@ -14,7 +14,31 @@ function httpGetAllLaunches(_req, res) {
 function httpAddNewLaunch(req, res) {
   //NOTE!! since @app we have used  express.json() MIDDLEWARE , any incoming req body is parsed to JS object that we can make use of.
   const launch = req.body;
+
+  //GUARD CLAUSE - REQUIRED INPUTS VALIDATION
+  // If any of these properties missing, we don not wish to register this input and send 4xx CLIENT induced error
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({ error: 'Missing required launch property' });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  // GUARD CLAUSE - DATE VALIDATION
+  // Option #1 - Check if entry is a date
+  // Explanation: Invalid date returns date.valueOf() as NaN. If isNaN checks true, its an invalid date, else it returns a UNISYS number of date which renders isNaN false.
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({ error: 'Invalid launch date' });
+  }
+  // // Option #2 - Check if entry is a date
+  // const date = new Date("Jan 121212, 2000") is an invalid entry and date.valueOf() returns Invalid Date response object.
+  // if (launch.launchDate.toString() === 'Invalid Date') {
+  //   return res.status(400).json({ error: 'Invalid launch date' });
+  // }
+
   addNewLaunch(launch);
 
   res.status(201).json(launch);
