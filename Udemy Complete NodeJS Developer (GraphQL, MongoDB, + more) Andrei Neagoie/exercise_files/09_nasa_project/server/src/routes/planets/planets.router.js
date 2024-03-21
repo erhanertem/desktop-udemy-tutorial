7d4f142c7e5a649352199a301be367b7af65751c
Erhan Ertem <eertem@gmail.com>
2024-03-21 23:52:47 +0300

    Apply route simplification fix to planets router per changes on app.js planetsRouter middleware

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/planets/planets.router.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/planets/planets.router.js
@@ -4,6 +4,8 @@ const { httpGetAllPlanets } = require('./planets.controller');
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', httpGetAllPlanets);
+// planetsRouter.get('/planets', httpGetAllPlanets);
+// NOTE!! /planets BECOMES REPITATIVE. @app.js we can add this route to planetsRouter middleware so that we do not need to specify it here.
+planetsRouter.get('/', httpGetAllPlanets);
 
 module.exports = planetsRouter;
