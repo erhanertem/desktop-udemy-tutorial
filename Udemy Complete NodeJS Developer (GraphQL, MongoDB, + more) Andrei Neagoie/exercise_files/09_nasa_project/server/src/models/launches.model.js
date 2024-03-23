249141753451e7a685ff6d4f758403efe68c77eb
Erhan Ertem <eertem@gmail.com>
2024-03-23 18:53:36 +0300

    Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Sections 9 - Finalize - DELETE/launches backend with frontend

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/models/launches.model.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/models/launches.model.js
@@ -3,35 +3,49 @@ const launches = new Map();
 let latestFlightNumber = 100;
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customer: ['ZTM', 'NASA'],
-    upcoming: true,
-    success: true,
+  flightNumber: 100,
+  mission: 'Kepler Exploration X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('December 27, 2030'),
+  target: 'Kepler-442 b',
+  customer: ['ZTM', 'NASA'],
+  upcoming: true,
+  success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
-    return Array.from(launches.values()); //Transforms map array into an array
+  return Array.from(launches.values()); //Transforms map array into an array
 }
 
 function addNewLaunch(launch) {
-    latestFlightNumber++;
-    launches.set(
-        latestFlightNumber,
-        Object.assign(launch, {
-            upcoming: true,
-            customers: ['Zero To Mastery', 'NASA'],
-            flightNumber: latestFlightNumber,
-        })
-    );
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      upcoming: true,
+      customers: ['Zero To Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+function abortLaunchById(launchId) {
+  // launches.delete(launchId);
+  // INSTEAD OF DELETING THE LAUNCH WE MARK IT AS ABORTED
+  const abortedLaunch = launches.get(launchId);
+  abortedLaunch.upcoming = false;
+  abortedLaunch.success = false;
+  return abortedLaunch;
 }
 
 module.exports = {
-    getAllLaunches,
-    addNewLaunch,
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
 };
--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/launches/launches.router.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/launches/launches.router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 } = require('./launches.controller');
 
 const launchesRouter = express.Router();
@@ -12,5 +13,6 @@ const launchesRouter = express.Router();
 // NOTE!! /launches BECOMES REPITATIVE. @app.js we can add this route to launcgesRouter middleware so that we do not need to specify it here.
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.delete('/:id', httpAbortLaunch); // express param syntax ':'
 
 module.exports = launchesRouter;
