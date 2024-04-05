854cd0b02d8f1a1bbee4a9ec0282def779f2b39c
Erhan Ertem <eertem@gmail.com>
2024-04-05 17:17:19 +0300

    Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Sections 14 -  Starter - Provide basic server setup for security testing

---
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/14_security-example/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+  return res.send('Your personal value is 42!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
