c6e39322810271fa114902f479ea656ab134f045
Erhan Ertem <eertem@gmail.com>
2024-04-09 12:26:38 +0300

    Secure secrets, assign OAUTH config object and setup preliminary OAUTH flow

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/14_security-example/server.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/14_security-example/server.js
@@ -5,14 +5,39 @@ const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
 
+require('dotenv').config();
+
 const PORT = 3000;
 
 const app = express();
 
 // > SECURITY MIDDLEWARES @ THE VERY TOP
+// NOTE: HELP PROTECT ALL END POINTS THAT COMES AFTER IT
 app.use(helmet());
 
-app.get('/secret', (req, res) => {
+// MIDDLEWARE FUNCTION
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; //TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({ error: 'You must login!' });
+  }
+  next();
+}
+
+// > OAUTH AUHENTICATION
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+// #1. USER LOGIN ENDPOINT - AUTHORIZATION CODE REQUEST TO AUTH GOOGLE
+app.get('/auth/google', (req, res) => {});
+// #2. THIS IS THE ENDPOINT GOOGLE SENDS THE AUTHORIZATION CODE RESPONSE TO THE APP
+app.get('/auth/google/callback', (req, res) => {});
+// #3. USER LOGOUT GENERIC ENDPOINT - USER DES NOT NEED TO PROVIDE ANY DATA
+app.get('/auth/logout', (req, res) => {});
+
+// IN EXPRESS EACH ENDPOINT CAN GET A MIDDLEWARE/MIDDLEWARES AFTER THE ROUTE DEFINITION
+app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal value is 42!');
 });
 
