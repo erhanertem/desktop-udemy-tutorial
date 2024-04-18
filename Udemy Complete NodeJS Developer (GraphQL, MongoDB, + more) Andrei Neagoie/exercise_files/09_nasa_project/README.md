d05f6dba76b4e0a85edb50f27029ef05d5a5c401
Erhan Ertem <eertem@gmail.com>
2024-04-18 12:09:10 +0300

    Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Sections 15 -  Finalize - Implement CI/CD for the Nasa Project

---
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/README.md
@@ -0,0 +1,19 @@
+### 👋 **I am Erhan ERTEM**
+
+&emsp;
+
+## Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie
+
+### **Objective:** Create fullstack app for Nasa Mission Control w/SpaceX Data
+
+&emsp;
+
+#### Link to Project &rarr; [Nasa Mission Control-App](https://...)
+
+![Screenshot](screenshot.gif)
+
+---
+
+![JS](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![nodeJS](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white) ![expressJs](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white) ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAF) ![insomnia](https://img.shields.io/badge/Insomnia-5849be?style=for-the-badge&logo=Insomnia&logoColor=white) ![postman](https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=Postman&logoColor=white) ![handlebars](https://img.shields.io/badge/Handlebars%20js-f0772b?style=for-the-badge&logo=handlebarsdotjs&logoColor=black) ![jest](https://img.shields.io/badge/Jest-C21325?style=for-the-badge&logo=jest&logoColor=white)
+
+&emsp;
--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/launches/launches.test.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/09_nasa_project/server/src/routes/launches/launches.test.js
@@ -1,97 +1,99 @@
-const supertest = require('supertest');
-const app = require('../../app');
-const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const supertest = require("supertest");
+const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
-const API_VERSION = '/v1';
+const API_VERSION = "/v1";
 
-describe('Launches API', () => {
-  // > PROVIDE CONNECTION BEFORE ALL TESTS START
-  beforeAll(async () => {
-    await mongoConnect();
-  });
-  // > PROVIDE DISCONNECTION AFTER ALL TESTS COMPLETED
-  afterAll(async () => {
-    await mongoDisconnect();
-  });
+describe("Launches API", () => {
+	// > PROVIDE CONNECTION BEFORE ALL TESTS START
+	beforeAll(async () => {
+		await mongoConnect();
+		await loadPlanetsData();
+	});
+	// > PROVIDE DISCONNECTION AFTER ALL TESTS COMPLETED
+	afterAll(async () => {
+		await mongoDisconnect();
+	});
 
-  // > SINGLE CASE TESTING
-  describe('Test GET /launches', () => {
-    test('It should respond with 200 success', async () => {
-      const response = await supertest(app)
-        .get(`${API_VERSION}/launches`)
-        .expect('Content-Type', /json/)
-        .expect(200); //via supertest syntax
-      // expect(response.statusCode).toBe(200); // via JEST syntax
-    });
-  });
+	// > SINGLE CASE TESTING
+	describe("Test GET /launches", () => {
+		test("It should respond with 200 success", async () => {
+			const response = await supertest(app)
+				.get(`${API_VERSION}/launches`)
+				.expect("Content-Type", /json/)
+				.expect(200); //via supertest syntax
+			// expect(response.statusCode).toBe(200); // via JEST syntax
+		});
+	});
 
-  // > MULTI CASE TESTING
-  describe('Test POST /launch', () => {
-    const launchDataWithInvalidDate = {
-      mission: 'USS Enterprise',
-      rocket: 'NCC 1701-D',
-      target: 'Kepler-62 f',
-      launchDate: 'Tombi 4, 2028',
-    };
-    const completeLaunchData = {
-      mission: 'USS Enterprise',
-      rocket: 'NCC 1701-D',
-      target: 'Kepler-62 f',
-      launchDate: 'January 4, 2028',
-    };
-    const launchDataWithoutDate = {
-      mission: 'USS Enterprise',
-      rocket: 'NCC 1701-D',
-      target: 'Kepler-62 f',
-    };
+	// > MULTI CASE TESTING
+	describe("Test POST /launch", () => {
+		const launchDataWithInvalidDate = {
+			mission: "USS Enterprise",
+			rocket: "NCC 1701-D",
+			target: "Kepler-62 f",
+			launchDate: "Tombi 4, 2028",
+		};
+		const completeLaunchData = {
+			mission: "USS Enterprise",
+			rocket: "NCC 1701-D",
+			target: "Kepler-62 f",
+			launchDate: "January 4, 2028",
+		};
+		const launchDataWithoutDate = {
+			mission: "USS Enterprise",
+			rocket: "NCC 1701-D",
+			target: "Kepler-62 f",
+		};
 
-    test('It should respond with 201 created', async () => {
-      // SUPERTEST POST FETCH TEST w/supertest functions
-      const response = await supertest(app)
-        .post(`${API_VERSION}/launches`)
-        .send(completeLaunchData)
-        .expect('Content-Type', /json/)
-        .expect(201);
+		test("It should respond with 201 created", async () => {
+			// SUPERTEST POST FETCH TEST w/supertest functions
+			const response = await supertest(app)
+				.post(`${API_VERSION}/launches`)
+				.send(completeLaunchData)
+				.expect("Content-Type", /json/)
+				.expect(201);
 
-      // JEST ASSERTION TESTING
-      // CHECK DATE ENTERED VERSUS RETURNED RESPONSE DATE IN NUMERIC UNIX VALUE
-      const requestDate = new Date(completeLaunchData.launchDate).valueOf();
-      const responseDate = new Date(response.body.launchDate).valueOf();
-      expect(responseDate).toBe(requestDate);
+			// JEST ASSERTION TESTING
+			// CHECK DATE ENTERED VERSUS RETURNED RESPONSE DATE IN NUMERIC UNIX VALUE
+			const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+			const responseDate = new Date(response.body.launchDate).valueOf();
+			expect(responseDate).toBe(requestDate);
 
-      // JEST ASSERTION TESTING
-      // CHECK IF CRIPPLED VERSION OF THE LAUNCH DATA MATCHES THE RESPONSE.BODY OBJECT
-      expect(response.body).toMatchObject(launchDataWithoutDate);
-    });
+			// JEST ASSERTION TESTING
+			// CHECK IF CRIPPLED VERSION OF THE LAUNCH DATA MATCHES THE RESPONSE.BODY OBJECT
+			expect(response.body).toMatchObject(launchDataWithoutDate);
+		});
 
-    test('It should catch missing required properties', async () => {
-      // SUPERTEST POST FETCH TEST w/supertest functions
-      const response = await supertest(app)
-        .post(`${API_VERSION}/launches`)
-        .send(launchDataWithoutDate) //Intentionally sendign an incomplete data
-        .expect('Content-Type', /json/)
-        .expect(400); //via supertest syntax
+		test("It should catch missing required properties", async () => {
+			// SUPERTEST POST FETCH TEST w/supertest functions
+			const response = await supertest(app)
+				.post(`${API_VERSION}/launches`)
+				.send(launchDataWithoutDate) //Intentionally sendign an incomplete data
+				.expect("Content-Type", /json/)
+				.expect(400); //via supertest syntax
 
-      // JEST ASSERTION TESTING
-      // CHECXK FOR STRICT EQUALITY
-      expect(response.body).toStrictEqual({
-        error: 'Missing required launch property',
-      });
-    });
+			// JEST ASSERTION TESTING
+			// CHECXK FOR STRICT EQUALITY
+			expect(response.body).toStrictEqual({
+				error: "Missing required launch property",
+			});
+		});
 
-    test('It should catch invalid dates', async () => {
-      // SUPERTEST POST FETCH TEST w/supertest functions
-      const response = await supertest(app)
-        .post(`${API_VERSION}/launches`)
-        .send(launchDataWithInvalidDate) //Intentionally sendign an incomplete data
-        .expect('Content-Type', /json/)
-        .expect(400); //via supertest syntax
+		test("It should catch invalid dates", async () => {
+			// SUPERTEST POST FETCH TEST w/supertest functions
+			const response = await supertest(app)
+				.post(`${API_VERSION}/launches`)
+				.send(launchDataWithInvalidDate) //Intentionally sendign an incomplete data
+				.expect("Content-Type", /json/)
+				.expect(400); //via supertest syntax
 
-      // JEST ASSERTION TESTING
-      // CHECXK FOR STRICT EQUALITY
-      expect(response.body).toStrictEqual({
-        error: 'Invalid launch date',
-      });
-    });
-  });
+			// JEST ASSERTION TESTING
+			// CHECXK FOR STRICT EQUALITY
+			expect(response.body).toStrictEqual({
+				error: "Invalid launch date",
+			});
+		});
+	});
 });
