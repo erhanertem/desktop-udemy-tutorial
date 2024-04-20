eda057e07eddce3100448b2df306a2fd24a14910
Erhan Ertem <eertem@gmail.com>
2024-04-21 00:41:44 +0300

    Structure a detailed database w/GraphQL

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/17_graphql-example/server.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/17_graphql-example/server.js
@@ -1,10 +1,10 @@
 // EXPRESS MIDDLEWARE OVER NODE
-const express = require('express');
+const express = require("express");
 // GRAPHQL CORE API FOR JS
-const { buildSchema } = require('graphql');
+const { buildSchema } = require("graphql");
 // GRAPHQL-EXPRESS CONNECTOR
 // const { createHandler } = require('graphql-http/lib/use/express');
-const { createYoga, createSchema } = require('graphql-yoga');
+const { createYoga, createSchema } = require("graphql-yoga");
 
 const PORT = 4000;
 
@@ -21,18 +21,70 @@ const PORT = 4000;
 // };
 
 const schema = createSchema({
-  typeDefs: `
-         type Query {
-            description: String
-            price: Float
-         }
-         `,
-  resolvers: {
-    Query: {
-      description: () => 'Red Shoe',
-      price: () => 42.12,
-    },
-  },
+   // Query is the root object ina GraphQL collection.
+   // Product, Review objs are custom type definitions. type! means the property needs to be entered (non-nullable)
+   // ID in capital is a special type provided by GraphQL. While given ids are strings still its being explicit to provide it as ID type to id property
+   typeDefs: `
+				type Query {
+				products: [Product]
+				orders: [Order]
+				}
+
+				type Product {
+				id: ID!
+				description: String!
+				reviews: [Review]
+				price: Float!
+				}
+
+				type Review {
+				rating: Int!
+				comment: String
+				}
+
+				type Order {
+				date: String!
+				subtotal: Float!
+				items: [OrderItem]
+				}
+
+				type OrderItem {
+				product: Product!
+				quantity: Int!
+				}
+			`,
+   resolvers: {
+      Query: {
+         products: () => [
+            {
+               id: "redshoe",
+               description: "Red Shoe",
+               price: 42.12,
+            },
+            {
+               id: "bluejean",
+               description: "Blue Jeans",
+               price: 55.55,
+            },
+         ],
+         orders: () => [
+            {
+               date: "2005-05-05",
+               subtotal: 90.22,
+               items: [
+                  {
+                     product: {
+                        id: "redshoe",
+                        description: "Old Red Shoe",
+                        price: 45.11,
+                     },
+                     quantity: 100,
+                  },
+               ],
+            },
+         ],
+      },
+   },
 });
 
 // -> CREATE EXPRESS HTTP CONNECTION HANDLER FOR THE GRAPHQL SCHEMA
@@ -43,14 +95,14 @@ const schema = createSchema({
 //   //   graphiql: true,
 // });
 const connectionHandler = createYoga({
-  schema,
-  graphiql: true,
+   schema,
+   graphiql: true,
 });
 
 const app = express();
 
-app.use('/graphql', connectionHandler);
+app.use("/graphql", connectionHandler);
 
 app.listen(PORT, () => {
-  console.log(`Running GraphQL server @ PORT ${PORT}...`);
+   console.log(`Running GraphQL server @ PORT ${PORT}...`);
 });
