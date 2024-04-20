17538104565efe707f13855d98494a8ccea1ca4c
Erhan Ertem <eertem@gmail.com>
2024-04-20 14:16:07 +0300

    Alternatively, create a working graphql-yoga express server

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/17_graphql-example/server.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/17_graphql-example/server.js
@@ -3,34 +3,53 @@ const express = require('express');
 // GRAPHQL CORE API FOR JS
 const { buildSchema } = require('graphql');
 // GRAPHQL-EXPRESS CONNECTOR
-const { createHandler } = require('graphql-http/lib/use/express');
+// const { createHandler } = require('graphql-http/lib/use/express');
+const { createYoga, createSchema } = require('graphql-yoga');
 
-const PORT = 3000;
+const PORT = 4000;
 
 // -> CREATE A GRAPHQL SCHEMA
-const schema = buildSchema(`
-type Query {
-   description: String
-   price: Float
-}
-`);
+// const schema = buildSchema(`
+// type Query {
+//    description: String
+//    price: Float
+// }
+// `);
+// const rootValue = {
+//   description: 'Red Shoe',
+//   price: 42.12,
+// };
 
-const rootValue = {
-  description: 'Red Shoe',
-  price: 42.12,
-};
+const schema = createSchema({
+  typeDefs: `
+         type Query {
+            description: String
+            price: Float
+         }
+         `,
+  resolvers: {
+    Query: {
+      description: () => 'Red Shoe',
+      price: () => 42.12,
+    },
+  },
+});
 
 // -> CREATE EXPRESS HTTP CONNECTION HANDLER FOR THE GRAPHQL SCHEMA
-const handler = createHandler({
+// const connectionHandler = createHandler({
+//   schema,
+//   rootValue,
+//   //   Not available property directly on graphql-http
+//   //   graphiql: true,
+// });
+const connectionHandler = createYoga({
   schema,
-  rootValue,
-  //   Not available property directly on graphql-http
-  //   graphiql: true,
+  graphiql: true,
 });
 
 const app = express();
 
-app.use('/graphql', handler);
+app.use('/graphql', connectionHandler);
 
 app.listen(PORT, () => {
   console.log(`Running GraphQL server @ PORT ${PORT}...`);
