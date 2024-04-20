0682e286bf8c665296e0bce3816b7543d0ade62c
Erhan Ertem <eertem@gmail.com>
2024-04-20 13:46:53 +0300

    Create a working graphql-http express server

--- Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/17_graphql-example/server.js
+++ Udemy Complete NodeJS Developer (GraphQL, MongoDB, + more) Andrei Neagoie/exercise_files/17_graphql-example/server.js
@@ -1,7 +1,37 @@
+// EXPRESS MIDDLEWARE OVER NODE
 const express = require('express');
+// GRAPHQL CORE API FOR JS
+const { buildSchema } = require('graphql');
+// GRAPHQL-EXPRESS CONNECTOR
+const { createHandler } = require('graphql-http/lib/use/express');
+
+const PORT = 3000;
+
+// -> CREATE A GRAPHQL SCHEMA
+const schema = buildSchema(`
+type Query {
+   description: String
+   price: Float
+}
+`);
+
+const rootValue = {
+  description: 'Red Shoe',
+  price: 42.12,
+};
+
+// -> CREATE EXPRESS HTTP CONNECTION HANDLER FOR THE GRAPHQL SCHEMA
+const handler = createHandler({
+  schema,
+  rootValue,
+  //   Not available property directly on graphql-http
+  //   graphiql: true,
+});
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Running GraphQL server...');
+app.use('/graphql', handler);
+
+app.listen(PORT, () => {
+  console.log(`Running GraphQL server @ PORT ${PORT}...`);
 });
