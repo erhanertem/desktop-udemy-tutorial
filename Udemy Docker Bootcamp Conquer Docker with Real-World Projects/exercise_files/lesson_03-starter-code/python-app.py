8fc1e04efbb4180ca432d8b53f672ac3a82d1b89
Erhan Ertem <eertem@gmail.com>
2024-04-27 23:51:47 +0300

    Udemy Docker Bootcamp: Conquer Docker with Real-World Projects - Finalize Section 5 - Build images from docker files

---
+++ Udemy Docker Bootcamp Conquer Docker with Real-World Projects/exercise_files/lesson_03-starter-code/python-app.py
@@ -0,0 +1,12 @@
+print("    ____        _   _                  _    ")
+print("   |  _ \\ _   _| |_| |__   ___  _ __  / \\   ")
+print("   | |_) | | | | __| '_ \\ / _ \\| '_ \\/  /   ")
+print("   |  __/| |_| | |_| | | | (_) | | | /\\_/    ")
+print("   |_|    \\__, |\\__|_| |_|\\___/|_| |_(_)    ")
+print("          |___/                             ")
+print("                                            ")
+print("             -- Python --                   ")
+
+# Developer: Captain DevOps, there's a Dockerfile here, but I'm not entirely sure how it works.
+#            I'm confident you can figure it out and make this application set sail smoothly.
+#            The helm is yours!
