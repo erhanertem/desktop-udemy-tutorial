8e623ad88355e5825c60657ecf0730644b34396a
Erhan Ertem <eertem@gmail.com>
2024-05-01 13:26:03 +0300

    Make Prettier beautify in string HTML fragments, add a GET /info endpoint to the button element and the corresponding endpoint in express

--- Udemy HTMX The Practical Guide/exercise_files/Section 02_01-starting-project/app.js
+++ Udemy HTMX The Practical Guide/exercise_files/Section 02_01-starting-project/app.js
@@ -2,38 +2,53 @@ import express from 'express';
 
 import { HTMX_KNOWLEDGE } from './data/htmx-info.js';
 
+/**
+ * html wrapper is needed for prettier formatting
+ */
+const html = String.raw;
+
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>HTMX Essentials</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-          
-        />
-        <link rel="icon" href="/icon.png" />
-        <link rel="stylesheet" href="/main.css" />
-        <script src="/htmx.js" defer></script>
-      </head>
-      <body>
-        <header id="main-header">
-          <img src="/htmx-logo.jpg" alt="HTMX Logo" />
-          <h1>Essentials</h1>
-        </header>
+	res.send(html`
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>HTMX Essentials</title>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+					rel="stylesheet"
+				/>
+				<link rel="icon" href="/icon.png" />
+				<link rel="stylesheet" href="/main.css" />
+				<script src="/htmx.js" defer></script>
+			</head>
+			<body>
+				<header id="main-header">
+					<img src="/htmx-logo.jpg" alt="HTMX Logo" />
+					<h1>Essentials</h1>
+				</header>
 
-        <main>
-          <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <button>Learn More</button>
-        </main>
-      </body>
-    </html>
-  `);
+				<main>
+					<p>
+						HTMX is a JavaScript library that you use without writing
+						JavaScript code.
+					</p>
+					<button hx-get="/info">Learn More</button>
+				</main>
+			</body>
+		</html>
+	`);
+});
+
+app.get('/info', (req, res) => {
+	res.send(html`
+		<ul>
+			${HTMX_KNOWLEDGE.map((info) => `<li>${info}</li>`)}
+		</ul>
+	`);
 });
 
 app.listen(3000);
