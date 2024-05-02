e26d6948a5b15de99be9ca13302d2a42110269b2
Erhan Ertem <eertem@gmail.com>
2024-05-02 09:14:37 +0300

    Udemy HTMX The Practical Guide - Section 2 - Add boilerplate practice project

---
+++ Udemy HTMX The Practical Guide/exercise_files/Section 02_07-practice-start/app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+
+/**
+ * html wrapper is needed for prettier formatting
+ */
+const html = String.raw;
+
+const courseGoals = [];
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.send(html`
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<title>Learn HTMX</title>
+				<link rel="stylesheet" href="/main.css" />
+				<script src="/htmx.js" defer></script>
+			</head>
+			<body>
+				<main>
+					<h1>Manage your course goals</h1>
+					<section>
+						<form id="goal-form">
+							<div>
+								<label htmlFor="goal">Goal</label>
+								<input type="text" id="goal" name="goal" />
+							</div>
+							<button type="submit">Add goal</button>
+						</form>
+					</section>
+					<section>
+						<ul id="goals">
+							${courseGoals.map(
+								(goal, index) => html`
+									<li id="goal-${index}">
+										<span>${goal}</span>
+										<button>Remove</button>
+									</li>
+								`
+							)}
+						</ul>
+					</section>
+				</main>
+			</body>
+		</html>
+	`);
+});
+
+app.listen(3000);
