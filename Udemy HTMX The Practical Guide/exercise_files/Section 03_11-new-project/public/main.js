a1e5c8bed063a9dc49e12e004627029336b5482f
Erhan Ertem <eertem@gmail.com>
2024-05-07 12:01:32 +0300

    Add modal barebone code

--- Udemy HTMX The Practical Guide/exercise_files/Section 03_11-new-project/public/main.js
+++ Udemy HTMX The Practical Guide/exercise_files/Section 03_11-new-project/public/main.js
@@ -1,5 +1,26 @@
-function showConfirmationModal() {
-	console.log('Showing modal...');
+function showConfirmationModal(event) {
+	event.preventDefault();
+	console.log(event);
+
+	// PROVIDE MODAL TEMPLATE
+	const confirmationModal = `
+		<dialog class="modal">
+			<div id="confirmation">
+				<h2>Are you sure?</h2>
+				<p>Do you really want to ACTION this place?</p>
+				<div id="confirmation-actions">
+					<button id="action-no" className="button-text">No</button>
+					<button id="action-yes" className="button">Yes</button>
+				</div>
+			</div>
+		</dialog>
+	`;
+	// INSERT MODAL TO THE DOM
+	document.body.insertAdjacentHTML('beforeend', confirmationModal);
+	// SELECT INSERTED DIALOG MODAL
+	const dialog = document.querySelector('dialog');
+	// MAKE DIALOG ELEMENT VISIBLE
+	dialog.showModal(); // showModal() is a built-in function for dialog element
 }
 
 document
