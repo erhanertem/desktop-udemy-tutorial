8c978bd7fa97502cc0c50e4dc4fc4fc7137b53b0
Erhan Ertem <eertem@gmail.com>
2024-05-09 10:23:07 +0300

    Udemy HTMX The Practical Guide - Finalize Section 4 - swap and retargeting based on BE error status using response-targets HTMX extension

--- Udemy HTMX The Practical Guide/exercise_files/Section 04_01-new-project/app.js
+++ Udemy HTMX The Practical Guide/exercise_files/Section 04_01-new-project/app.js
@@ -31,18 +31,22 @@ app.get('/', (req, res) => {
 				/>
 				<link rel="stylesheet" href="/main.css" />
 				<script src="/htmx.js" defer></script>
+				<script src="/htmx-ext-response_target.js" defer></script>
 			</head>
 			<body>
 				<main>
 					<form
+						hx-ext="response-targets"
 						hx-post="/login"
 						hx-headers='{"x-csrf-token": "abc"}'
-						hx-target="#extra-information"
+						hx-target-422="#extra-information"
+						hx-target-500="#server-side-error"
 						hx-sync="this:replace"
 					>
 						<div>
 							<img src="/images/auth-icon.jpg" alt="A lock icon" />
 						</div>
+						<div id="server-side-error"></div>
 						<div class="control">
 							<label for="email">Email</label>
 							<input
@@ -115,7 +119,7 @@ app.post('/login', (req, res) => {
 	}
 
 	if (Object.keys(errors).length > 0) {
-		res.send(html`
+		res.status(422).send(html`
 			<div id="extra-information">
 				<ul id="form-errors">
 					${Object.keys(errors)
@@ -129,13 +133,21 @@ app.post('/login', (req, res) => {
 	// Simulate random server-side error
 	// NOTE: WE DO NOT WANT TO DISPALY THIS ERROR MESSAGE @ TARGET hx-target="#extra-information" AS SHOWEN ON THE FORM ELEMENT. SO WE NEED RETARGETTING SPECIFICALLY HERE.
 	if (Math.random() > 0) {
-		res.setHeader('HX-Retarget', '.control'); // Select the first element with 'control' class and override Target to this element.
-		res.setHeader('HX-Reswap', 'beforebegin'); // Place it infront of the 'control' class element.
-		return res.send(
-			html`<p class="error">
-				A server-side error has occured. Please try again.
-			</p>`
-		);
+		// --> #1. Select the first element with 'control' class and override Target to this element.
+		// -> #1. via nodejs method
+		// res.setHeader('HX-Retarget', '.control');
+		// -> #2. handle via response-targets extension
+		// --> #2. Place it infront of the 'control' class element.
+		// -> #1. via nodejs method
+		// res.setHeader('HX-Reswap', 'beforebegin');
+		// -> #2. Introduce a new element to target and swap - <div id="server-side-error"></div>
+		return res
+			.status(500)
+			.send(
+				html`<p class="error">
+					A server-side error has occured. Please try again.
+				</p>`
+			);
 	}
 
 	// Once credentials validated, we redirect to authenticated endpoint
