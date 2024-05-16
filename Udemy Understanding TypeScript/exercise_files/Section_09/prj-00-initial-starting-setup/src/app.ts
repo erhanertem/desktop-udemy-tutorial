e51b5da335f8266e2f0baaebd135a1dd4474165b
Erhan Ertem <eertem@gmail.com>
2024-05-16 14:20:50 +0300

    Render a HTML template using OOP approach

--- Udemy Understanding TypeScript/exercise_files/Section_09/prj-00-initial-starting-setup/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/Section_09/prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,30 @@
-// Code goes here!
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+
+	constructor() {
+		// Select container and template fragment
+		this.templateElement = document.getElementById(
+			'project-input'
+		)! as HTMLTemplateElement;
+		this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+		// Make a copy of the selected template fragment to be used later
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		// Select form element inside the copied fragment
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		// Call the private method that inserts a copy of the selected form element inside the pointed container div element
+		this.attach();
+	}
+
+	// Private method for copying the form HTML inside the div container
+	private attach() {
+		this.hostElement.insertAdjacentElement('afterbegin', this.element);
+	}
+}
+
+const prjInput = new ProjectInput();
