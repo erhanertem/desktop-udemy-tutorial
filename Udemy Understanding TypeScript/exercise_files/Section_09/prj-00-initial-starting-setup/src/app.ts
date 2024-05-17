b746b3d6e2742e8cb4af7d2e5a1951ea662abb7c
Erhan Ertem <eertem@gmail.com>
2024-05-17 10:50:38 +0300

    Create one project state manager class with singleton pattern

--- Udemy Understanding TypeScript/exercise_files/Section_09/prj-00-initial-starting-setup/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/Section_09/prj-00-initial-starting-setup/src/app.ts
@@ -1,3 +1,34 @@
+// -> Project State Management Class - Singleton Class (Unique not instantiated more than once!)
+class ProjectState {
+	private projects: any[] = [];
+	private static instance: ProjectState;
+
+	// Private constructor for Singleton class creation
+	private constructor() {}
+
+	static getInstance() {
+		if (this.instance) {
+			return this.instance;
+		}
+		this.instance = new ProjectState();
+		return this.instance;
+	}
+
+	addProject(title: string, description: string, numOfPeople: number) {
+		const newProject = {
+			id: Math.random().toString(),
+			title: title,
+			description: description,
+			people: numOfPeople,
+		};
+		this.projects.push(newProject);
+	}
+}
+
+// Singleton Classes are not instantiated with new Keyword
+// const projectState = new ProjectState();
+const projectState = ProjectState.getInstance();
+
 // Validation Logic
 interface Validatable {
 	value: string | number;
