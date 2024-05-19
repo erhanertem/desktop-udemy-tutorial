2bfd50a4a2a4d7125ac416ffe09c380e171cbcdd
Erhan Ertem <eertem@gmail.com>
2024-05-19 17:36:19 +0300

    Use getter to dynamically change the returned text based on count of persons

--- Udemy Understanding TypeScript/exercise_files/Section_09/prj-00-initial-starting-setup/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/Section_09/prj-00-initial-starting-setup/src/app.ts
@@ -184,6 +184,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 	private project: Project;
 
+	get persons() {
+		if (this.project.people === 1) {
+			return ' 1 person';
+		} else {
+			return ` ${this.project.people} persons`;
+		}
+	}
+
 	constructor(hostId: string, project: Project) {
 		super('single-project', hostId, project.id, false);
 		this.project = project;
@@ -196,7 +204,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 	protected renderContent() {
 		this.element.querySelector('h2')!.textContent = this.project.title;
 		this.element.querySelector('h3')!.textContent =
-			this.project.people.toString();
+			this.persons + ' assigned.';
 		this.element.querySelector('p')!.textContent = this.project.description;
 	}
 }
