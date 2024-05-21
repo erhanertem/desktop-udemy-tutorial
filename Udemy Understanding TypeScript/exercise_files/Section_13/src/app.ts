95b46194ff517da43f180158e1fad5a4e653b5e7
Erhan Ertem <eertem@gmail.com>
2024-05-21 23:52:49 +0300

    Add HTML template and establish event listener for input to send to google map api

--- Udemy Understanding TypeScript/exercise_files/Section_13/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/Section_13/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+	event.preventDefault();
+	const enteredAddress = addressInput.value;
+
+	// Send this to Google Map API
+	console.log(enteredAddress);
+}
+
+form.addEventListener('submit', searchAddressHandler);
