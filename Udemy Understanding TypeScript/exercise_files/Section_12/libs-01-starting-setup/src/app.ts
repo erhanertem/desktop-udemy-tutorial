f10a44fc1221e5391d09a4f54c3bd5c1357a698b
Erhan Ertem <eertem@gmail.com>
2024-05-21 15:09:06 +0300

    class-transformer TS friendly library for transforming fetched content

--- Udemy Understanding TypeScript/exercise_files/Section_12/libs-01-starting-setup/src/app.ts
+++ Udemy Understanding TypeScript/exercise_files/Section_12/libs-01-starting-setup/src/app.ts
@@ -1,8 +1,31 @@
 // Code goes here!
 import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
+
+import { Product } from './product.model';
 
 declare const GLOBAL: any;
 
 console.log(_.shuffle([1, 2, 3, 4, 5]));
 
 console.log(GLOBAL);
+
+// Some fetched response received from a DB hypothetically
+const fetchedProducts = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: ' A Book', price: 10.99 },
+];
+// // This how we store data ['A Book', $12.99]
+// // Manual gtransformation code would be like:
+// const products = fetchedProducts.map(
+//   (each) => new Product(each.title, each.price)
+// );
+// Via class-transformer npm package which works well with TS
+const products = plainToInstance(Product, fetchedProducts);
+for (const product of products) {
+  console.log(product.getInformation());
+}
+
+const p1 = new Product('A Book', 12.99);
+console.log(p1.getInformation());
