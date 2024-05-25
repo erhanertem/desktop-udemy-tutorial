4293f46bd0e0de69aef72eab6bc0800e675d64f0
Erhan Ertem <eertem@gmail.com>
2024-05-25 12:40:34 +0300

    Using union operator for type checking and  type guards to differentiate logic based on the input type

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 10/sort/build/index.js
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 10/sort/build/index.js
@@ -8,10 +8,16 @@ var Sorter = /** @class */ (function () {
         // BUBBLE SORT ALGORITHM
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                // -> TYPE GUARD FOR ARRAY INPUT
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                // -> TYPE GUARD FOR STRING INPUT
+                if (typeof this.collection === 'string') {
                 }
             }
         }
@@ -21,3 +27,9 @@ var Sorter = /** @class */ (function () {
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
+console.log("'a'.charCodeAt(0) :", 'a'.charCodeAt(0));
+console.log("'b'.charCodeAt(0) :", 'b'.charCodeAt(0));
+console.log("'z'.charCodeAt(0) :", 'z'.charCodeAt(0));
+console.log("'A'.charCodeAt(0) :", 'A'.charCodeAt(0));
+console.log("'B'.charCodeAt(0) :", 'B'.charCodeAt(0));
+console.log("'Z'.charCodeAt(0) :", 'Z'.charCodeAt(0));
