0c15f37d0668c1905c388cb712483166e4ec6444
Erhan Ertem <eertem@gmail.com>
2024-05-25 13:31:30 +0300

    Create a universal sorter class and  interface to dictate sortable consistency

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 10/sort/build/index.js
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 10/sort/build/index.js
@@ -1,35 +1,8 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        // BUBBLE SORT ALGORITHM
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                // -> TYPE GUARD FOR ARRAY INPUT
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                // -> TYPE GUARD FOR STRING INPUT
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([50, 3, -5, 0]);
+var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
-console.log("'a'.charCodeAt(0) :", 'a'.charCodeAt(0));
-console.log("'b'.charCodeAt(0) :", 'b'.charCodeAt(0));
-console.log("'z'.charCodeAt(0) :", 'z'.charCodeAt(0));
-console.log("'A'.charCodeAt(0) :", 'A'.charCodeAt(0));
-console.log("'B'.charCodeAt(0) :", 'B'.charCodeAt(0));
-console.log("'Z'.charCodeAt(0) :", 'Z'.charCodeAt(0));
+console.log(numbersCollection.data);
--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 10/sort/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 10/sort/src/index.ts
@@ -1,36 +1,8 @@
-class Sorter {
-	constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-	sort(): void {
-		const { length } = this.collection;
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 
-		// BUBBLE SORT ALGORITHM
-		for (let i = 0; i < length; i++) {
-			for (let j = 0; j < length - i - 1; j++) {
-				// -> TYPE GUARD FOR ARRAY INPUT
-				if (this.collection instanceof Array) {
-					if (this.collection[j] > this.collection[j + 1]) {
-						const temp = this.collection[j];
-						this.collection[j] = this.collection[j + 1];
-						this.collection[j + 1] = temp;
-					}
-				}
-
-				// -> TYPE GUARD FOR STRING INPUT
-				if (typeof this.collection === 'string') {
-				}
-			}
-		}
-	}
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
-
-console.log("'a'.charCodeAt(0) :", 'a'.charCodeAt(0));
-console.log("'b'.charCodeAt(0) :", 'b'.charCodeAt(0));
-console.log("'z'.charCodeAt(0) :", 'z'.charCodeAt(0));
-console.log("'A'.charCodeAt(0) :", 'A'.charCodeAt(0));
-console.log("'B'.charCodeAt(0) :", 'B'.charCodeAt(0));
-console.log("'Z'.charCodeAt(0) :", 'Z'.charCodeAt(0));
+console.log(numbersCollection.data);
