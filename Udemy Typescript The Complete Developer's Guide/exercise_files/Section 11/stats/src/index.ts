c3f25f70fc4a1b94ec48fa7249d7189391e91eb9
Erhan Ertem <eertem@gmail.com>
2024-05-26 10:25:36 +0300

    Retrieve data set from excel sheet and make a report using plain JS

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
@@ -1,1 +1,21 @@
 import fs from 'fs';
+
+// readFileSync reads text, image, etc. {encoding: 'utf-8'} option returns the buffer data as text string
+const matches = fs
+	.readFileSync('football.csv', { encoding: 'utf-8' })
+	.split('\n')
+	.map((row: string): string[] => {
+		return row.split(',');
+	});
+console.log(matches);
+
+let manUnitedWins = 0;
+for (let match of matches) {
+	if (
+		(match[1] === 'Man United' && match[5] === 'H') ||
+		(match[2] === 'Man United' && match[5] === 'A')
+	) {
+		manUnitedWins++;
+	}
+}
+console.log(`Man United won ${manUnitedWins} games`);
