57511e6581a730266e040670e916772db03b09e1
Erhan Ertem <eertem@gmail.com>
2024-05-26 14:05:41 +0300

    Extract csv reading logic into  a class

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
@@ -1,13 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-// readFileSync reads text, image, etc. {encoding: 'utf-8'} option returns the buffer data as text string
-const matches = fs
-	.readFileSync('football.csv', { encoding: 'utf-8' })
-	.split('\n')
-	.map((row: string): string[] => {
-		return row.split(',');
-	});
-console.log(matches);
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 // enum - enumeration
 const MatchResult = {
@@ -17,7 +11,7 @@ const MatchResult = {
 };
 
 let manUnitedWins = 0;
-for (let match of matches) {
+for (let match of reader.data) {
 	if (
 		(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
 		(match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
