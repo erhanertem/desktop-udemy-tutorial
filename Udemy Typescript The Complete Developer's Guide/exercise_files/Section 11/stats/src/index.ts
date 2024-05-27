83b31f99489e7fbc45ff9278de27ef2f5dff0528
Erhan Ertem <eertem@gmail.com>
2024-05-27 13:44:58 +0300

    Make a more universal class using Composition Approach with analyzer and report customization classes - Analyzer

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
@@ -9,16 +9,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-// Filter Logic Process
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-	if (
-		(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
-		(match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
-	) {
-		manUnitedWins++;
-	}
-}
 console.log(`Man United won ${manUnitedWins} games`);
 
 // > Path #1 . Inheritance Solution
