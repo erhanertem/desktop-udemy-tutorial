13828b5253b051f9496857de2bc53289fe17c854
Erhan Ertem <eertem@gmail.com>
2024-05-27 12:56:00 +0300

    Provide composition solution aside inheritance solution

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 11/stats/src/index.ts
@@ -1,14 +1,17 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+// > Path #2 Interface-composition solution
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+// Create an instance of the customized reader setup
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-const dateOfFirstMatch = reader.data[0][0];
-console.log('dateOfFirstMatch :', dateOfFirstMatch);
-
+// Filter Logic Process
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
 	if (
 		(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
 		(match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
@@ -17,3 +20,22 @@ for (let match of reader.data) {
 	}
 }
 console.log(`Man United won ${manUnitedWins} games`);
+
+// > Path #1 . Inheritance Solution
+// const reader = new MatchReader('football.csv');
+// reader.read();
+
+// const dateOfFirstMatch = reader.data[0][0];
+// console.log('dateOfFirstMatch :', dateOfFirstMatch);
+
+// Filter Logic Process
+// let manUnitedWins = 0;
+// for (let match of reader.data) {
+// 	if (
+// 		(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+// 		(match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
+// 	) {
+// 		manUnitedWins++;
+// 	}
+// }
+// console.log(`Man United won ${manUnitedWins} games`);
