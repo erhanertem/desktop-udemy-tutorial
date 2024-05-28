fd9729dfe1d3874606b2a982e76cb030cc1a1d91
Erhan Ertem <eertem@gmail.com>
2024-05-28 19:05:51 +0300

    Create save functionality for PUT & POST requests

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/index.ts
@@ -1,8 +1,13 @@
 import { User } from './models/User';
 
 const user = new User({ id: 1 });
-
 user.fetch();
+user.set({ name: 'New Name' });
+user.set({ age: 1 });
+user.save();
+
+const user1 = new User({ name: 'new record', age: 9999 });
+user1.save();
 
 setTimeout(() => {
 	console.log(user);
--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 
+const DB_URL = 'http://localhost:3000/users/';
+
 interface UserProps {
 	id?: number;
 	name?: string;
@@ -43,7 +45,18 @@ export class User {
 
 	fetch(): void {
 		axios
-			.get(`http://localhost:3000/users/${this.get('id')}`)
+			.get(`${DB_URL}${this.get('id')}`)
 			.then((res: AxiosResponse): void => this.set(res.data));
+	}
+
+	save(): void {
+		const idExist = this.get('id');
+		if (idExist) {
+			// Execute a PUT request to modify the data
+			axios.put(`${DB_URL}${this.get('id')}`, this.data);
+		} else {
+			// Execute a POST request to create a new one
+			axios.post(`${DB_URL}`, this.data);
+		}
 	}
 }
