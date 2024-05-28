0feafa32b1cea5c8d605fd410959c06adc516438
Erhan Ertem <eertem@gmail.com>
2024-05-28 11:41:10 +0300

    Add in snippet for registering event handling type and corresponding callback fn to event store

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.ts
@@ -19,5 +19,9 @@ export class User {
 		Object.assign(this.data, update);
 	}
 
-	on(eventName: string, callbackFn: CallbackFn): void {}
+	on(eventName: string, callbackFn: CallbackFn): void {
+		const handlers = this.events[eventName] || []; // If the key exists = CallbackFn[] else returns undefined because none assigned yet so we got to define by default empty[] to inser in the upcoming callbackFn
+		handlers.push(callbackFn);
+		this.events[eventName] = handlers;
+	}
 }
