610eeef42860067ab562c7848705690b499fe922
Erhan Ertem <eertem@gmail.com>
2024-05-28 11:28:22 +0300

    Construct user model with property setter/getter and event listener

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: 'Aeron' });
+
+user.get('name');
+console.log("user.get('name') :", user.get('name'));
+user.get('age');
+console.log("user.get('age') :", user.get('age'));
---
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.ts
@@ -0,0 +1,23 @@
+interface UserProps {
+	name?: string;
+	age?: number;
+}
+
+// Type Alias
+type CallbackFn = () => {};
+
+export class User {
+	private events: { [key: string]: CallbackFn[] } = {};
+
+	constructor(private data: UserProps) {}
+
+	get(propertyName: string): string | number {
+		return this.data[propertyName];
+	}
+
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
+	}
+
+	on(eventName: string, callbackFn: CallbackFn) {}
+}
