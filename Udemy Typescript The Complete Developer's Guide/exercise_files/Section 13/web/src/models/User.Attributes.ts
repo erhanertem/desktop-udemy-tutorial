39c1648098b9d737b6c6ab6f83d67b7df41c7b6a
Erhan Ertem <eertem@gmail.com>
2024-05-29 10:34:43 +0300

    Test keyof @ generics code

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.Attributes.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/models/User.Attributes.ts
@@ -1,3 +1,5 @@
+import { UserProps } from './User';
+
 export class Attributes<T extends object> {
 	constructor(private data: T) {}
 
@@ -9,3 +11,11 @@ export class Attributes<T extends object> {
 		Object.assign(this.data, update);
 	}
 }
+
+const attrs = new Attributes<UserProps>({
+	id: 5,
+	age: 20,
+	name: 'whatever',
+});
+const name = attrs.get('name');
+const age = attrs.get('age');
