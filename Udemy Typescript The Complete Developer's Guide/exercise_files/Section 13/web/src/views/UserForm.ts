c851f4d8411243576255b9de5dcfc887011d75d4
Erhan Ertem <eertem@gmail.com>
2024-05-30 13:44:40 +0300

    Setup functional injection of event handlers on the template @ UserForm

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/views/UserForm.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/views/UserForm.ts
@@ -3,22 +3,54 @@ import { html } from 'code-tag';
 export class UserForm {
 	constructor(public parent: Element) {}
 
+	eventsMap(): { [key: string]: () => void } {
+		return {
+			'click:button': this.onButtonClick,
+			'mouseenter:h1': this.onHeaderHover,
+		};
+	}
+
+	onButtonClick(): void {
+		console.log('Say Hi!!!⚠️⚠️⚠️⚠️');
+	}
+
+	onHeaderHover(): void {
+		console.log('I am on Hover!!!👉👉👉');
+	}
+
 	template(): string {
 		return html`
 			<div>
 				<h1>User Form</h1>
 				<input />
+				<button>Click Me!</button>
 			</div>
 		`;
 	}
 
+	bindEvents(fragment: DocumentFragment): void {
+		const eventsMap = this.eventsMap();
+
+		for (let eventKey in eventsMap) {
+			const [eventName, selector] = eventKey.split(':');
+
+			fragment
+				.querySelectorAll(selector)
+				.forEach((el) =>
+					el.addEventListener(eventName, eventsMap[eventKey]),
+				);
+		}
+	}
+
 	render(): void {
-		// Create a template tag element
 		// NOTE: The < template > tag is used as a container to hold some HTML content hidden from the user when the page loads.
+		// #1. Create a template tag element
 		const templateElement = document.createElement('template');
-		// Insert HTML content inside this template element
+		// #2. Insert HTML content inside this template element
 		templateElement.innerHTML = this.template();
-		// Make template element content visible
+		// #3. Bind Event handlers on the template string
+		this.bindEvents(templateElement.content);
+		// #4. Make template element content visible
 		this.parent.append(templateElement.content);
 	}
 }
