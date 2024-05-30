61f85ecbc576a11447b78c2e11214c6595aa7a07
Erhan Ertem <eertem@gmail.com>
2024-05-30 15:37:59 +0300

    Render the random age + clear the parent container

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/views/UserForm.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 13/web/src/views/UserForm.ts
@@ -3,7 +3,16 @@ import { html } from 'code-tag';
 import { User } from './../models/User';
 
 export class UserForm {
-	constructor(public parent: Element, public model: User) {}
+	constructor(public parent: Element, public model: User) {
+		this.bindModel();
+	}
+
+	bindModel(): void {
+		// NOTE: @model.ts set() update function, when its called, we trigger a change notification event. This change trigger will help us re-render the UserForm.
+		this.model.on('change', () => {
+			this.render();
+		});
+	}
 
 	eventsMap(): { [key: string]: () => void } {
 		return {
@@ -43,14 +52,16 @@ export class UserForm {
 	}
 
 	render(): void {
+		// #1. Remove any previously renderered content inside the div container to render
+		this.parent.innerHTML = '';
 		// NOTE: The < template > tag is used as a container to hold some HTML content hidden from the user when the page loads.
-		// #1. Create a template tag element
+		// #2. Create a template tag element
 		const templateElement = document.createElement('template');
-		// #2. Insert HTML content inside this template element
+		// #3. Insert HTML content inside this template element
 		templateElement.innerHTML = this.template();
-		// #3. Bind Event handlers on the template string
+		// #4. Bind Event handlers on the template string
 		this.bindEvents(templateElement.content);
-		// #4. Make template element content visible
+		// #5. Make template element content visible
 		this.parent.append(templateElement.content);
 	}
 }
