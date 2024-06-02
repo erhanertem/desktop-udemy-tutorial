215841c931ab9df1c87f28348e0a2baf628dfd4a
Erhan Ertem <eertem@gmail.com>
2024-06-02 10:47:51 +0300

    Set router object  to host our routes

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 14/server/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 14/server/src/index.ts
@@ -2,16 +2,19 @@ import { html } from 'code-tag';
 
 import express, { Request, Response } from 'express';
 
+import { router } from './routes/LoginRoutes';
+
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(html`
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  `);
-});
+app.use(router);
+// app.get('/', (req: Request, res: Response) => {
+// 	res.send(html`
+// 		<div>
+// 			<h1>Hi there!</h1>
+// 		</div>
+// 	`);
+// });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+	console.log('Listening on port 3000');
 });
