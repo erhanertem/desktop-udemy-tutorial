9c5435058e464f44ed2a11c579ac572429a68d26
Erhan Ertem <eertem@gmail.com>
2024-06-02 11:02:13 +0300

    Define post and get fetch for the /login endpoint

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 14/server/src/index.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 14/server/src/index.ts
@@ -1,19 +1,13 @@
-import { html } from 'code-tag';
+import express from 'express';
 
-import express, { Request, Response } from 'express';
-
-import { router } from './routes/LoginRoutes';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(router);
-// app.get('/', (req: Request, res: Response) => {
-// 	res.send(html`
-// 		<div>
-// 			<h1>Hi there!</h1>
-// 		</div>
-// 	`);
-// });
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 14/server/src/routes/loginRoutes.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 14/server/src/routes/loginRoutes.ts
@@ -1,9 +1,29 @@
-import { Router } from 'express';
+import { html } from 'code-tag';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	res.send('hi there');
+// When hit this endpoint, provide this layout
+router.get('/login', (req: Request, res: Response) => {
+	res.send(html`
+		<form method="POST">
+			<div>
+				<label>Email</label>
+				<input name="email" />
+			</div>
+			<div>
+				<label>Password</label>
+				<input name="password" type="password" />
+			</div>
+			<button>Submit</button>
+		</form>
+	`);
+});
+
+// When hit post request on this end point, do....
+router.post('/login', (req: Request, res: Response) => {
+	const { email, password } = req.body;
+	res.send(email + password);
 });
 
 export { router };
