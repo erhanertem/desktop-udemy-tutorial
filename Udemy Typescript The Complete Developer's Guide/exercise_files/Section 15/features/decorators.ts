8a31378e0681d447213b5de98b7f026543304905
Erhan Ertem <eertem@gmail.com>
2024-06-03 12:03:00 +0300

    Create a factory decorator function

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 15/features/decorators.ts
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 15/features/decorators.ts
@@ -5,23 +5,29 @@ class Boat {
 		return `This boasts color is ${this.color}`;
 	}
 
-	@LogError
+	@LogError('Oops boat was sunk in ocean')
 	pilot(): void {
 		throw new Error();
 		// console.log('swish');
 	}
 }
 
-function LogError(_target: any, _key: string, desc: PropertyDescriptor): void {
-	console.log(desc.value);
-	const method = desc.value;
+function LogError(errorMessage: string) {
+	return function (
+		_target: any,
+		_key: string,
+		desc: PropertyDescriptor,
+	): void {
+		console.log(desc.value);
+		const method = desc.value;
 
-	desc.value = function () {
-		try {
-			method();
-		} catch (e) {
-			console.log('Oops, boat was sunk');
-		}
+		desc.value = function () {
+			try {
+				method();
+			} catch (e) {
+				console.log(errorMessage);
+			}
+		};
 	};
 }
 
