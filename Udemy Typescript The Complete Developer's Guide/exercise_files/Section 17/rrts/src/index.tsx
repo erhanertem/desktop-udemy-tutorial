4c024ed4a58980ae0f9caba4d95e0ff53701b7cc
Erhan Ertem <eertem@gmail.com>
2024-06-05 12:43:42 +0300

    Props and state @ class based react component w/TS

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 17/rrts/src/index.tsx
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 17/rrts/src/index.tsx
@@ -1,34 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// // > Older Syntax
-// import ReactDOM from 'react-dom';
+// > Older Syntax - ClassBased
+import ReactDOM from 'react-dom';
 
-// interface AppProps {
-// 	color: string;
-// }
-
-// class App_ extends React.Component<AppProps> {
-// 	render() {
-// 		return <div>Hi there. {this.props.color}</div>;
-// 	}
-// }
-// ReactDOM.render(<App_ color="red" />, document.querySelector('#root'));
-
-// > Newer Syntax
-import ReactDOM from 'react-dom/client';
-
-// NOTE: An interface has to accompany a React Component
 interface AppProps {
-	color?: string;
-}
-// React Component
-function App({ color }: AppProps) {
-	return <div>Hi there. {color}</div>;
+	color: string;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-	<React.StrictMode>
-		<App color="red" />
-	</React.StrictMode>,
-);
+class App extends React.Component<AppProps> {
+	state = { counter: 0 };
+
+	onIncrement = (): void => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+	onDecrement = (): void => {
+		this.setState({ counter: this.state.counter - 1 });
+	};
+
+	render() {
+		return (
+			<div>
+				<p>Hi there. {this.props.color}</p>
+				<button onClick={this.onIncrement}>Increment</button>
+				<button onClick={this.onDecrement}>Decrement</button>
+				{this.state.counter}
+			</div>
+		);
+	}
+}
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+
+// // > Newer Syntax
+// import ReactDOM from 'react-dom/client';
+
+// // NOTE: An interface has to accompany a React Component
+// interface AppProps {
+// 	color?: string;
+// }
+
+// // React Component
+// function App({ color }: AppProps) {
+// 	const [counter, setCounter] = useState(0);
+
+// 	const onIncrement = (): void => {
+// 		setCounter((counter) => counter + 1);
+// 	};
+// 	const onDecrement = (): void => {
+// 		setCounter((counter) => counter - 1);
+// 	};
+
+// 	return (
+// 		<div>
+// 			<p>Hi there. {color}</p>
+// 			<button onClick={onIncrement}>Increment</button>
+// 			<button onClick={onDecrement}>Decrement</button>
+// 			{counter}
+// 		</div>
+// 	);
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root')!);
+// root.render(
+// 	<React.StrictMode>
+// 		<App color="red" />
+// 	</React.StrictMode>,
+// );
