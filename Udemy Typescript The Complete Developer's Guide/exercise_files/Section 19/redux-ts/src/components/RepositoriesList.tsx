de19f8b4e2b60e4bd8b049770a7fc1fafaa33a7a
Erhan Ertem <eertem@gmail.com>
2024-06-07 11:20:04 +0300

    Read state into react component via useSelector react-redux hook

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 19/redux-ts/src/components/RepositoriesList.tsx
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 19/redux-ts/src/components/RepositoriesList.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, ReactElement, useState } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 // import { actionCreators } from '../state';
 // Custom hook to simplify calling actionCreators
@@ -9,6 +10,8 @@ function RepositoriesList(): ReactElement {
 	const [term, setTerm] = useState('');
 	// const dispatch: Function = useDispatch();
 	const { searchRepositories } = useActions();
+	const state = useSelector((state: any) => state.repositories);
+	console.log(state);
 
 	function onSubmit(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
