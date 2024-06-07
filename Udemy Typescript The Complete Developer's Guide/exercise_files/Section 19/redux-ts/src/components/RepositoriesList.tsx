483f686d953c9e7354413d58ebea3fa8bba3181d
Erhan Ertem <eertem@gmail.com>
2024-06-07 09:58:13 +0300

    Wireup and type event handler on the form component

--- Udemy Typescript The Complete Developer's Guide/exercise_files/Section 19/redux-ts/src/components/RepositoriesList.tsx
+++ Udemy Typescript The Complete Developer's Guide/exercise_files/Section 19/redux-ts/src/components/RepositoriesList.tsx
@@ -1,10 +1,16 @@
-import { ReactElement } from 'react';
+import { FormEvent, ReactElement, useState } from 'react';
 
 function RepositoriesList(): ReactElement {
+	const [term, setTerm] = useState('');
+
+	function onSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+	}
+
 	return (
 		<div>
-			<form>
-				<input />
+			<form onSubmit={onSubmit}>
+				<input value={term} onChange={(e) => setTerm(e.target.value)} />
 				<button>Search</button>
 			</form>
 		</div>
