eca8def52ba80ed5b9d4e8ff5ec798f229266f51
Erhan Ertem <eertem@gmail.com>
2024-06-24 23:56:03 +0300

    Udemy JavaScript Algorithms and Data Structures Masterclass - Colt Steele - Finalize Section 9 - Recursions Exercises Part 2

--- Udemy JavaScript Algorithms and Data Structures Masterclass Colt Steele/exercise_files/section_09.js
+++ Udemy JavaScript Algorithms and Data Structures Masterclass Colt Steele/exercise_files/section_09.js
@@ -108,11 +108,151 @@ function flatten_(arr, newArr = []) {
 
 // --> PART 2
 // > EXERCISE #19
+function capitalizeFirst(arr, newArr = []) {
+	// BASE CASE
+	if (!arr.length) {
+		return newArr;
+	}
+	// RECURSIVE
+	newArr.push(arr[0].charAt(0).toUpperCase() + arr[0].slice(1));
+	return capitalizeFirst(arr.slice(1), newArr);
+}
+// console.log("capitalizeFirst(['car', 'taco', 'banana']) :", capitalizeFirst(['car', 'taco', 'banana'])); // ['Car','Taco','Banana']
 
 // > EXERCISE #20
+function nestedEvenSum(obj, sum = 0) {
+	for (const key in obj) {
+		if (typeof obj[key] === 'object') {
+			sum += nestedEvenSum(obj[key]);
+		} else if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+			sum += obj[key];
+		}
+	}
+	return sum;
+}
+
+var obj1 = {
+	outer: 2,
+	obj: {
+		inner: 2,
+		otherObj: {
+			superInner: 2,
+			notANumber: true,
+			alsoNotANumber: 'yup',
+		},
+	},
+};
+
+var obj2 = {
+	a: 2,
+	b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+	c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+	d: 1,
+	e: { e: { e: 2 }, ee: 'car' },
+};
+
+// console.log('nestedEvenSum(obj1) :', nestedEvenSum(obj1)); // 6
+// console.log('nestedEvenSum(obj2) :', nestedEvenSum(obj2)); // 10
 
 // > EXERCISE #21
+function capitalizeWords(array) {
+	if (array.length === 1) {
+		return [array[0].toUpperCase()];
+	}
+	let res = capitalizeWords(array.slice(0, -1));
+	res.push(array.slice(array.length - 1)[0].toUpperCase());
+	return res;
+}
+let words = ['i', 'am', 'learning', 'recursion'];
+// console.log('capitalizeWords(words) :', capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
 
 // > EXERCISE #22
+function stringifyNumbers(obj) {
+	var newObj = {};
+	for (const key in obj) {
+		if (typeof obj[key] === 'number') {
+			newObj[key] = obj[key].toString();
+		} else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+			newObj[key] = stringifyNumbers(obj[key]);
+		} else {
+			newObj[key] = obj[key];
+		}
+	}
+	return newObj;
+}
+/*
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+/*
 
+stringifyNumbers(obj)
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
 // > EXERCISE #23
+function collectStrings_(obj) {
+	var stringsArr = [];
+
+	function gatherStrings(o) {
+		for (const key in o) {
+			if (typeof o[key] === 'string') {
+				stringsArr.push(o[key]);
+			} else if (typeof o[key] === 'object') {
+				return gatherStrings(o[key]);
+			}
+		}
+	}
+
+	gatherStrings(obj);
+
+	return stringsArr;
+}
+function collectStrings(obj) {
+	var stringsArr = [];
+	for (var key in obj) {
+		if (typeof obj[key] === 'string') {
+			stringsArr.push(obj[key]);
+		} else if (typeof obj[key] === 'object') {
+			stringsArr = stringsArr.concat(collectStrings(obj[key]));
+		}
+	}
+
+	return stringsArr;
+}
+const obj = {
+	stuff: 'foo',
+	data: {
+		val: {
+			thing: {
+				info: 'bar',
+				moreInfo: {
+					evenMoreInfo: {
+						weMadeIt: 'baz',
+					},
+				},
+			},
+		},
+	},
+};
+
+collectStrings(obj); // ["foo", "bar", "baz"])
