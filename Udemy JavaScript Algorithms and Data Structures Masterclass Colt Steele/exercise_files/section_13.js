26f96161c812ca606bf3fd2efc6bae94c6c595f1
Erhan Ertem <eertem@gmail.com>
2024-06-27 16:38:39 +0300

    Add notes

--- Udemy JavaScript Algorithms and Data Structures Masterclass Colt Steele/exercise_files/section_13.js
+++ Udemy JavaScript Algorithms and Data Structures Masterclass Colt Steele/exercise_files/section_13.js
@@ -1,9 +1,13 @@
 // LESSON 13 - INSERTION SORT
 // > SORTING ALGORITHMS
 function insertionSort(arr) {
+	// RESERVE FIRST ARRAY ELEMENT TO START FOR SORTING - THE SECOND ELEMENT ONWARDS IS ACQUIRED FOR INSERT SORTING
 	for (let i = 1; i < arr.length; i++) {
+		// INSERT SORTING TO LEFT WING ARRAY
 		for (let j = i; j >= 0; j--) {
+			// CHECK IF THE PRECEDING ELEMENT IS BIGGER - IF SO SWATCH ELSE BREAK SORTING
 			if (arr[j - 1] > arr[j]) {
+				// SWATCH ELEMENTS LOGIC
 				[arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
 			} else {
 				break;
