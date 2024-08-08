f95e1dda664229a4fbdf1367beb9f5318fd959fd
Erhan Ertem <eertem@gmail.com>
2024-08-08 13:21:33 +0300

    Udemy Redis The Complete Developer's Guide Stephen Grider -  Finalize Section 6 - Some issues around hset and hgetall

--- Udemy Redis The Complete Developer's Guide Stephen Grider/Section03/rbay/sandbox/index.ts
+++ Udemy Redis The Complete Developer's Guide Stephen Grider/Section03/rbay/sandbox/index.ts
@@ -1,5 +1,20 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950,
+		engine: { cylinders: 8 },
+		owner: null || '',
+		service: undefined || ''
+	});
+	const car = await client.hGetAll('car');
+	console.log(car);
+	const car2 = await client.hGetAll('car#12212');
+	console.log(car2);
+	if (Object.keys(car2).length === 0) {
+		console.log('Car not found, respond with 404');
+	}
+};
 run();
