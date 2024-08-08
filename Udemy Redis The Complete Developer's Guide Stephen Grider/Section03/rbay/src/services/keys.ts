cd6d3f3b19b0a21e3c20b9fae321d4e9d20949a3
Erhan Ertem <eertem@gmail.com>
2024-08-08 10:50:07 +0300

    Udemy Redis The Complete Developer's Guide Stephen Grider -  Finalize Section 3 - Add getter setter with factory function to automate key naming for redis operations

---
+++ Udemy Redis The Complete Developer's Guide Stephen Grider/Section03/rbay/src/services/keys.ts
@@ -0,0 +1,1 @@
+export const pageCacheKeys = (id: string) => `pagecache#${id}`;
--- Udemy Redis The Complete Developer's Guide Stephen Grider/Section03/rbay/src/services/queries/page-cache.ts
+++ Udemy Redis The Complete Developer's Guide Stephen Grider/Section03/rbay/src/services/queries/page-cache.ts
@@ -1,3 +1,17 @@
-export const getCachedPage = (route: string) => {};
+import { pageCacheKeys } from '$services/keys';
+import { client } from '$services/redis';
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/singup'];
+
+export const getCachedPage = (route: string) => {
+	if (cacheRoutes.includes(route)) {
+		return client.get(pageCacheKeys(route)); // Reddis command : GET 'pagecache#/about'
+	}
+	return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+	if (cacheRoutes.includes(route)) {
+		return client.set(pageCacheKeys(route), page, { EX: 2 }); // Reddis command : SET 'pagecache#/about', '<p><h1>....</h1></p>' , EX: 2
+	}
+};
