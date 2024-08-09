f7729978954c31b32dceebd2d2791e81aa4d58df
Erhan Ertem <eertem@gmail.com>
2024-08-09 11:58:15 +0300

    Udemy Redis The Complete Developer's Guide Stephen Grider -  Section 10 - Showing like items from the client

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/likes.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/likes.ts
@@ -1,12 +1,18 @@
 import { client } from '$services/redis';
 import { itemsKey, userLikesKey } from '$services/keys';
+import { getItems } from './items';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
 	// Check if the user has already liked the item
 	return client.sIsMember(userLikesKey(userId), itemId); // Reddis command : SISMEMBER 'userlikes:userId' itemId
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+	// Fetch all the item ID's from this user's liked list
+	const ids = await client.sMembers(userLikesKey(userId));
+	// Fetch all the item hashes with those item ids and return as an array
+	return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
 	// Add liked item to the user's like set data list
