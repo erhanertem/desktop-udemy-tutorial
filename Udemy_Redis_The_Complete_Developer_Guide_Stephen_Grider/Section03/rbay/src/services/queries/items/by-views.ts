bae4d8766ad7737d13e710ac1c8aa502d11c1214
Erhan Ertem <eertem@gmail.com>
2024-08-10 19:16:13 +0300

    Deserialize the sort raw data

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/items/by-views.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/items/by-views.ts
@@ -1,14 +1,29 @@
 import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { deserialize } from './deserialize';
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
 	// NOTE: Here sort command pulls each record of items#**.name and items#**.views properties and publish aside the matching member ids in items:view. We make sure the array includes the member ids via '#' annotation.
-	const results = await client.sort(itemsByViewsKey(), {
+	let results: any = await client.sort(itemsByViewsKey(), {
 		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
 		BY: 'noscore', // We could have put also 'xvgsfdfdgfdfg' . Does not matter. Since items:views sorted set data is already sorted.
 		DIRECTION: order,
 		LIMIT: { offset, count }
 	});
-
 	console.log(results);
+
+	const items = [];
+	while (results.length) {
+		// Strip the first set of items
+		const [id, name, views, ...rest] = results;
+		// Deserialize given data
+		const item = deserialize(id, { name, views });
+		// Drop in the temporary array
+		items.push(item);
+		// Assign the remainder data as the new array set to extract data for the next item recursivly
+		results = rest;
+	}
+
+	// Return the rebuilt-deserailized items
+	return items;
 };
