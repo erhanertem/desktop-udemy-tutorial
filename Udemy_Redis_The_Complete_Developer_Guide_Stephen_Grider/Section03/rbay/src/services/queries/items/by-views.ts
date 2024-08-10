ef437604ce014468af4443307535cff6f62021d1
Erhan Ertem <eertem@gmail.com>
2024-08-10 18:59:23 +0300

    Udemy Redis The Complete Developer's Guide Stephen Grider -  Section 13 - Get the union of name and view properties of corresponding members in the items:view table in one go via sort() methid in reddis

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/items/by-views.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/items/by-views.ts
@@ -1,1 +1,14 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	// NOTE: Here sort command pulls each record of items#**.name and items#**.views properties and publish aside the matching member ids in items:view. We make sure the array includes the member ids via '#' annotation.
+	const results = await client.sort(itemsByViewsKey(), {
+		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
+		BY: 'noscore', // We could have put also 'xvgsfdfdgfdfg' . Does not matter. Since items:views sorted set data is already sorted.
+		DIRECTION: order,
+		LIMIT: { offset, count }
+	});
+
+	console.log(results);
+};
