4e32beb73e9929de5b687595aa7677ec604e3901
Erhan Ertem <eertem@gmail.com>
2024-08-10 11:14:50 +0300

    Udemy Redis The Complete Developer's Guide Stephen Grider -  Section 12 - Increment views count on item hash table + item:views sorted set table

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/views.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/views.ts
@@ -1,1 +1,11 @@
-export const incrementView = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+
+export const incrementView = async (itemId: string, userId: string) => {
+	return Promise.all([
+		// Increment 'views' value inside the items#[itemId] hash table by 1
+		client.hIncrBy(itemsKey(itemId), 'views', 1),
+		// Apply the increment change on items:views sorted set table
+		client.zIncrBy(itemsByViewsKey(), 1, itemId)
+	]);
+};
