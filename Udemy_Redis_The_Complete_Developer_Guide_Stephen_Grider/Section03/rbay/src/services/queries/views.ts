4951b25940404acf458bacacb227ed8a312938a4
Erhan Ertem <eertem@gmail.com>
2024-08-13 00:04:23 +0300

    Transform 2 step operation into a single fetch via LUA scripting in redis for incrementView function

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/views.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/views.ts
@@ -2,16 +2,31 @@ import { client } from '$services/redis';
 import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	// Attemp to insert userId inside the hyperlolog data table called items:views#itemId - if already added return 0 else 1
-	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+	// IMPORTANT NOTE: This block of code is a good candidate for using LUA scripting to get all processed in one go @ redis. Because, we get response from redis, and dependign on that response we proceed w/ additional redis tasks.
+	// // Attempt to insert userId inside the hyperlolog data table called items:views#itemId - if already added return 0 else 1
+	// const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+	// // If not inserted before yields 1 which is truthy and proceeds w/ increment operations
+	// if (inserted) {
+	// 	return Promise.all([
+	// 		// Increment 'views' value inside the items#[itemId] hash table by 1
+	// 		client.hIncrBy(itemsKey(itemId), 'views', 1),
+	// 		// Apply the increment change on items:views sorted set table
+	// 		client.zIncrBy(itemsByViewsKey(), 1, itemId)
+	// 	]);
+	// }
 
-	// If not inserted before yields 1 which is truthy and proceeds w/ increment operations
-	if (inserted) {
-		return Promise.all([
-			// Increment 'views' value inside the items#[itemId] hash table by 1
-			client.hIncrBy(itemsKey(itemId), 'views', 1),
-			// Apply the increment change on items:views sorted set table
-			client.zIncrBy(itemsByViewsKey(), 1, itemId)
-		]);
-	}
+	/**
+	 * KEYS I NEED TO ACCESS
+	 * 1. itemsViewsKey
+	 * 2. itemsKey
+	 * 3. itemsByViewsKey
+	 * EVALSHA <ID> 3 ....
+	 *
+	 * ARGUMENTS I NEED TO ACCEPT
+	 * 1. itemId
+	 * 2. userId
+	 * */
+
+	// CALL THE LUA SCRIPT FUNCTION
+	return client.incrementView(itemId, userId);
 };
--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/redis/client.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/redis/client.ts
@@ -1,3 +1,4 @@
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
 import { createClient, defineScript } from 'redis';
 
 const client = createClient({
@@ -8,35 +9,69 @@ const client = createClient({
 	password: process.env.REDIS_PW,
 	scripts: {
 		// Step#1. Adds additional methods to the client object
-		addOneAndStore: defineScript({
-			// Step#3. Number of keys to be expected by EVALSHA call
-			NUMBER_OF_KEYS: 1,
-			// Step#2. Define the LUA script
-			SCRIPT: "return redis.call('SET', KEYS[1], 1+tonumber(ARGV[1]))",
+		incrementView: defineScript({
+			// Step#3. Number of keys to be expected by EVALSHA call - BASED ON views.ts NOTES
+			NUMBER_OF_KEYS: 3,
+			// Step#2. Define the LUA script as its outlined in views.ts
+			SCRIPT: `
+         local itemsViewsKey = KEYS[1],
+         local itemsKey = KEYS[2],
+         local itemsByViewsKey = KEYS[3],
+         local itemId = ARGV[1],
+         localuserId = ARGV[2],
+
+         local inserted = redis.call('PFADD', itemsViewsKey, userId),
+         if inserted == 1 then
+            redis.call('HINCRBY', itemsKey, 'views', 1)
+            redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+         end
+         `,
 			// Step#4. Mapping how the arguments passed onto EVALSHA when calling addOneAndStore()
-			transformArguments(key: string, value: number) {
-				return [key, value.toString()];
-				/* 
-            ['books:count', '5']
-            EVALSHA <ID> 1 'books:count' '5'
-             */
+			transformArguments(itemId: string, userId: string) {
+				return [
+					itemsViewsKey(itemId), // -> items:views#asdf
+					itemsKey(itemId), // -> items#asdf
+					itemsByViewsKey(), // -> items:views
+					itemId, // -> asdf
+					userId // -> u123
+				];
+				// EVALSHA <ID> 3 items:views#asdf items#asdf items:views asdf u123
 			},
 			// Step#5. Define what to do with the reply
-			transformReply(reply: any) {
-				return reply;
-			}
+			// Do nothing with the returned value
+			transformReply() {}
 		})
+		// // DEMO DEFINITION
+		// // Step#1. Adds additional methods to the client object
+		// addOneAndStore: defineScript({
+		// 	// Step#3. Number of keys to be expected by EVALSHA call
+		// 	NUMBER_OF_KEYS: 1,
+		// 	// Step#2. Define the LUA script
+		// 	SCRIPT: "return redis.call('SET', KEYS[1], 1+tonumber(ARGV[1]))",
+		// 	// Step#4. Mapping how the arguments passed onto EVALSHA when calling addOneAndStore()
+		// 	transformArguments(key: string, value: number) {
+		// 		return [key, value.toString()];
+		// 		/*
+		//       ['books:count', '5']
+		//       EVALSHA <ID> 1 'books:count' '5'
+		//        */
+		// 	},
+		// 	// Step#5. Define what to do with the reply
+		// 	transformReply(reply: any) {
+		// 		return reply;
+		// 	}
+		// })
 	}
 });
 
-// DEMO LUA SCRIPTION PROCESS
-client.on('connect', async () => {
-	// Upload Script
-	await client.addOneAndStore('books:count', 5);
-	// Ask for result
-	const result = await client.get('books:count');
-	console.log(result);
-});
+// // DEMO LUA SCRIPTION PROCESS
+// client.on('connect', async () => {
+// 	// Upload Script
+// 	await client.addOneAndStore('books:count', 5);
+// 	// Ask for result
+// 	const result = await client.get('books:count');
+// 	console.log(result);
+// });
 
 client.on('error', (err) => console.error(err));
 client.connect();
