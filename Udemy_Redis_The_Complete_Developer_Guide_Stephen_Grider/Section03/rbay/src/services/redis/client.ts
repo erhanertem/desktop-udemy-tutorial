7e22209327717e9043a66607210d09055f0f2b61
Erhan Ertem <eertem@gmail.com>
2024-08-15 16:15:48 +0300

    Create search index when app launches

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/redis/client.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/redis/client.ts
@@ -1,5 +1,6 @@
 import { itemsByViewsKey, itemsKey, itemsViewsKey } from '$services/keys';
 import { createClient, defineScript } from 'redis';
+import { createIndexes } from './create-indexes';
 
 const client = createClient({
 	socket: {
@@ -91,5 +92,13 @@ const client = createClient({
 
 client.on('error', (err) => console.error(err));
 client.connect();
+
+client.on('connect', async () => {
+	try {
+		await createIndexes();
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 export { client };
