93bc4bc1048d7b3b0093e05be869ddfdcfe0e316
Erhan Ertem <eertem@gmail.com>
2024-08-15 15:21:04 +0300

    Disallow re-creating the existing index

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/redis/create-indexes.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/redis/create-indexes.ts
@@ -3,6 +3,13 @@ import { SchemaFieldTypes } from 'redis';
 import { itemsIndexKey, itemsKey } from '$services/keys';
 
 export const createIndexes = async () => {
+	// GUARD CLAUSE - if index already exists do not attempt to create a new one else app give give an error for attempting to create an existing one
+	const indexes = await client.ft._list();
+	const exists = indexes.find((index) => index === itemsIndexKey());
+	if (exists) {
+		return;
+	}
+
 	// Implement indexes creation logic here
 	return client.ft.create(
 		itemsIndexKey(),
