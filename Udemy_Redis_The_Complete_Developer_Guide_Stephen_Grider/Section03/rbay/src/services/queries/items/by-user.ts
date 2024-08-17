4666ee9facabddda9564afffdb58398f18c06562
Erhan Ertem <eertem@gmail.com>
2024-08-17 14:58:20 +0300

    Build itemsbyuser dashboard with sorting/pagination

--- Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/items/by-user.ts
+++ Udemy_Redis_The_Complete_Developer_Guide_Stephen_Grider/Section03/rbay/src/services/queries/items/by-user.ts
@@ -1,3 +1,7 @@
+import { client } from '$services/redis';
+import { itemsIndexKey } from '$services/keys';
+import { deserialize } from './deserialize';
+
 interface QueryOpts {
 	page: number;
 	perPage: number;
@@ -5,4 +9,20 @@ interface QueryOpts {
 	direction: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+export const itemsByUser = async (userId: string, opts: QueryOpts) => {
+	// The curlies { ${....} } is because of using TAG schemafield type element
+	const query = `@ownerId:{${userId}}`;
+	const sortCriteria = opts.sortBy && opts.direction && { BY: opts.sortBy, DIRECTION: opts.direction };
+	const { total, documents } = await client.ft.search(itemsIndexKey(), query, {
+		ON: 'hash',
+		SORTBY: sortCriteria,
+		LIMIT: { from: opts.page * opts.perPage, size: opts.perPage },
+	} as any);
+	console.log(total, documents);
+	// Implement the logic to fetch items from the Redis database
+	// using the provided userId and query options
+	return {
+		totalPages: Math.ceil(total / opts.perPage),
+		items: documents.map(({ id, value }) => deserialize(id.replace('items#', ''), value as any)),
+	};
+};
