3c2f47209041e2d769d9ed7c297f5249e5aff426
Erhan Ertem <eertem@gmail.com>
2024-08-21 20:46:01 +0300

    Udemy The Ultimate React Course 2024 React Redux and More Jonas Schmedtmann - Section 06 - State event forms components / Date Counter v1 Exercise (Revamp)

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/06-date-counter-v1/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/06-date-counter-v1/src/App.js
@@ -2,101 +2,44 @@ import { useState } from 'react';
 import './styles.css';
 
 export default function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Counter />
+		</div>
+	);
 }
 
 function Counter() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
+	const [step, setStep] = useState(1);
 
-  const date = new Date();
-  date.setDate(date.getDate() + count);
+	const date = new Date('june 21 2027');
+	date.setDate(date.getDate() + count);
 
-  function decreaseStep(step) {
-    if (step > 1) {
-      setStep((s) => s - 1);
-    }
-  }
-  function increaseStep() {
-    setStep((s) => s + 1);
-  }
-
-  function decreaseCount(step) {
-    setCount((c) => c - step);
-  }
-  function increaseCount(step) {
-    console.log(step);
-    setCount((c) => c + step);
-  }
-
-  return (
-    <div>
-      <div>
-        <button onClick={() => decreaseStep(step)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={increaseStep}>+</button>
-      </div>
-      <div>
-        <button onClick={() => decreaseCount(step)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => increaseCount(step)}>+</button>
-      </div>
-
-      <p>
-        <span>
-          {count === 0
-            ? 'Today is '
-            : count > 0
-            ? `${count} days from today is `
-            : `${Math.abs(count)} days ago was `}
-        </span>
-        <span>{date.toDateString()}</span>
-      </p>
-    </div>
-  );
+	return (
+		<div>
+			<br />
+			<div>
+				<button onClick={() => setStep((step) => step - 1)}>-</button>
+				<span>Step: {step}</span>
+				<button onClick={() => setStep((step) => step + 1)}>+</button>
+			</div>
+			<br />
+			<div>
+				<button onClick={() => setCount((count) => count - step)}>-</button>
+				<span>Count: {count}</span>
+				<button onClick={() => setCount((count) => count + step)}>+</button>
+			</div>
+			<p>
+				<span>
+					{count === 0
+						? 'Today is '
+						: count > 0
+						? `${count} days from today is `
+						: `${count} days ago was `}
+				</span>
+				<span>{date.toDateString()}</span>
+			</p>
+		</div>
+	);
 }
-
-// function Counter() {
-// 	const [count, setCount] = useState(0);
-// 	const [step, setStep] = useState(1);
-
-// 	const date = new Date('june 21 2027');
-// 	date.setDate(date.getDate() + count);
-
-// 	function decreaseStep(step) {
-// 		if (step > 1) {
-// 			setStep((c) => c - 1);
-// 		}
-// 	}
-
-// 	return (
-// 		<div>
-// 			<div>
-// 				<button onClick={() => decreaseStep(step)}>-</button>
-// 				<span>Step: {step}</span>
-// 				<button onClick={() => setStep((c) => c + 1)}>+</button>
-// 			</div>
-
-// 			<div>
-// 				<button onClick={() => setCount((c) => c - step)}>-</button>
-// 				<span>Count: {count}</span>
-// 				<button onClick={() => setCount((c) => c + step)}>+</button>
-// 			</div>
-
-// 			<p>
-// 				<span>
-// 					{count === 0
-// 						? 'Today is '
-// 						: count > 0
-// 						? `${count} days from today is `
-// 						: `${Math.abs(count)} days ago was `}
-// 				</span>
-// 				<span>{date.toDateString()}</span>
-// 			</p>
-// 		</div>
-// 	);
-// }
