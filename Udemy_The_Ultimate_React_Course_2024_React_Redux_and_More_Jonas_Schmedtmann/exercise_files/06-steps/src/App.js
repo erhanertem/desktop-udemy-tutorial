91d86d9ec17a5c615d04d643f36390b381868c9d
Erhan Ertem <eertem@gmail.com>
2024-08-21 20:24:37 +0300

    Udemy The Ultimate React Course 2024 React Redux and More Jonas Schmedtmann - Section 06 - State event forms components / Steps Component Exercise (Revamp)

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/06-steps/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/06-steps/src/App.js
@@ -1,77 +1,60 @@
 import { useState } from 'react';
 
 const messages = [
-  'Learn React ✅',
-  'Apply for jobs 💼',
-  'Invest your new income 💵',
+	'Learn React ⚛️',
+	'Apply for jobs 💼',
+	'Invest your new income 🤑',
 ];
 
-export default function App() {
-  return (
-    <div>
-      <Steps />
-      <Steps />
-    </div>
-  );
+function App() {
+	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
+
+	function handlePrevious() {
+		if (step > 1) {
+			setStep((step) => (step % 3) - 1);
+		}
+	}
+	function handleNext() {
+		setStep((step) => (step % 3) + 1);
+	}
+
+	return (
+		<>
+			<button
+				className="close"
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				&times;
+			</button>
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step >= 1 ? 'active' : ''}>1</div>
+						<div className={step >= 2 ? 'active' : ''}>2</div>
+						<div className={step >= 3 ? 'active' : ''}>3</div>
+					</div>
+					<p className="message">
+						Step {step}: {messages[step - 1]}
+					</p>
+					<div className="buttons">
+						<button
+							style={{ backgroundColor: '#7950f2', color: '#fff' }}
+							onClick={handlePrevious}
+						>
+							Previous
+						</button>
+						<button
+							style={{ backgroundColor: '#7950f2', color: '#fff' }}
+							onClick={handleNext}
+						>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
-function Steps() {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
-  // const [test, setTest] = useState({ name: 'Jonas' });
-
-  function handlePrevious() {
-    if (step > 1) setStep((s) => s - 1);
-  }
-  function handleNext() {
-    if (step < 3) {
-      setStep((s) => s + 1);
-      //   setStep((s) => s + 1);
-    }
-    //BAD PRACTICE
-    // test.name = 'Fred';
-    // setTest({ name: 'Fred' });
-  }
-
-  return (
-    <>
-      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
-        &times;
-      </button>
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? 'active' : ''}>1</div>
-            <div className={step >= 2 ? 'active' : ''}>2</div>
-            <div className={step >= 3 ? 'active' : ''}>3</div>
-          </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-            {/* {test.name} */}
-          </p>
-
-          <div className="buttons">
-            <button
-              style={{
-                backgroundColor: '#7950f2',
-                color: '#fff',
-              }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{
-                backgroundColor: '#7950f2',
-                color: '#fff',
-              }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
-}
+export default App;
