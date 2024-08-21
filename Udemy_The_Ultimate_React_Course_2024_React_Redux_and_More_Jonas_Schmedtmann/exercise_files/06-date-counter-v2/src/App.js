1e17032c92cb4843a04b69839d508febcef222cb
Erhan Ertem <eertem@gmail.com>
2024-08-21 23:34:39 +0300

    Udemy The Ultimate React Course 2024 React Redux and More Jonas Schmedtmann - Section 06 - State event forms components / Date Counter v2 Exercise (Revamp)

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/06-date-counter-v2/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/06-date-counter-v2/src/App.js
@@ -13,50 +13,48 @@ function Counter() {
 	const [count, setCount] = useState(0);
 	const [step, setStep] = useState(1);
 
-	const date = new Date();
-	date.setDate(date.getDate() + count);
-
 	function handleReset() {
 		setCount(0);
 		setStep(1);
 	}
 
+	const date = new Date('june 21 2027');
+	date.setDate(date.getDate() + count);
+
 	return (
 		<div>
+			<br />
 			<div>
 				<input
 					type="range"
-					id="step"
 					min="1"
 					max="10"
 					value={step}
-					onChange={(event) => setStep(Number(event.target.value))}
-					name="Step"
+					onChange={(e) => setStep(Number(e.target.value))}
 				/>
-				<label for="step">Step : {step}</label>
+				<span>Step: {step}</span>
 			</div>
-
+			<br />
 			<div>
-				<button onClick={() => setCount((c) => c - step)}>-</button>
+				<button onClick={() => setCount((count) => count - step)}>-</button>
 				<input
 					type="text"
 					value={count}
-					onChange={(event) => setCount(Number(event.target.value))}
+					onChange={(e) => setCount(Number(e.target.value))}
 				></input>
-				<button onClick={() => setCount((c) => c + step)}>+</button>
+				<button onClick={() => setCount((count) => count + step)}>+</button>
 			</div>
-
 			<p>
 				<span>
 					{count === 0
 						? 'Today is '
 						: count > 0
 						? `${count} days from today is `
-						: `${Math.abs(count)} days ago was `}
+						: `${count} days ago was `}
 				</span>
 				<span>{date.toDateString()}</span>
 			</p>
-
+			{/* CONDITIONALLY SHOW THE RESET BUTTON IF ANY OF THE ITEMS ARE NOT @ THEIR DEFAULT */}
 			{(count !== 0 || step !== 1) && (
 				<div>
 					<button onClick={handleReset}>Reset</button>
