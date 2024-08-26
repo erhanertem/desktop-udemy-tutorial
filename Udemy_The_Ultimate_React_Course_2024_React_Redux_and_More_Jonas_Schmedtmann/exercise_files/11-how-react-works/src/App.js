27d07b704ed4aeea871c16da47fc276834872515
Erhan Ertem <eertem@gmail.com>
2024-08-26 11:27:07 +0300

    Udemy The Ultimate React Course 2024 React Redux and More Jonas Schmedtmann - Section 11 - How React Works Behind the Scenes  / Exercise around rendering mechanism, states, eventhandlers, events, key props (Revamp)

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/11-how-react-works/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/11-how-react-works/src/App.js
@@ -1,121 +1,150 @@
 import { useState } from 'react';
 
 const content = [
-	{
-		summary: 'React is a library for building UIs',
-		details:
-			'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	},
-	{
-		summary: 'State management is like giving state a home',
-		details:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	},
-	{
-		summary: 'We can think of props as the component API',
-		details:
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-	},
+  {
+    summary: 'React is a library for building UIs',
+    details:
+      'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    summary: 'State management is like giving state a home',
+    details:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    summary: 'We can think of props as the component API',
+    details:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
 ];
 
 export default function App() {
-	return (
-		<div>
-			<Tabbed content={content} />
-		</div>
-	);
+  return (
+    <div>
+      <Tabbed content={content} />
+    </div>
+  );
 }
 
-// console.log(<DifferentContent test={23} />);
+// console.log(JSON.stringify(<DifferentContent test={23} />));
+// console.log(JSON.stringify(DifferentContent()));
 
 function Tabbed({ content }) {
-	const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
+  console.log(activeTab);
+  return (
+    <div>
+      <div className="tabs">
+        <Tab
+          num={0}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+        <Tab
+          num={1}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+        <Tab
+          num={2}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+        <Tab
+          num={3}
+          activeTab={activeTab}
+          onClick={setActiveTab}
+        />
+      </div>
 
-	return (
-		<div>
-			<div className="tabs">
-				<Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
-				<Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
-				<Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
-				<Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
-			</div>
+      {activeTab <= 2 ? (
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
+      ) : (
+        <DifferentContent />
+      )}
 
-			{activeTab <= 2 ? (
-				<TabContent item={content.at(activeTab)} key={content.at(activeTab).summary} />
-			) : (
-				<DifferentContent />
-			)}
-		</div>
-	);
+      {/* {TabContent({ item: content.at(0) })} */}
+    </div>
+  );
 }
 
 function Tab({ num, activeTab, onClick }) {
-	return (
-		<button className={activeTab === num ? 'tab active' : 'tab'} onClick={() => onClick(num)}>
-			Tab {num + 1}
-		</button>
-	);
+  return (
+    <button
+      className={activeTab === num ? 'tab active' : 'tab'}
+      onClick={() => onClick(num)}
+    >
+      Tab {num + 1}
+    </button>
+  );
 }
 
 function TabContent({ item }) {
-	const [showDetails, setShowDetails] = useState(true);
-	const [likes, setLikes] = useState(0);
+  const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(0);
 
-	console.log('RENDER');
+  console.log('RENDER');
 
-	function handleInc() {
-		// setLikes(likes + 1);
-		setLikes((likes) => likes + 1);
-	}
-	function handleTripleInc() {
-		// setLikes(likes + 1);
-		// setLikes(likes + 1);
-		// setLikes(likes + 1);
+  function handleInc() {
+    setLikes((likes) => likes + 1);
+  }
+  function handleTripleInc() {
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // console.log(likes);
+    // setLikes(likes + 1);
 
-		setLikes((likes) => likes + 1);
-		setLikes((likes) => likes + 1);
-		setLikes((likes) => likes + 1);
-	}
+    // handleInc();
+    // handleInc();
+    // handleInc();
 
-	function handleUndo() {
-		setShowDetails(true);
-		setLikes(0);
-		console.log(likes);
-	}
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
 
-	function handleUndoLater() {
-		setTimeout(handleUndo, 2000);
-	}
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
+  }
 
-	return (
-		<div className="tab-content">
-			<h4>{item.summary}</h4>
-			{showDetails && <p>{item.details}</p>}
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
 
-			<div className="tab-actions">
-				<button onClick={() => setShowDetails((h) => !h)}>
-					{showDetails ? 'Hide' : 'Show'} details
-				</button>
+  return (
+    <div className="tab-content">
+      <h4>{item.summary}</h4>
+      {showDetails && <p>{item.details}</p>}
 
-				<div className="hearts-counter">
-					<span>{likes} ❤️</span>
-					<button onClick={handleInc}>+</button>
-					<button onClick={handleTripleInc}>+++</button>
-				</div>
-			</div>
+      <div className="tab-actions">
+        <button onClick={() => setShowDetails((h) => !h)}>
+          {showDetails ? 'Hide' : 'Show'} details
+        </button>
 
-			<div className="tab-undo">
-				<button onClick={handleUndo}>Undo</button>
-				<button onClick={handleUndoLater}>Undo in 2s</button>
-			</div>
-		</div>
-	);
+        <div className="hearts-counter">
+          <span>{likes} ❤️</span>
+          <button onClick={handleInc}>+</button>
+          <button onClick={handleTripleInc}>+++</button>
+        </div>
+      </div>
+
+      <div className="tab-undo">
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
+      </div>
+    </div>
+  );
 }
 
 function DifferentContent() {
-	return (
-		<div className="tab-content">
-			<h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
-		</div>
-	);
+  return (
+    <div className="tab-content">
+      <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
+    </div>
+  );
 }
