53c7864ca9acc67d8c773e15d7179d18d8849caf
Erhan Ertem <eertem@gmail.com>
2024-08-30 16:56:29 +0300

    Udemy The Ultimate React Course 2024 React Redux and More Jonas Schmedtmann - Section 13 - Exercise useGeoLocation custom hook (Revamp)

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/13-geolocation/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/13-geolocation/src/App.js
@@ -1,69 +1,71 @@
 import { useState } from 'react';
 
 function useGeolocation() {
-	const [isLoading, setIsLoading] = useState(false);
-	const [position, setPosition] = useState({});
-	const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
 
-	function getPosition() {
-		if (!navigator.geolocation) return setError('Your browser does not support geolocation');
+  function getPosition() {
+    if (!navigator.geolocation) return setError('Your browser does not support geolocation');
 
-		setIsLoading(true);
-		navigator.geolocation.getCurrentPosition(
-			(pos) => {
-				setPosition({
-					lat: pos.coords.latitude,
-					lng: pos.coords.longitude,
-				});
-				setIsLoading(false);
-			},
-			(error) => {
-				setError(error.message);
-				setIsLoading(false);
-			},
-		);
-	}
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
 
-	return { getPosition, isLoading, error, position };
+  return { getPosition, isLoading, error, position };
 }
 
 export default function App() {
-	const {
-		getPosition,
-		isLoading,
-		error,
-		position: { lat, lng },
-	} = useGeolocation();
-	const [countClicks, setCountClicks] = useState(0);
-	// const { lat, lng } = position;
+  const {
+    getPosition,
+    isLoading,
+    error,
+    position: { lat, lng },
+  } = useGeolocation();
 
-	function handleClick() {
-		setCountClicks((count) => count + 1);
-		getPosition();
-	}
+  const [countClicks, setCountClicks] = useState(0);
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
 
-	return (
-		<div>
-			<button onClick={handleClick} disabled={isLoading}>
-				Get my position
-			</button>
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        disabled={isLoading}
+      >
+        Get my position
+      </button>
 
-			{isLoading && <p>Loading position...</p>}
-			{error && <p>{error}</p>}
-			{!isLoading && !error && lat && lng && (
-				<p>
-					Your GPS position:{' '}
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
-					>
-						{lat}, {lng}
-					</a>
-				</p>
-			)}
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
 
-			<p>You requested position {countClicks} times</p>
-		</div>
-	);
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
 }
