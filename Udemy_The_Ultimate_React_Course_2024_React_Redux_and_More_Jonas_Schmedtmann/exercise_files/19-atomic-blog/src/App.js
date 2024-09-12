6c0a9b8b873f04bef6073552d7f0f9218a75b3f4
Erhan Ertem <eertem@gmail.com>
2024-09-12 23:02:14 +0300

    Make use of useMemo hook for an object prop in a memoized component

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/App.js
@@ -4,194 +4,196 @@ import { PostProvider, usePosts } from './PostContext';
 import Test from './Test';
 
 function createRandomPost() {
-  return {
-    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    body: faker.hacker.phrase(),
-  };
+	return {
+		title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+		body: faker.hacker.phrase(),
+	};
 }
 
 function App() {
-  // const x = usePosts();
-  // console.log(x);
+	// const x = usePosts();
+	// console.log(x);
 
-  const [isFakeDark, setIsFakeDark] = useState(false);
+	const [isFakeDark, setIsFakeDark] = useState(false);
 
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-  useEffect(
-    function () {
-      document.documentElement.classList.toggle('fake-dark-mode');
-    },
-    [isFakeDark]
-  );
+	// Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+	useEffect(
+		function () {
+			document.documentElement.classList.toggle('fake-dark-mode');
+		},
+		[isFakeDark]
+	);
 
-  return (
-    // #2. WRAP THE ENTIRE APP WITH CUSTOM CONTEXT PROVIDER TO PROVIDE CHILD COMPONENTS THE GLOBAL VALUES/METHODS
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? '☀️' : '🌙'}
-      </button>
+	return (
+		// #2. WRAP THE ENTIRE APP WITH CUSTOM CONTEXT PROVIDER TO PROVIDE CHILD COMPONENTS THE GLOBAL VALUES/METHODS
+		<section>
+			<button
+				onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+				className="btn-fake-dark-mode"
+			>
+				{isFakeDark ? '☀️' : '🌙'}
+			</button>
 
-      {/* WRAP ONLY THE CHILD COMPONENTS THAT NEEDS TO SERVED BY THE CONTEXT PROVIDER */}
-      <PostProvider>
-        <Header />
-        <Main />
-        <Archive />
-        <Footer />
-      </PostProvider>
-    </section>
-  );
+			{/* WRAP ONLY THE CHILD COMPONENTS THAT NEEDS TO SERVED BY THE CONTEXT PROVIDER */}
+			<PostProvider>
+				<Header />
+				<Main />
+				<Archive />
+				<Footer />
+			</PostProvider>
+		</section>
+	);
 }
 
 function Header() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { posts, onClearPosts } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { posts, onClearPosts } = usePosts();
 
-  return (
-    <header>
-      <h1>
-        <span>⚛️</span>The Atomic Blog
-      </h1>
-      <div>
-        <Results posts={posts} />
-        <SearchPosts />
-        <button onClick={onClearPosts}>Clear posts</button>
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<h1>
+				<span>⚛️</span>The Atomic Blog
+			</h1>
+			<div>
+				<Results posts={posts} />
+				<SearchPosts />
+				<button onClick={onClearPosts}>Clear posts</button>
+			</div>
+		</header>
+	);
 }
 
 function SearchPosts() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { searchQuery, setSearchQuery } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { searchQuery, setSearchQuery } = usePosts();
 
-  return (
-    <input
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      placeholder="Search posts..."
-    />
-  );
+	return (
+		<input
+			value={searchQuery}
+			onChange={(e) => setSearchQuery(e.target.value)}
+			placeholder="Search posts..."
+		/>
+	);
 }
 
 function Results() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { posts } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { posts } = usePosts();
 
-  return <p>🚀 {posts.length} atomic posts found</p>;
+	return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
 function Main() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { posts, onAddPost } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { posts, onAddPost } = usePosts();
 
-  return (
-    <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
-    </main>
-  );
+	return (
+		<main>
+			<FormAddPost onAddPost={onAddPost} />
+			<Posts posts={posts} />
+		</main>
+	);
 }
 
 function Posts() {
-  return (
-    <section>
-      <List />
-    </section>
-  );
+	return (
+		<section>
+			<List />
+		</section>
+	);
 }
 
 function FormAddPost() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { onAddPost } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { onAddPost } = usePosts();
 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
 
-  const handleSubmit = function (e) {
-    e.preventDefault();
-    if (!body || !title) return;
-    onAddPost({ title, body });
-    setTitle('');
-    setBody('');
-  };
+	const handleSubmit = function (e) {
+		e.preventDefault();
+		if (!body || !title) return;
+		onAddPost({ title, body });
+		setTitle('');
+		setBody('');
+	};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Post title"
-      />
-      <textarea
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-        placeholder="Post body"
-      />
-      <button>Add post</button>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+				placeholder="Post title"
+			/>
+			<textarea
+				value={body}
+				onChange={(e) => setBody(e.target.value)}
+				placeholder="Post body"
+			/>
+			<button>Add post</button>
+		</form>
+	);
 }
 
 function List() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { posts } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { posts } = usePosts();
 
-  return (
-    <>
-      <ul>
-        {posts.map((post, i) => (
-          <li key={i}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
+	return (
+		<>
+			<ul>
+				{posts.map((post, i) => (
+					<li key={i}>
+						<h3>{post.title}</h3>
+						<p>{post.body}</p>
+					</li>
+				))}
+			</ul>
 
-      {/* <Test /> */}
-    </>
-  );
+			{/* <Test /> */}
+		</>
+	);
 }
 
 function Archive() {
-  // #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
-  const { onAddPost } = usePosts();
+	// #3. CONSUMING CONTEXT VALUES/METHODS OF THE POSTCONTEXT OBJECT
+	const { onAddPost } = usePosts();
 
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-  const [posts] = useState(() =>
-    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
-  );
+	// Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+	const [posts] = useState(() =>
+		// 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+		Array.from({ length: 10000 }, () => createRandomPost())
+	);
 
-  const [showArchive, setShowArchive] = useState(false);
+	const [showArchive, setShowArchive] = useState(false);
 
-  return (
-    <aside>
-      <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? 'Hide archive posts' : 'Show archive posts'}
-      </button>
+	return (
+		<aside>
+			<h2>Post archive</h2>
+			<button onClick={() => setShowArchive((s) => !s)}>
+				{showArchive ? 'Hide archive posts' : 'Show archive posts'}
+			</button>
 
-      {showArchive && (
-        <ul>
-          {posts.map((post, i) => (
-            <li key={i}>
-              <p>
-                <strong>{post.title}:</strong> {post.body}
-              </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </aside>
-  );
+			{showArchive && (
+				<ul>
+					{posts.map((post, i) => (
+						<li key={i}>
+							<p>
+								<strong>{post.title}:</strong> {post.body}
+							</p>
+							<button onClick={() => onAddPost(post)}>
+								Add as new post
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
+		</aside>
+	);
 }
 
 function Footer() {
-  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+	return <footer>&copy; by The Atomic Blog ✌️</footer>;
 }
 
 export default App;
