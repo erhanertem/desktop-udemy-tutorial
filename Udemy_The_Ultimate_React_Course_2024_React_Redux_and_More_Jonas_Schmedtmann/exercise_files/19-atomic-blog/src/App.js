24f67bc70089ba32393a12d6813e1adbfe5d0ad3
Erhan Ertem <eertem@gmail.com>
2024-09-12 23:23:49 +0300

    Make use of useCallback hook for functions in a memoized component

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 import { PostProvider, usePosts } from './PostContext';
-import Test from './Test';
+// import Test from './Test';
 
 function createRandomPost() {
 	return {
