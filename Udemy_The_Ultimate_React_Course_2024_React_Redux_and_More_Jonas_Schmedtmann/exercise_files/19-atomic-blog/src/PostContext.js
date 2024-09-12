1a213d0a6a4d54ffd258042501fe189553a811e8
Erhan Ertem <eertem@gmail.com>
2024-09-13 00:08:49 +0300

    Custom Context Provider state object memorization via useMemo

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/PostContext.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/PostContext.js
@@ -1,11 +1,11 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useMemo, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
-  return {
-    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    body: faker.hacker.phrase(),
-  };
+	return {
+		title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+		body: faker.hacker.phrase(),
+	};
 }
 
 // #1. CREATE A NEW CONTEXT PROVIDER COMPONENT OUTSIDE THE APP COMPONENT
@@ -14,48 +14,53 @@ function createRandomPost() {
 const PostContext = createContext();
 
 function PostProvider({ children }) {
-  // #2. PROVIDE CONTEXT VALUES/METHODS
-  const [posts, setPosts] = useState(() => Array.from({ length: 30 }, () => createRandomPost()));
-  const [searchQuery, setSearchQuery] = useState('');
+	// #2. PROVIDE CONTEXT VALUES/METHODS
+	const [posts, setPosts] = useState(() =>
+		Array.from({ length: 30 }, () => createRandomPost())
+	);
+	const [searchQuery, setSearchQuery] = useState('');
 
-  // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) => `${post.title} ${post.body}`.toLowerCase().includes(searchQuery.toLowerCase()))
-      : posts;
+	// Derived state. These are the posts that will actually be displayed
+	const searchedPosts =
+		searchQuery.length > 0
+			? posts.filter((post) =>
+					`${post.title} ${post.body}`
+						.toLowerCase()
+						.includes(searchQuery.toLowerCase())
+			  )
+			: posts;
 
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-  }
+	function handleAddPost(post) {
+		setPosts((posts) => [post, ...posts]);
+	}
 
-  function handleClearPosts() {
-    setPosts([]);
-  }
+	function handleClearPosts() {
+		setPosts([]);
+	}
 
-  // #3. RETURN CONTEXT WITH PROVIDERS
-  return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
-  );
+	const value = useMemo(() => {
+		return {
+			posts: searchedPosts,
+			onAddPost: handleAddPost,
+			onClearPosts: handleClearPosts,
+			searchQuery,
+			setSearchQuery,
+		};
+	}, [searchedPosts, searchQuery]);
+
+	// #3. RETURN CONTEXT WITH PROVIDERS
+	return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
 }
 
 // #4. CREATE CUSTOM HOOK FOR CONSUMING CONTEXT
 function usePosts() {
-  const context = useContext(PostContext);
+	const context = useContext(PostContext);
 
-  //GUARD CLAUSE
-  if (context === undefined) throw new Error('PostContext was used outside of the PostProvider');
+	//GUARD CLAUSE
+	if (context === undefined)
+		throw new Error('PostContext was used outside of the PostProvider');
 
-  return context;
+	return context;
 }
 
 /* 
--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/index.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-atomic-blog/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-// import App from "./App";
-import App from './App-memo';
+import App from './App';
+// import App from './App-memo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
