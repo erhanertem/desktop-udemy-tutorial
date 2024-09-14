5645490c7498f91492b5e1ec0563057e0e36afdb
Erhan Ertem <eertem@gmail.com>
2024-09-15 01:56:04 +0300

    Udemy The Ultimate React Course 2024 React Redux and More Jonas Schmedtmann - Section 19 - Workout Timer Exercise / Linking multiple useEffects (Revamp)

--- Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-workout-timer/src/App.js
+++ Udemy_The_Ultimate_React_Course_2024_React_Redux_and_More_Jonas_Schmedtmann/exercise_files/19-workout-timer/src/App.js
@@ -3,64 +3,69 @@ import Calculator from './Calculator';
 import ToggleSounds from './ToggleSounds';
 
 function formatTime(date) {
-	return new Intl.DateTimeFormat('en', {
-		month: 'short',
-		year: '2-digit',
-		hour: '2-digit',
-		minute: '2-digit',
-		second: '2-digit',
-	}).format(date);
+  return new Intl.DateTimeFormat('en', {
+    month: 'short',
+    year: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(date);
 }
 
 function App() {
-	const [allowSound, setAllowSound] = useState(true);
-	const [time, setTime] = useState(formatTime(new Date()));
+  const [allowSound, setAllowSound] = useState(true);
+  const [time, setTime] = useState(formatTime(new Date()));
 
-	// Will be be AM or PM
-	const partOfDay = time.slice(-2);
+  // Will be be AM or PM
+  const partOfDay = time.slice(-2);
 
-	// COMPLEMENTS TO MEMO @ CALCULATOR COMPONENT.ARRAY IS AN OBJECT LITERAL AND NEEDS TO BE PASSED BY USEMEMO TO MAKE IT SAME OBJECT
-	const workouts = useMemo(() => {
-		return [
-			{
-				name: 'Full-body workout',
-				numExercises: partOfDay === 'AM' ? 9 : 8,
-			},
-			{
-				name: 'Arms + Legs',
-				numExercises: 6,
-			},
-			{
-				name: 'Arms only',
-				numExercises: 3,
-			},
-			{
-				name: 'Legs only',
-				numExercises: 4,
-			},
-			{
-				name: 'Core only',
-				numExercises: partOfDay === 'AM' ? 5 : 4,
-			},
-		];
-	}, [partOfDay]);
+  const workouts = useMemo(() => {
+    return [
+      {
+        name: 'Full-body workout',
+        numExercises: partOfDay === 'AM' ? 9 : 8,
+      },
+      {
+        name: 'Arms + Legs',
+        numExercises: 6,
+      },
+      {
+        name: 'Arms only',
+        numExercises: 3,
+      },
+      {
+        name: 'Legs only',
+        numExercises: 4,
+      },
+      {
+        name: 'Core only',
+        numExercises: partOfDay === 'AM' ? 5 : 4,
+      },
+    ];
+  }, [partOfDay]);
 
-	useEffect(function () {
-		const id = setInterval(function () {
-			setTime(formatTime(new Date()));
-		}, 1000);
+  useEffect(function () {
+    const id = setInterval(function () {
+      setTime(formatTime(new Date()));
+    }, 1000);
 
-		return () => clearInterval(id);
-	}, []);
+    return () => clearInterval(id);
+  }, []);
 
-	return (
-		<main>
-			<h1>Workout timer</h1>
-			<time>For your workout on {time}</time>
-			<ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
-			<Calculator workouts={workouts} allowSound={allowSound} />
-		</main>
-	);
+  return (
+    <main>
+      <h1>Workout timer</h1>
+      <time>For your workout on {time}</time>
+      <ToggleSounds
+        allowSound={allowSound}
+        setAllowSound={setAllowSound}
+      />
+      <Calculator
+        workouts={workouts}
+        allowSound={allowSound}
+      />
+    </main>
+  );
 }
 
 export default App;
