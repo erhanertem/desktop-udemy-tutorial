7a83f0f7baf50a2d1b4dcbb3c4819b9eaab62d87
Erhan Ertem <eertem@gmail.com>
2024-12-04 12:15:36 +0300

    Establish controller for error handlers

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/app.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const express = require('express');
-const html = require('./util/html');
 
 const app = express();
 
@@ -12,6 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 // Middleware to parse application/x-www-form-urlencoded data (expressjs)
 app.use(express.urlencoded({ extended: true }));
@@ -27,41 +27,10 @@ app.use(shopRoutes);
 
 // ERROR HANDLING
 // > 404 Middleware for non-existent routes
-app.use((req, res, next) => {
-	res.render('404', {
-		pageTitle: 'Page Not Found',
-		layout: false,
-		path: '',
-		styles: '<link rel="stylesheet" href="/css/main.css" >',
-	});
-});
+app.use(errorController.get404);
 
 // > Error-handling middleware
-app.use((err, req, res, next) => {
-	console.error(err.stack); // Log the error
-
-	// Check for specific errors, like JSON parsing issues
-	if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-		return res.status(400).send(html`
-			<html>
-				<head>
-					<title>Error</title>
-				</head>
-				<body>
-					<h1>Invalid Request Body</h1>
-					<p>You will be redirected shortly...</p>
-					<script>
-						setTimeout(() => {
-							window.location.href = '/';
-						}, 1500);
-					</script>
-				</body>
-			</html>
-		`);
-	}
-
-	res.status(500).send('Internal Server Error'); // Generic error response
-});
+app.use(errorController.get500);
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
---
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/controllers/error.js
@@ -0,0 +1,36 @@
+const html = require('../util/html');
+
+exports.get404 = (req, res, next) => {
+	res.render('404', {
+		pageTitle: 'Page Not Found',
+		layout: false,
+		path: '',
+		styles: '<link rel="stylesheet" href="/css/main.css" >',
+	});
+};
+
+exports.get500 = (err, req, res, next) => {
+	console.error(err.stack); // Log the error
+
+	// Check for specific errors, like JSON parsing issues
+	if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+		return res.status(400).send(html`
+			<html>
+				<head>
+					<title>Error</title>
+				</head>
+				<body>
+					<h1>Invalid Request Body</h1>
+					<p>You will be redirected shortly...</p>
+					<script>
+						setTimeout(() => {
+							window.location.href = '/';
+						}, 1500);
+					</script>
+				</body>
+			</html>
+		`);
+	}
+
+	res.status(500).send('Internal Server Error'); // Generic error response
+};
