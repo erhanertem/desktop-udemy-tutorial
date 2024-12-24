12c1ea6b124ae1631a518aa80fe82acc55b7a9f1
Erhan Ertem <eertem@gmail.com>
2024-12-24 20:43:18 +0300

    Add same product to the cart

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/controllers/shop.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/controllers/shop.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
-const CartItem = require('../models/cart-item');
 
 exports.getProducts = async (req, res, next) => {
 	try {
@@ -62,17 +61,19 @@ exports.postCart = async (req, res, next) => {
 	let updatedQuantity;
 	// If it is in the cart, update the quantity of the product
 	if (productInstance.length) {
-		console.log('Hello');
 		product = productInstance[0];
+		// Update the quantity - sequelize provides access to quantity field of the cartItem as product and cartItem is linked
 		updatedQuantity = product.cartItem.quantity + 1;
 	} else {
 		// If there is no product instance, grap the product info
 		product = await Product.findByPk(productId);
 		// Set the quantity to 1
 		updatedQuantity = 1;
-		// Add the product to the user's current cart + update the quantity @ corresponding junction table
-		await fetchedCart.addProduct(product, { through: { quantity: updatedQuantity } });
 	}
+
+	// Add the product to the user's current cart + update the quantity @ corresponding junction table
+	await fetchedCart.addProduct(product, { through: { quantity: updatedQuantity } });
+
 	// Redirect to the cart page
 	res.redirect('/cart');
 };
