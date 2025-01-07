8e96c55f166f0808d0c5fe822f6bb6410317fdd8
Erhan Ertem <eertem@gmail.com>
2025-01-08 00:22:50 +0300

    Create product schema and name it as Product for referencing in mongoose

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/models/product.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/models/product.js
@@ -1,56 +1,17 @@
-// const { ObjectId } = require('mongodb');
-// const { db } = require('../util/nosqldatabase');
-// // const { getDb } = require('../util/nosqldatabase');
+// // Alt#1
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// module.exports = mongoose.model('Product');
 
-// class Product {
-// 	constructor(title, price, description, imageUrl, id, userId) {
-// 		this.title = title;
-// 		this.imageUrl = imageUrl;
-// 		this.description = description;
-// 		this.price = price;
-// 		this._id = id ? ObjectId.createFromHexString(id) : null; // ObjectId.createFromHexString() throws error if id by default is given null. So, we need to check if id is null or not
-// 		this.userId = userId; // Identify the product submitter
-// 	}
+// Alt#2
+const { Schema, model } = require('mongoose');
 
-// 	// Save the product to the database - Note: its not a static method because it needs to be called onto a product instance prodyct.save()... not Product.save()...
-// 	saveProduct() {
-// 		// const db = getDb();
-// 		let dbOperation;
-// 		if (this._id) {
-// 			// Update the product with given Id
-// 			dbOperation = db().collection('products').updateOne({ _id: this._id }, { $set: this });
-// 		} else {
-// 			// Create a new product
-// 			dbOperation = db().collection('products').insertOne(this);
-// 		}
+const productSchema = new Schema({
+	title: { type: String, required: true },
+	price: { type: Number, required: true },
+	description: { type: String, required: true },
+	imageUrl: { type: String, required: true },
+	userId: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // 'User' is the name of the model we created in user.js file. ref: 'User' refers to the User model
+});
 
-// 		return dbOperation;
-// 	}
-
-// 	// Fetch all products from the database
-// 	static fetchAllProducts() {
-// 		// const db = getDb();
-// 		const productCollection = db().collection('products');
-// 		return productCollection.find().toArray(); // Returns all products w/ no pagination - be cautious
-// 	}
-
-// 	static findProductById(productId) {
-// 		// const db = getDb();
-// 		const productCollection = db().collection('products');
-// 		// Convert productId to ObjectId - The _id field in MongoDB is not a plain string; it is a special type called ObjectId. So, we need to convert the string ID to an ObjectId before we can use it in a query.
-// 		const objectId = ObjectId.createFromHexString(productId);
-
-// 		return productCollection.findOne({ _id: objectId });
-// 	}
-
-// 	static deleteById(productId) {
-// 		// const db = getDb();
-// 		const productCollection = db().collection('products');
-// 		const objectId = ObjectId.createFromHexString(productId);
-// 		console.log('objectId :', objectId);
-
-// 		return productCollection.deleteOne({ _id: objectId });
-// 	}
-// }
-
-// module.exports = Product;
+module.exports = model('Product', productSchema); // Export the model with a name called 'Product' w/ a schema design as established by productSchema
