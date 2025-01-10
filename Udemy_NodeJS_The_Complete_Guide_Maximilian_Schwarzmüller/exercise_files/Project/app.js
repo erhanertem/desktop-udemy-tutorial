f36efe33286dc10902b05653782046b03cc201e6
Erhan Ertem <eertem@gmail.com>
2025-01-11 00:51:25 +0300

    Reinstate dummy user creation and temporary dummy user middleware to complete client side actions

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/app.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/app.js
@@ -14,8 +14,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
-// TEMP - DISABLED
-// const User = require('./models/user');
+const User = require('./models/user');
 
 // Middleware to parse application/x-www-form-urlencoded data (expressjs)
 app.use(express.urlencoded({ extended: true }));
@@ -33,23 +32,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // EXPRESSJS MIDDLEWARE
 
 // Manage a dummy user till authentication is established
-// TEMP - DISABLED
-// app.use((req, res, next) => {
-// 	User.findUserById('6776556d8efe3687a09553a6')
-// 		.then((user) => {
-// 			if (user) {
-// 				console.log('Found user');
-// 				req.user = new User(user.name, user.email, user.cart, user._id); // Create a user object from the fetched user data help us solicit User object functions as well.
-// 				next();
-// 			} else {
-// 				throw new Error('User not found');
-// 			}
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 			next(err); // Pass error to error-handling middleware
-// 		});
-// });
+app.use((req, res, next) => {
+	User.findById('67818dc96d1f446bb00f1402')
+		.then((user) => {
+			if (user) {
+				console.log('Found user');
+				req.user = new User(user.name, user.email, user.cart, user._id); // Create a user object from the fetched user data help us solicit User object functions as well.
+				next();
+			} else {
+				throw new Error('User not found');
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			next(err); // Pass error to error-handling middleware
+		});
+});
 
 // Express Routers
 app.use('/admin', adminRoutes);
@@ -65,10 +63,25 @@ app.use(errorController.get500);
 // Server initializer with mongoDB in IEFF style
 (async () => {
 	try {
+		// Connect to mongoDB
 		await mongoose.connect(
 			`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_USER_PASS}@shop.vs6wu.mongodb.net/shop?retryWrites=true&w=majority`
 		);
 
+		// TEMP - Create a dummy user after connecting to MongoDB
+		const isThereAnyUser = await User.findOne();
+		if (!isThereAnyUser) {
+			// Create a new dummy user
+			const user = new User({
+				name: 'Ernie',
+				email: 'e@e.com',
+				cart: { items: [] },
+			});
+			// Save to DB
+			await user.save();
+		}
+
+		// Initialize backend server
 		app.listen(process.env.PORT, process.env.DB_HOST, () => {
 			console.log(
 				`Listening on port ${process.env.PORT}, running on DB_HOST:${process.env.DB_HOST} in ${
