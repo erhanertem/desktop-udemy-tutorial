46faada8ff45ce70808cffba554865162cbcf983
Erhan Ertem <eertem@gmail.com>
2025-01-15 21:11:53 +0300

    Setup/configure express-session middleware

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/app.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // Load appropriate .env file based on NODE_ENV
@@ -17,12 +18,6 @@ const User = require('./models/user');
 // Init Express App
 const app = express();
 
-// Parse JSON Data
-// Middleware to parse application/x-www-form-urlencoded data (expressjs)
-app.use(express.urlencoded({ extended: true }));
-// Middleware to parse application/json data (expressjs)
-app.use(express.json());
-
 // Desiginate a template engine to use
 app.set('view engine', 'ejs');
 // Tell where the template engine files are located
@@ -30,6 +25,19 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve static content folder
 app.use(express.static(path.join(__dirname, 'public')));
+// Parse JSON Data
+// Middleware to parse application/x-www-form-urlencoded data (expressjs)
+app.use(express.urlencoded({ extended: true }));
+// Middleware to parse application/json data (expressjs)
+app.use(express.json());
+// Handle user sessions for stateful cookie-based authentication
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET, // Session secret
+		resave: false, //  The session will not be saved back to the session store on every response, even if the session was never modified during the request. This can help improve performance by reducing unnecessary writes to the session store.
+		saveUninitialized: false, // A session will not be created and saved to the session store unless it is modified. This helps reduce storage usage by only saving sessions that have meaningful data.
+	})
+);
 
 // EXPRESSJS MIDDLEWARE
 
