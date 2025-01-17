684b39b7a5a2921e7f5607376aeaabe8d7345ae4
Erhan Ertem <eertem@gmail.com>
2025-01-17 11:44:31 +0300

    Fix bug @ error handler - missing authentication variable passed onto ejs

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/controllers/error.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/controllers/error.js
@@ -4,6 +4,7 @@ exports.get404 = (req, res, next) => {
 		layout: false,
 		path: '',
 		styles: '<link rel="stylesheet" href="/css/main.css" >',
+		isAuthenticated: !!req.session.isLoggedIn, // Per postLogin value @ auth.js
 	});
 };
 
@@ -12,7 +13,12 @@ exports.get500 = (err, req, res, next) => {
 
 	// Check for specific errors, like JSON parsing issues
 	if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-		return res.status(400).render('errors/400', { pageTitle: 'Error', path: '', message: 'Invalid Request Body' });
+		return res.status(400).render('errors/400', {
+			pageTitle: 'Error',
+			path: '',
+			message: 'Invalid Request Body',
+			isAuthenticated: !!req.session.isLoggedIn, // Per postLogin value @ auth.js
+		});
 	}
 
 	res.status(500).send('Internal Server Error'); // Generic error response
