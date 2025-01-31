69d4d21014b909b993546ab16e9d0cdd1c5b8eac
Erhan Ertem <eertem@gmail.com>
2025-02-01 00:43:05 +0300

    Provide validators on forms @ add/edit admin product endpoints

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/routes/admin.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, param, query, body, header } = require('express-validator');
 
 const adminController = require('../controllers/admin');
 
@@ -11,7 +12,17 @@ const router = express.Router();
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // POST /admin/add-product
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post(
+	'/add-product',
+	[
+		body('title').isAlphanumeric().isLength({ min: 3 }).trim(),
+		body('imageUrl').isURL(),
+		body('price').isFloat(),
+		body('description').isLength({ min: 5, max: 400 }).trim(),
+	],
+	isAuth,
+	adminController.postAddProduct
+);
 
 // GET /admin/list-products
 router.get('/list-products', isAuth, adminController.getAllProducts);
@@ -20,7 +31,17 @@ router.get('/list-products', isAuth, adminController.getAllProducts);
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
 // POST /admin/edit-product
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post(
+	'/edit-product',
+	[
+		body('title').isAlphanumeric().isLength({ min: 3 }).trim(),
+		body('imageUrl').isURL(),
+		body('price').isFloat(),
+		body('description').isLength({ min: 5, max: 400 }).trim(),
+	],
+	isAuth,
+	adminController.postEditProduct
+);
 
 // POST /admin/delete-product
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
