fda166dbd9cb28e91ebdfb39652ebe6f6bb76844
Erhan Ertem <eertem@gmail.com>
2025-02-12 22:44:14 +0300

    Make path UNIX based regardless of OS being used @ image URL path extractor util function

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/util/relativePathExtractor.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project/util/relativePathExtractor.js
@@ -5,7 +5,8 @@ const extractRelativePath = function (fullPath) {
 	const match = fullPath.match(/images\\.+/);
 	const relativePath = match ? match[0] : null; // Output: images\2025-02-12T10-39-02.335Z-2.jpeg
 	const updatedPath = path.sep + relativePath; // Output: \images\2025-02-12T10-39-02.335Z-2.jpeg
-	return updatedPath;
+	const finalUrlPath = path.normalize(updatedPath).replace(/\\/g, '/'); // OS indeoendent URL path with unified unix syntax
+	return finalUrlPath;
 };
 
 module.exports = extractRelativePath;
