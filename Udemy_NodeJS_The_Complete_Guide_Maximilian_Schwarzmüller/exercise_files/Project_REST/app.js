8a48f56b7da03b35b3788510f8cb7a9f3b98fb0b
Erhan Ertem <eertem@gmail.com>
2025-02-24 00:51:09 +0300

    Include dotenv package for accessing global variables

--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project_REST/app.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project_REST/app.js
@@ -1,4 +1,10 @@
+const path = require('path');
 const express = require('express');
+
+const dotenv = require('dotenv');
+// Load appropriate .env file based on NODE_ENV
+const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+dotenv.config({ path: path.resolve(__dirname, envFile) });
 
 const feedRoutes = require('./routes/feed');
 
@@ -13,6 +19,10 @@ app.use(express.json());
 // App Routes
 app.use('/feed', feedRoutes); // Prefixes the endpoint URL with /feed
 
+// Start the server
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 app.listen(PORT, DB_HOST, () => {
 	console.log(`Listening on port ${PORT}, running on DB_HOST:${DB_HOST} in ${NODE_ENV} mode`);
 });
--- Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project_REST/controllers/feed.js
+++ Udemy_NodeJS_The_Complete_Guide_Maximilian_Schwarzmüller/exercise_files/Project_REST/controllers/feed.js
@@ -1,1 +1,11 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+	res.status(200).json({
+		posts: [
+			{
+				_id: '1',
+				title: 'First Post',
+				content: 'This is the first post',
+			},
+		],
+	});
+};
