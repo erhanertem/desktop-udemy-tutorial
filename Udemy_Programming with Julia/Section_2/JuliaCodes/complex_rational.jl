be04801bb3c03a9ad8ccfdd455f918f28a54d8f9
Erhan Ertem <eertem@gmail.com>
2025-03-24 18:19:44 +0300

    Complex and rational Numeric Data Types

---
+++ Udemy_Programming with Julia/Section_2/JuliaCodes/complex_rational.jl
@@ -0,0 +1,23 @@
+3 + 4im
+x = 3 + 4im
+typeof(x)
+y = 2.1 + 3.5im
+typeof(y)
+
+im^2 # -1.0+0im
+
+sqrt(-1) # Error
+√Complex(-1) # 0+1im
+√(-1 + 0im) # 0+1im
+
+(2 + 3im) * (4 + 4im)
+
+c = 5 + 7im
+real(c)
+imag(c)
+
+x = 5 // 7
+typeof(x)
+
+isa(1, Complex)
+isa("Pentium", String)
--- Udemy_Programming with Julia/Section_2/JuliaCodes/integer_float.jl
+++ Udemy_Programming with Julia/Section_2/JuliaCodes/integer_float.jl
@@ -2,6 +2,7 @@ typemax(Int8)
 typemax(Int16)
 typemin(Int16)
 typemax(Int64)
+println(typemax(Int64) + 1)
 typemin(Int64)
 
 typeof(12)
@@ -13,6 +14,7 @@ typeof(2.2e-2)
 # Float32
 2.2f2
 typeof(2.2f2)
+Float32(3.14)
 
 # Using data types for conversion
 typeof(3.1)
@@ -31,26 +33,35 @@ Inf16
 Inf32
 1 / Inf # 0
 1 / 0 # Inf
+isinf(1 / 0)
 
 # NaN
 0 / 0
 0 * Inf
+isnan(0 / 0)
 
-a = 1;
-b = 3;
-c = 5;
-isinf(a / b)
-isnan(b / c)
 
 eps()
+eps(1.0)
 eps(Float32)
 eps(Float16)
 
-eps(1.0)
+eps(Float32(1.0))
 eps(1000.0)
+
+x = 1.0;
+y = eps(x) / 2;
+x + y == x
 
 typeof(true)
 typeof(false)
 
 Bool(1)
 Bool(0)
+
+x = 10^50
+x = big(10)^50
+
+x = Inf32
+y = 5.2f0
+println(x + y)
