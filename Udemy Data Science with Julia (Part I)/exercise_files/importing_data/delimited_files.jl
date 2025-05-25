7171ee6d444318238c11fff8a755f9ab1dc04f84
Erhan Ertem <eertem@gmail.com>
2025-05-25 18:08:56 +0300

    File operations - Delimited CSV files

---
+++ Udemy Data Science with Julia (Part I)/exercise_files/importing_data/delimited_files.jl
@@ -0,0 +1,17 @@
+using CSV, DataFrames
+currPath = dirname(@__FILE__)
+cd(currPath)
+readdir()
+cd("03ImportData")
+
+student_data = CSV.read("student_data.csv", DataFrame)
+student_data = CSV.read("student_data.csv", DataFrame,
+  delim=';',
+  header=true,
+  missingstring="N/A",
+  dateformat="dd/mm/yyyy")
+
+input_data = select(student_data, 1:12) # Select 1thru12 columns on student_data DF
+
+CSV.write("input_data.csv", input_data)
+CSV.write("input_data.csv", input_data, delim="|")
